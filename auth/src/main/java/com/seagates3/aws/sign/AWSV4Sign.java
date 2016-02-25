7468235b15a363f91e8872863fcecca8b0dc2529
Arjun <arjun.hariharan@seagate.com>
2016-02-25 22:42:46 +0530

    Fix signing algo v4 with path style URL Update system tests to test signing algorithm.
    Change-Id: Ied198e9eb0a897f4f6c1c759a8cc0e3cd7e17985

--- auth/src/main/java/com/seagates3/aws/sign/AWSV4Sign.java
+++ auth/src/main/java/com/seagates3/aws/sign/AWSV4Sign.java
@@ -16,31 +16,27 @@
  * Original author:  Arjun Hariharan <arjun.hariharan@seagate.com>
  * Original creation date: 17-Sep-2014
  */
-
 package com.seagates3.aws.sign;
 
+import com.seagates3.model.ClientRequestToken;
+import com.seagates3.model.Requestor;
+import com.seagates3.util.BinaryUtil;
 import java.io.UnsupportedEncodingException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import com.seagates3.authserver.AuthServerConfig;
-import com.seagates3.model.ClientRequestToken;
-import com.seagates3.model.Requestor;
-import com.seagates3.util.BinaryUtil;
+public class AWSV4Sign implements AWSSign {
 
-public class AWSV4Sign implements AWSSign{
-
-    /*
+    /**
      * Return true if the signature is valid.
+     *
+     * @param clientRequestToken
+     * @param requestor
+     * @return True if authenticated.
      */
     @Override
     public Boolean authenticate(ClientRequestToken clientRequestToken,
@@ -55,19 +51,16 @@ public class AWSV4Sign implements AWSSign{
         String secretKey = requestor.getAccesskey().getSecretKey();
         signingKey = deriveSigningKey(clientRequestToken, secretKey);
 
-        signature = calculateSignature(signingKey ,stringToSign);
+        signature = calculateSignature(signingKey, stringToSign);
 
         return signature.equals(clientRequestToken.getSignature());
     }
 
-    /*
+    /**
      * Canonical Request is in the following format.
      *
-     * HTTPRequestMethod + '\n' +
-     * CanonicalURI + '\n' +
-     * CanonicalQueryString + '\n' +
-     * CanonicalHeaders + '\n' +
-     * SignedHeaders + '\n' +
+     * HTTPRequestMethod + '\n' + CanonicalURI + '\n' + CanonicalQueryString +
+     * '\n' + CanonicalHeaders + '\n' + SignedHeaders + '\n' +
      * HexEncode(Hash(RequestPayload))
      */
     private String createCanonicalRequest(ClientRequestToken clientRequestToken) {
@@ -77,8 +70,7 @@ public class AWSV4Sign implements AWSSign{
         Map<String, String> requestHeaders = clientRequestToken.getRequestHeaders();
 
         httpMethod = clientRequestToken.getHttpMethod();
-        canonicalURI = createCanonicalUri(requestHeaders.get("host"),
-                clientRequestToken.getUri(), clientRequestToken.isVirtualHost());
+        canonicalURI = clientRequestToken.getUri();
         canonicalQuery = getCanonicalQuery(clientRequestToken.getQuery());
         canonicalHeader = createCanonicalHeader(clientRequestToken);
         hashedPayload = createHashedPayload(clientRequestToken);
@@ -95,18 +87,18 @@ public class AWSV4Sign implements AWSSign{
     }
 
     /**
-     * Calculate the hash of Payload using the following formula.
-     * HashedPayload = Lowercase(HexEncode(Hash(requestPayload)))
+     * Calculate the hash of Payload using the following formula. HashedPayload
+     * = Lowercase(HexEncode(Hash(requestPayload)))
      *
      * @param clientRequestToken
      * @return Hashed Payload
      */
     private String createHashedPayload(ClientRequestToken clientRequestToken) {
-        if(clientRequestToken.getRequestHeaders().get("x-amz-content-sha256") != null) {
+        if (clientRequestToken.getRequestHeaders().get("x-amz-content-sha256") != null) {
             return clientRequestToken.getRequestHeaders().get("x-amz-content-sha256");
         } else {
             String hashMethodName = AWSSign.AWSHashFunction.get(
-                                        clientRequestToken.getSigningAlgorithm());
+                    clientRequestToken.getSigningAlgorithm());
             Method method;
             String hashedPayload = "";
             try {
@@ -123,15 +115,12 @@ public class AWSV4Sign implements AWSSign{
         }
     }
 
-    /*
-     * The string to sign includes meta information about your request and
-     * about the canonical request.
+    /**
+     * The string to sign includes meta information about your request and about
+     * the canonical request.
      *
-     * Structure of String to sign
-     * Algorithm + '\n' +
-     * RequestDate + '\n' +
-     * CredentialScope + '\n' +
-     * HashedCanonicalRequest
+     * Structure of String to sign Algorithm + '\n' + RequestDate + '\n' +
+     * CredentialScope + '\n' + HashedCanonicalRequest
      */
     private String createStringToSign(String canonicalRequest,
             ClientRequestToken clientRequestToken) {
@@ -146,16 +135,14 @@ public class AWSV4Sign implements AWSSign{
         return stringToSign;
     }
 
-    /*
+    /**
      * Before calculating the signature, derive a signing key secret access key.
      *
      * Algorithm-
      *
-     * kSecret = Your AWS Secret Access Key
-     * kDate = HMAC("AWS4" + kSecret, Date)
-     * kRegion = HMAC(kDate, Region)
-     * kService = HMAC(kRegion, Service)
-     * kSigning = HMAC(kService, "aws4_request")
+     * kSecret = Your AWS Secret Access Key kDate = HMAC("AWS4" + kSecret, Date)
+     * kRegion = HMAC(kDate, Region) kService = HMAC(kRegion, Service) kSigning
+     * = HMAC(kService, "aws4_request")
      */
     private byte[] deriveSigningKey(ClientRequestToken clientRequestToken, String secretKey) {
         try {
@@ -179,7 +166,7 @@ public class AWSV4Sign implements AWSSign{
         return null;
     }
 
-    /*
+    /**
      * Return the signature of the request.
      *
      * signature = HexEncode(HMAC(derived-signing-key, string-to-sign))
@@ -195,33 +182,34 @@ public class AWSV4Sign implements AWSSign{
         return null;
     }
 
-    /*
+    /**
      * The canonical headers consist of a list of all the HTTP headers are
-     * include as a part of the AWS request.
-     * Convert all header names to lowercase and trim excess white space
-     * characters out of the header values.
+     * include as a part of the AWS request. Convert all header names to
+     * lowercase and trim excess white space characters out of the header
+     * values.
      *
-     * Canonical Header = CanonicalHeadersEntry0 + CanonicalHeadersEntry1
-     *                      + ... + CanonicalHeadersEntryN
+     * Canonical Header = CanonicalHeadersEntry0 + CanonicalHeadersEntry1 + ...
+     * + CanonicalHeadersEntryN
      *
-     * CanonicalHeadersEntry = Lowercase(HeaderName) + ':' + Trimall(HeaderValue) + '\n'
+     * CanonicalHeadersEntry = Lowercase(HeaderName) + ':' +
+     * Trimall(HeaderValue) + '\n'
      */
     private String createCanonicalHeader(ClientRequestToken clientRequestToken) {
         String headerValue;
         String canonicalHeader = "";
         Map<String, String> requestHeaders = clientRequestToken.getRequestHeaders();
 
-        for(String s : clientRequestToken.getSignedHeaders().split(";")) {
-            if(s.equalsIgnoreCase("content-type")) {
+        for (String s : clientRequestToken.getSignedHeaders().split(";")) {
+            if (s.equalsIgnoreCase("content-type")) {
                 headerValue = requestHeaders.get(s).trim();
 
                 /*
                  * Strangely, the aws .net sdk doesn't send the content type.
                  * Hence the content type is hard coded.
                  */
-                if(headerValue.isEmpty()) {
+                if (headerValue.isEmpty()) {
                     canonicalHeader += "content-type:application/x-www-form-urlencoded;"
-                                    + " charset=utf-8\n";
+                            + " charset=utf-8\n";
                 } else {
                     canonicalHeader += String.format("%s:%s\n", s, headerValue);
                 }
@@ -234,108 +222,43 @@ public class AWSV4Sign implements AWSSign{
         return canonicalHeader;
     }
 
-    /*
-     * For S3 APIs
-     *   If the host is path style, then remove the bucket name from the URI
-     *   to form canonical URI.
-     *
-     * Ex - host = s3.seagate.com
-     *      URI = /bucket/myfile
-     *      CanonicalURI = /myfile
-     */
-    private String createCanonicalUri(String host, String uri, Boolean virtualHost) {
-        if(virtualHost) {
-            return uri;
-        }
-
-        List<String> s3Endpoints = Arrays.asList(AuthServerConfig.getEndpoints());
-        List<String> uriEndpoints = new ArrayList<>();
-        uriEndpoints.add(AuthServerConfig.getDefaultEndpoint());
-        uriEndpoints.addAll(s3Endpoints);
-
-        String patternToMatch;
-        Pattern pattern;
-        Matcher matcher;
-
-        /*
-         * Iterate over the endpoints to check if the host is an s3 end point.
-         * If the host is an s3 end point and is in path style, then remove the
-         * bucket name from the URI and return it.
-         */
-        for(String endPoint : uriEndpoints) {
-            patternToMatch = String.format("%s[:\\d]*$", endPoint);
-            pattern = Pattern.compile(patternToMatch);
-            matcher = pattern.matcher(host);
-            if(matcher.matches()) {
-                /*
-                 * It is an s3 end point and the host is in path style format.
-                 * Remove the bucket name from the URI and return it.
-                 */
-                pattern = Pattern.compile("/.*/.*");
-                matcher = pattern.matcher(uri);
-
-                /*
-                 * Check if the uri in the following format
-                 *    /[bucket]/../..
-                 */
-                if(matcher.matches()) {
-                    String[] tokens = uri.split("/[\\w]*/",2);
-                    String canonicalUri = "/" + tokens[1];
-                    return canonicalUri;
-                } else {
-                    /*
-                     * Unrecognized pattern. Return URI as it is.
-                     */
-                    return uri;
-                }
-            }
-        }
-
-        /*
-         * The host is not an s3 end point. It could be an IAM or STS end point.
-         * Return the URI as it is.
-         */
-        return uri;
-    }
-
     /**
      * To construct the canonical query string, complete the following steps:
      *
-     * 1. URI-encode each parameter name and value according to the following rules:
-     *   a. Do not URL-encode any of the unreserved characters that RFC 3986 defines:
-     *      A-Z, a-z, 0-9, hyphen ( - ), underscore ( _ ), period ( . ), and tilde ( ~ ).
-     *   b. Percent-encode all other characters with %XY, where X and Y are
-     *      hexadecimal characters (0-9 and uppercase A-F).
-     *      For example, the space character must be encoded as %20
-     *      (not using '+', as some encoding schemes do) and extended UTF-8
-     *      characters must be in the form %XY%ZA%BC.
+     * 1. URI-encode each parameter name and value according to the following
+     * rules: a. Do not URL-encode any of the unreserved characters that RFC
+     * 3986 defines: A-Z, a-z, 0-9, hyphen ( - ), underscore ( _ ), period ( .
+     * ), and tilde ( ~ ). b. Percent-encode all other characters with %XY,
+     * where X and Y are hexadecimal characters (0-9 and uppercase A-F). For
+     * example, the space character must be encoded as %20 (not using '+', as
+     * some encoding schemes do) and extended UTF-8 characters must be in the
+     * form %XY%ZA%BC.
      *
-     * 2. Sort the encoded parameter names by character code
-     *     (that is, in strict ASCII order). For example, a parameter name
-     *     that begins with the uppercase letter F (ASCII code 70) precedes a
-     *     parameter name that begins with a lowercase letter b (ASCII code 98).
+     * 2. Sort the encoded parameter names by character code (that is, in strict
+     * ASCII order). For example, a parameter name that begins with the
+     * uppercase letter F (ASCII code 70) precedes a parameter name that begins
+     * with a lowercase letter b (ASCII code 98).
      *
-     * 3. Build the canonical query string by starting with the first
-     *    parameter name in the sorted list.
+     * 3. Build the canonical query string by starting with the first parameter
+     * name in the sorted list.
      *
-     * 4. For each parameter, append the URI-encoded parameter name, followed
-     *    by the character '=' (ASCII code 61), followed by the URI-encoded
-     *    parameter value.
-     *    Use an empty string for parameters that have no value.
+     * 4. For each parameter, append the URI-encoded parameter name, followed by
+     * the character '=' (ASCII code 61), followed by the URI-encoded parameter
+     * value. Use an empty string for parameters that have no value.
      *
      * 5. Append the character '&' (ASCII code 38) after each parameter value
-     *    except for the last value in the list.
+     * except for the last value in the list.
      */
     private String getCanonicalQuery(String query) {
-        if(query.isEmpty()) {
+        if (query.isEmpty()) {
             return "";
         }
 
         Map<String, String> queryParams = new TreeMap<>();
-        String[] tokens =  query.split("&");
-        for(String token: tokens) {
+        String[] tokens = query.split("&");
+        for (String token : tokens) {
             String[] subTokens = token.split("=");
-            if(subTokens.length == 2) {
+            if (subTokens.length == 2) {
                 queryParams.put(subTokens[0], subTokens[1]);
             } else {
                 queryParams.put(subTokens[0], "");
@@ -353,7 +276,7 @@ public class AWSV4Sign implements AWSSign{
 
             }
 
-            if(entries.hasNext()) {
+            if (entries.hasNext()) {
                 canonicalString += "&";
             }
         }
--- st/clitests/s3cmd_spec.py
+++ st/clitests/s3cmd_spec.py
@@ -8,6 +8,12 @@ from s3cmd import S3cmdTest
 # Config.log_enabled = True
 # Config.dummy_run = True
 
+# TODO
+# DNS-compliant bucket names should not contains underscore or other special characters.
+# The allowed characters are [a-zA-Z0-9.-]*
+#
+# Add validations to S3 server and write system tests for the same.
+
 #  ***MAIN ENTRY POINT
 
 # Run before all to setup the test environment.
@@ -18,76 +24,88 @@ PyCliTest('Before_all').before_all()
 Config.config_file = "pathstyle.s3cfg"
 
 # ************ Create bucket ************
-S3cmdTest('s3cmd can create bucket').create_bucket("seagate_bucket").execute_test().command_is_successful()
+S3cmdTest('s3cmd can create bucket').create_bucket("seagatebucket").execute_test().command_is_successful()
 
 # ************ List buckets ************
-S3cmdTest('s3cmd can list buckets').list_buckets().execute_test().command_is_successful().command_response_should_have('s3://seagate_bucket')
+S3cmdTest('s3cmd can list buckets').list_buckets().execute_test().command_is_successful().command_response_should_have('s3://seagatebucket')
 
 # ************ 3k FILE TEST ************
-S3cmdTest('s3cmd can upload 3k file').upload_test("seagate_bucket", "3kfile", 3000).execute_test().command_is_successful()
+S3cmdTest('s3cmd can upload 3k file').upload_test("seagatebucket", "3kfile", 3000).execute_test().command_is_successful()
 
-S3cmdTest('s3cmd can download 3k file').download_test("seagate_bucket", "3kfile").execute_test().command_is_successful().command_created_file("3kfile")
+S3cmdTest('s3cmd can download 3k file').download_test("seagatebucket", "3kfile").execute_test().command_is_successful().command_created_file("3kfile")
 
 # ************ 8k FILE TEST ************
-S3cmdTest('s3cmd can upload 8k file').upload_test("seagate_bucket", "8kfile", 8192).execute_test().command_is_successful()
+S3cmdTest('s3cmd can upload 8k file').upload_test("seagatebucket", "8kfile", 8192).execute_test().command_is_successful()
 
-S3cmdTest('s3cmd can download 8k file').download_test("seagate_bucket", "8kfile").execute_test().command_is_successful().command_created_file("8kfile")
+S3cmdTest('s3cmd can download 8k file').download_test("seagatebucket", "8kfile").execute_test().command_is_successful().command_created_file("8kfile")
 
 # ************ OBJECT LISTING TEST ************
-S3cmdTest('s3cmd can list objects').list_objects('seagate_bucket').execute_test().command_is_successful().command_response_should_have('s3://seagate_bucket/3kfile').command_response_should_have('s3://seagate_bucket/8kfile')
+S3cmdTest('s3cmd can list objects').list_objects('seagatebucket').execute_test().command_is_successful().command_response_should_have('s3://seagatebucket/3kfile').command_response_should_have('s3://seagatebucket/8kfile')
 
-S3cmdTest('s3cmd can list specific objects').list_specific_objects('seagate_bucket', '3k').execute_test().command_is_successful().command_response_should_have('s3://seagate_bucket/3kfile').command_response_should_not_have('s3://seagate_bucket/8kfile')
+S3cmdTest('s3cmd can list specific objects').list_specific_objects('seagatebucket', '3k').execute_test().command_is_successful().command_response_should_have('s3://seagatebucket/3kfile').command_response_should_not_have('s3://seagatebucket/8kfile')
 
 # ************ DELETE OBJECT TEST ************
-S3cmdTest('s3cmd can delete 3k file').delete_test("seagate_bucket", "3kfile").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete 3k file').delete_test("seagatebucket", "3kfile").execute_test().command_is_successful()
 
-S3cmdTest('s3cmd can delete 8k file').delete_test("seagate_bucket", "8kfile").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete 8k file').delete_test("seagatebucket", "8kfile").execute_test().command_is_successful()
 
 # ************ 700K FILE TEST ************
-S3cmdTest('s3cmd can upload 700K file').upload_test("seagate_bucket", "700Kfile", 716800).execute_test().command_is_successful()
+S3cmdTest('s3cmd can upload 700K file').upload_test("seagatebucket", "700Kfile", 716800).execute_test().command_is_successful()
 
-S3cmdTest('s3cmd can download 700K file').download_test("seagate_bucket", "700Kfile").execute_test().command_is_successful().command_created_file("700Kfile")
+S3cmdTest('s3cmd can download 700K file').download_test("seagatebucket", "700Kfile").execute_test().command_is_successful().command_created_file("700Kfile")
 
-S3cmdTest('s3cmd can delete 700K file').delete_test("seagate_bucket", "700Kfile").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete 700K file').delete_test("seagatebucket", "700Kfile").execute_test().command_is_successful()
 
 # ************ Delete bucket TEST ************
-S3cmdTest('s3cmd can delete bucket').delete_bucket("seagate_bucket").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete bucket').delete_bucket("seagatebucket").execute_test().command_is_successful()
+
+# ************ Signing algorithm test ************
+S3cmdTest('s3cmd can create bucket seagate-bucket').create_bucket("seagate-bucket").execute_test().command_is_successful()
+S3cmdTest('s3cmd can create bucket SEAGATEBUCKET123').create_bucket("SEAGATEBUCKET123").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete bucket seagate-bucket').delete_bucket("seagate-bucket").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete bucket SEAGATEBUCKET123').delete_bucket("SEAGATEBUCKET123").execute_test().command_is_successful()
 
 # Virtual Host style tests.
 Config.config_file = "virtualhoststyle.s3cfg"
 
 # ************ Create bucket ************
-S3cmdTest('s3cmd can create bucket').create_bucket("seagate_bucket").execute_test().command_is_successful()
+S3cmdTest('s3cmd can create bucket').create_bucket("seagatebucket").execute_test().command_is_successful()
 
 # ************ List buckets ************
-S3cmdTest('s3cmd can list buckets').list_buckets().execute_test().command_is_successful().command_response_should_have('s3://seagate_bucket')
+S3cmdTest('s3cmd can list buckets').list_buckets().execute_test().command_is_successful().command_response_should_have('s3://seagatebucket')
 
 # ************ 3k FILE TEST ************
-S3cmdTest('s3cmd can upload 3k file').upload_test("seagate_bucket", "3kfile", 3000).execute_test().command_is_successful()
+S3cmdTest('s3cmd can upload 3k file').upload_test("seagatebucket", "3kfile", 3000).execute_test().command_is_successful()
 
-S3cmdTest('s3cmd can download 3k file').download_test("seagate_bucket", "3kfile").execute_test().command_is_successful().command_created_file("3kfile")
+S3cmdTest('s3cmd can download 3k file').download_test("seagatebucket", "3kfile").execute_test().command_is_successful().command_created_file("3kfile")
 
 # ************ 8k FILE TEST ************
-S3cmdTest('s3cmd can upload 8k file').upload_test("seagate_bucket", "8kfile", 8192).execute_test().command_is_successful()
+S3cmdTest('s3cmd can upload 8k file').upload_test("seagatebucket", "8kfile", 8192).execute_test().command_is_successful()
 
-S3cmdTest('s3cmd can download 8k file').download_test("seagate_bucket", "8kfile").execute_test().command_is_successful().command_created_file("8kfile")
+S3cmdTest('s3cmd can download 8k file').download_test("seagatebucket", "8kfile").execute_test().command_is_successful().command_created_file("8kfile")
 
 # ************ OBJECT LISTING TEST ************
-S3cmdTest('s3cmd can list objects').list_objects('seagate_bucket').execute_test().command_is_successful().command_response_should_have('s3://seagate_bucket/3kfile').command_response_should_have('s3://seagate_bucket/8kfile')
+S3cmdTest('s3cmd can list objects').list_objects('seagatebucket').execute_test().command_is_successful().command_response_should_have('s3://seagatebucket/3kfile').command_response_should_have('s3://seagatebucket/8kfile')
 
-S3cmdTest('s3cmd can list specific objects').list_specific_objects('seagate_bucket', '3k').execute_test().command_is_successful().command_response_should_have('s3://seagate_bucket/3kfile').command_response_should_not_have('s3://seagate_bucket/8kfile')
+S3cmdTest('s3cmd can list specific objects').list_specific_objects('seagatebucket', '3k').execute_test().command_is_successful().command_response_should_have('s3://seagatebucket/3kfile').command_response_should_not_have('s3://seagatebucket/8kfile')
 
 # ************ DELETE OBJECT TEST ************
-S3cmdTest('s3cmd can delete 3k file').delete_test("seagate_bucket", "3kfile").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete 3k file').delete_test("seagatebucket", "3kfile").execute_test().command_is_successful()
 
-S3cmdTest('s3cmd can delete 8k file').delete_test("seagate_bucket", "8kfile").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete 8k file').delete_test("seagatebucket", "8kfile").execute_test().command_is_successful()
 
 # ************ 700K FILE TEST ************
-S3cmdTest('s3cmd can upload 700K file').upload_test("seagate_bucket", "700Kfile", 716800).execute_test().command_is_successful()
+S3cmdTest('s3cmd can upload 700K file').upload_test("seagatebucket", "700Kfile", 716800).execute_test().command_is_successful()
 
-S3cmdTest('s3cmd can download 700K file').download_test("seagate_bucket", "700Kfile").execute_test().command_is_successful().command_created_file("700Kfile")
+S3cmdTest('s3cmd can download 700K file').download_test("seagatebucket", "700Kfile").execute_test().command_is_successful().command_created_file("700Kfile")
 
-S3cmdTest('s3cmd can delete 700K file').delete_test("seagate_bucket", "700Kfile").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete 700K file').delete_test("seagatebucket", "700Kfile").execute_test().command_is_successful()
 
 # ************ Delete bucket TEST ************
-S3cmdTest('s3cmd can delete bucket').delete_bucket("seagate_bucket").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete bucket').delete_bucket("seagatebucket").execute_test().command_is_successful()
+
+# ************ Signing algorithm test ************
+S3cmdTest('s3cmd can create bucket seagate-bucket').create_bucket("seagate-bucket").execute_test().command_is_successful()
+S3cmdTest('s3cmd can create bucket SEAGATEBUCKET123').create_bucket("SEAGATEBUCKET123").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete bucket seagate-bucket').delete_bucket("seagate-bucket").execute_test().command_is_successful()
+S3cmdTest('s3cmd can delete bucket SEAGATEBUCKET123').delete_bucket("SEAGATEBUCKET123").execute_test().command_is_successful()
