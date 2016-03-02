4edf2820baa221e13553a322ddb93a80d8b076c2
Rajesh Nambiar <rajesh.nambiar@seagate.com>
2016-03-02 16:48:28 +0000

    Introduced logging mechanism
    Change-Id: Ie60467004905c159f15620b4e2ff38d23f4a0890

---
+++ server/s3_log.h
@@ -0,0 +1,60 @@
+/*
+ * COPYRIGHT 2016 SEAGATE LLC
+ *
+ * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+ * HEREIN, ARE THE EXCLUSIVE PROPERTY OF SEAGATE TECHNOLOGY
+ * LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+ * THE PRIOR WRITTEN PERMISSION OF SEAGATE TECHNOLOGY LIMITED,
+ * BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+ * USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+ * EXCEPT AS ALLOWED BY THE TERMS OF SEAGATE LICENSES AND AGREEMENTS.
+ *
+ * YOU SHOULD HAVE RECEIVED A COPY OF SEAGATE'S LICENSE ALONG WITH
+ * THIS RELEASE. IF NOT PLEASE CONTACT A SEAGATE REPRESENTATIVE
+ * http://www.seagate.com/contact
+ *
+ * Original author:  Rajesh Nambiar   <rajesh.nambiar@seagate.com>
+ * Original author:  Kaustubh Deorukhkar   <kaustubh.deorukhkar@seagate.com>
+ * Original creation date: 2-March-2016
+ */
+
+#pragma once
+
+#ifndef __MERO_FE_S3_SERVER_LOG_H__
+#define __MERO_FE_S3_SERVER_LOG_H__
+
+#define S3_LOG_FATAL 1
+#define S3_LOG_ERROR 2
+#define S3_LOG_WARN  3
+#define S3_LOG_INFO  4
+#define S3_LOG_DEBUG 5
+
+
+
+extern const char *log_level_str[S3_LOG_DEBUG];
+
+extern FILE *fp_log;
+
+extern int s3log_level;
+
+//++
+// Log format:
+//   Log level:[HH:MM:SS,MilliSec Month:Date:Year] "<filename>":[line#] <function name>: <Log description>
+// Example:
+//   DEBUG:[15:46:17,091 02:02:2016] "server/s3server.cc":[219]  main: S3 Server running...
+// --
+
+#define __S3QUOTE(x) # x
+#define  _S3QUOTE(x)  __S3QUOTE(x)
+#define s3_debug_strlen(x)     strlen(x)
+#define s3_log(loglevel,fmt, ...)  \
+        if(loglevel <= s3log_level) {  \
+          struct timeval tval;  \
+          gettimeofday(&tval,NULL);   \
+          unsigned int nMillisec = tval.tv_usec / 1000;  \
+          struct tm * dm = localtime(&tval.tv_sec);      \
+          fprintf(fp_log, "%s:[%02d:%02d:%02d,%03d %02d:%02d:%04d] " _S3QUOTE(__FILE__) ":[" _S3QUOTE(__LINE__) "]  %s: " \
+          fmt "\n", log_level_str[loglevel-1], dm->tm_hour, dm->tm_min, dm->tm_sec,nMillisec, dm->tm_mon, dm->tm_mday,dm->tm_year + 1900, __func__, ## __VA_ARGS__); \
+          fflush(fp_log);  \
+        }
+#endif
--- server/s3server.cc
+++ server/s3server.cc
@@ -33,6 +33,7 @@
 #include "s3_error_codes.h"
 #include "s3_perf_logger.h"
 #include "s3_timer.h"
+#include "s3_log.h"
 
 #define WEBSTORE "/home/seagate/webstore"
 
@@ -44,10 +45,18 @@ const char *clovis_confd_addr = "localhost@tcp:12345:33:100";
 const char *clovis_prof = "<0x7000000000000001:0>";
 uint16_t bind_port      = 8081;
 short    clovis_layout_id = 9;
-
+char *s3_log_file;
+char *s3_log_mode;
 // S3 Auth service
 const char *auth_ip_addr = "127.0.0.1";
 uint16_t auth_port = 8085;
+
+const char *log_level_str[S3_LOG_DEBUG] = {"FATAL", "ERROR", "WARN", "INFO", "DEBUG"};
+
+FILE *fp_log;
+
+//To be read from config file
+int s3log_level = S3_LOG_INFO;
 
 /* MD5 helper */
 /* KD xxx - needed? intention? convert 16bytes to 32bytes readable string */
@@ -72,7 +81,7 @@ void get_oid_using_hash(const char* url, struct m0_uint128 *object_id)
     size_t len = 0;
     uint64_t hash128_64[2];
 
-    printf("URI = %s\n", url);
+    s3_log(S3_LOG_INFO, "URI = %s\n", url);
     len = strlen(url);
     MurmurHash3_x64_128(url, len, 0, &hash128_64);
 
@@ -112,7 +121,7 @@ s3_handler(evhtp_request_t * req, void * a) {
 
 extern "C" evhtp_res
 dispatch_request(evhtp_request_t * req, evhtp_headers_t * hdrs, void * arg ) {
-    printf("RECEIVED Request headers. Create S3Request with arg = %s\n", (char*)arg);
+    s3_log(S3_LOG_INFO, "RECEIVED Request headers. Create S3Request with arg = %s\n", (char*)arg);
 
     S3Router *router = (S3Router*)arg;
 
@@ -128,7 +137,7 @@ dispatch_request(evhtp_request_t * req, evhtp_headers_t * hdrs, void * arg ) {
 
 extern "C" evhtp_res
 process_request_data(evhtp_request_t * req, evbuf_t * buf, void * arg) {
-  printf("RECEIVED Request body for sock = %d\n", req->conn->sock);
+  s3_log(S3_LOG_DEBUG, "RECEIVED Request body for sock = %d\n", req->conn->sock);
   S3RequestObject* request = (S3RequestObject*)req->cbarg;
 
   if (request) {
@@ -139,10 +148,10 @@ process_request_data(evhtp_request_t * req, evbuf_t * buf, void * arg) {
 
     request->notify_incoming_data(s3_buf);
 
-    printf("got %zu bytes of data for sock = %d\n", bytes_received, req->conn->sock);
+    s3_log(S3_LOG_DEBUG, "got %zu bytes of data for sock = %d\n", bytes_received, req->conn->sock);
   } else {
     evhtp_unset_all_hooks(&req->conn->hooks);
-    printf("S3 request failed, Ignoring data for this request \n");
+    s3_log(S3_LOG_DEBUG, "S3 request failed, Ignoring data for this request \n");
   }
 
   return EVHTP_RES_OK;
@@ -157,7 +166,7 @@ set_s3_connection_handlers(evhtp_connection_t * conn, void * arg) {
     return EVHTP_RES_OK;
 }
 
-const char * optstr = "a:p:l:c:s:d:h:i";
+const char * optstr = "a:p:l:c:s:m:o:d:h:i";
 
 const char * help   =
     "Options: \n"
@@ -168,7 +177,9 @@ const char * help   =
     "  -c <str> : clovis confd address     (default: 10.0.2.15@tcp:12345:33:100)\n"
     "  -s <str> : Auth Service address     (default: 127.0.0.1)\n"
     "  -d <int> : Auth Service port        (default: 8085)\n"
-    "  -i <int> : Clovis layout id         (default: 9 (1MB))\n";
+    "  -i <int> : Clovis layout id         (default: 9 (1MB))\n"
+    "  -o <str> : S3 Log file              (default: stdout)\n"
+    "  -m <str> : S3 Log Level             (DEBUG | INFO | ERROR | FATAL  default is : INFO)\n";
 
 int
 parse_args(int argc, char ** argv) {
@@ -204,19 +215,46 @@ parse_args(int argc, char ** argv) {
             case 'i':
                 clovis_layout_id      = atoi(optarg);
                 break;
+            case 'o':
+                s3_log_file           = strdup(optarg);
+                break;
+            case 'm':
+                s3_log_mode            = strdup(optarg);
+                break;
             default:
                 printf("Unknown opt %s\n", optarg);
                 return -1;
         } /* switch */
     }
-    printf("bind_addr = %s\n", bind_addr);
-    printf("bind_port = %d\n", bind_port);
-    printf("clovis_local_addr = %s\n", clovis_local_addr);
-    printf("clovis_confd_addr = %s\n", clovis_confd_addr);
-    printf("Auth server: %s\n",auth_ip_addr);
-    printf("Auth server port: %d\n",auth_port);
-    printf("clovis_layout_id = %d\n", clovis_layout_id);
+    if(s3_log_file) {
+      fp_log = std::fopen(s3_log_file, "a");
+      if(fp_log == NULL) {
+        printf("File opening of log %s failed\n",s3_log_file);
+        return -1;
+      }
+    } else {
+      fp_log = stdout;
+    }
 
+    s3_log(S3_LOG_INFO, "bind_addr = %s\n", bind_addr);
+    s3_log(S3_LOG_INFO, "bind_port = %d\n", bind_port);
+    s3_log(S3_LOG_INFO, "clovis_local_addr = %s\n", clovis_local_addr);
+    s3_log(S3_LOG_INFO, "clovis_confd_addr = %s\n", clovis_confd_addr);
+    s3_log(S3_LOG_INFO, "Auth server: %s\n",auth_ip_addr);
+    s3_log(S3_LOG_INFO, "Auth server port: %d\n",auth_port);
+    s3_log(S3_LOG_INFO, "clovis_layout_id = %d\n", clovis_layout_id);
+    s3_log(S3_LOG_INFO, "s3_log_file = %s\n", s3_log_file);
+    s3_log(S3_LOG_INFO, "s3_log_mode = %s\n", s3_log_mode);
+
+    if(strcmp(s3_log_mode,"INFO") == 0) {
+      s3log_level = S3_LOG_INFO;
+    } else if(strcmp(s3_log_mode, "DEBUG") == 0) {
+      s3log_level = S3_LOG_DEBUG;
+    } else if(strcmp(s3_log_mode, "ERROR") == 0) {
+      s3log_level = S3_LOG_ERROR;
+    } else if(strcmp(s3_log_mode, "FATAL") == 0) {
+      s3log_level = S3_LOG_FATAL;
+    }
     return 0;
 } /* parse_args */
 
@@ -239,7 +277,7 @@ main(int argc, char ** argv) {
     evbase_t * evbase = event_base_new();
     evthread_use_pthreads();
     if (evthread_make_base_notifiable(evbase)<0) {
-      printf("Couldn't make base notifiable!");
+      s3_log(S3_LOG_ERROR, "Couldn't make base notifiable!");
       return 1;
     }
     evhtp_t  * htp    = evhtp_new(evbase, NULL);
@@ -259,7 +297,7 @@ main(int argc, char ** argv) {
     /* Initilise mero and Clovis */
     rc = init_clovis(clovis_local_addr, clovis_confd_addr, clovis_prof, clovis_layout_id);
     if (rc < 0) {
-        printf("clovis_init failed!\n");
+        s3_log(S3_LOG_FATAL, "clovis_init failed!\n");
         return rc;
     }
 
@@ -287,6 +325,8 @@ main(int argc, char ** argv) {
 #endif
 
     delete router;
-
+    if(fp_log != NULL && fp_log != stdout) {
+      std::fclose(fp_log);
+    }
     return 0;
 }
