d844a38c26b97a53a53ac0916ddc71cb4d1b8c2d
Rajesh Nambiar <rajesh.nambiar@seagate.com>
2016-08-05 05:36:53 +0100

    Mero-0000: S3 - Fix to ut failure, reset the ids in case of empty name.
    Change-Id: Id18bc6abcc42e0ed7c5e7cb740bc91631dd68c4c

--- server/s3_uri_to_mero_oid.cc
+++ server/s3_uri_to_mero_oid.cc
@@ -32,8 +32,18 @@ void S3UriToMeroOID(const char* name, struct m0_uint128 *object_id) {
   size_t len = 0;
   uint64_t hash128_64[2];
   struct m0_uint128 tmp_uint128;
+  object_id->u_hi = object_id->u_lo = 0;
+  if (name == NULL) {
+    s3_log(S3_LOG_FATAL, "The input parameter 'name' is NULL\n");
+    return;
+  }
 
   len = strlen(name);
+  if (len == 0) {
+    // oid should not be 0
+    s3_log(S3_LOG_FATAL, "The input parameter 'name' is empty string\n");
+    return;
+  }
   MurmurHash3_x64_128(name, len, 0, &hash128_64);
 
   // Reset the higher 8 bits, will be used by Mero
