7cece42445eb37d537810a0d290ac83972788239
Kaustubh Deorukhkar <kaustubh.deorukhkar@seagate.com>
2016-09-20 04:19:01 -0400

    MERO-2036: S3 - Fix the s3stopsystem.sh to handle single unnumbered instance
    Change-Id: Ia5df23213546b1d57193fcfd731ade94264d40cc

--- system/s3stopsystem.sh
+++ system/s3stopsystem.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
-PIDSTR=`cat /var/run/s3server.$1.pid`
+
+if [[ ${1} == +([0-9]) ]]; then
+  PIDFILE="/var/run/s3server.$1.pid"
+else
+  PIDFILE="/var/run/s3server.pid"
+fi
+
+PIDSTR=`cat $PIDFILE`
+
 if [ "$PIDSTR" != "" ]; then
   kill -s TERM $PIDSTR
   if [ "$?" != "0" ]; then
-    rm /var/run/s3server.$1.pid
+    rm $PIDFILE
   fi
 fi
