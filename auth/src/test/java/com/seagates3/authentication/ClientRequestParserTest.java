f362a2deeb6a89bb022fa1fbf6793652cfb4be7d
Sushant Mane <sushant.mane@seagate.com>
2017-02-14 03:18:47 -0600

    CASTOR-2113: S3 - Refactor authentication/ClientRequestParser unit tests
    Change-Id: Ib2b81cb9f6b29e1bc7c809c5731af84d97bc357f

--- auth/src/test/java/com/seagates3/authentication/ClientRequestParserTest.java
+++ auth/src/test/java/com/seagates3/authentication/ClientRequestParserTest.java
@@ -1,5 +1,5 @@
 /*
- * COPYRIGHT 2016 SEAGATE LLC
+ * COPYRIGHT 2017 SEAGATE LLC
  *
  * THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
  * HEREIN, ARE THE EXCLUSIVE PROPERTY OF SEAGATE TECHNOLOGY
@@ -14,39 +14,50 @@
  * http://www.seagate.com/contact
  *
  * Original author: Sushant Mane <sushant.mane@seagate.com>
- * Original creation date: 26-Dec-2016
+ * Original creation date: 14-Feb-2017
  */
+
 package com.seagates3.authentication;
 
 import com.seagates3.authserver.AuthServerConfig;
 import com.seagates3.aws.AWSV2RequestHelper;
 import com.seagates3.aws.AWSV4RequestHelper;
+import io.netty.handler.codec.http.DefaultFullHttpRequest;
 import io.netty.handler.codec.http.FullHttpRequest;
+import io.netty.handler.codec.http.HttpMethod;
+import io.netty.handler.codec.http.HttpVersion;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.powermock.api.mockito.PowerMockito;
+import org.powermock.api.mockito.mockpolicies.Slf4jMockPolicy;
+import org.powermock.core.classloader.annotations.MockPolicy;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
 import java.util.Map;
+import java.util.TreeMap;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(AuthServerConfig.class)
-public class ClientRequestParserPositiveTest {
+@MockPolicy(Slf4jMockPolicy.class)
+public class ClientRequestParserTest {
 
-    private FullHttpRequest httpRequest = mock(FullHttpRequest.class);
+    private FullHttpRequest httpRequest;
     private Map<String, String> requestBody;
 
     @Before
     public void setUp() {
         String[] endpoints = {"s3-us.seagate.com",
                 "s3-europe.seagate.com", "s3-asia.seagate.com"};
-        PowerMockito.mockStatic(AuthServerConfig.class);
-        PowerMockito.when(AuthServerConfig.getEndpoints()).thenReturn(endpoints);
+        requestBody = new TreeMap<>();
+        httpRequest = mock(FullHttpRequest.class);
+        mockStatic(AuthServerConfig.class);
+        when(AuthServerConfig.getEndpoints()).thenReturn(endpoints);
     }
 
     @Test
@@ -74,5 +85,34 @@ public class ClientRequestParserPositiveTest {
         assertNotNull(token);
         assertEquals("AKIAIOSFODNN7EXAMPLE", token.getAccessKeyId());
     }
-}
 
+    @Test(expected = NullPointerException.class)
+    public void parseTest_RequestBodyNull_NullPointerException() {
+        ClientRequestParser.parse(httpRequest, null);
+    }
+
+    @Test
+    public void parseTest_AuthenticateUser_AuthorizationHeaderNull() {
+        requestBody.put("Action", "AuthenticateUser");
+
+        assertNull(ClientRequestParser.parse(httpRequest, requestBody));
+    }
+
+    @Test
+    public void parseTest_AuthorizeUser_AuthorizationHeaderNull() {
+        requestBody.put("Action", "AuthorizeUser");
+
+        assertNull(ClientRequestParser.parse(httpRequest, requestBody));
+    }
+
+    @Test
+    public void parseTest_OtherAction_AuthorizationHeaderNull() {
+        requestBody.put("Action", "OtherAction");
+        FullHttpRequest fullHttpRequest
+                = new DefaultFullHttpRequest(HttpVersion.HTTP_1_1, HttpMethod.GET, "/");
+        fullHttpRequest.headers().add("XYZ", "");
+        when(httpRequest.headers()).thenReturn(fullHttpRequest.headers());
+
+        assertNull(ClientRequestParser.parse(httpRequest, requestBody));
+    }
+}
