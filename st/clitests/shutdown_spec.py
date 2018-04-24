471f58afbe2806124c0e8068be1bdbceddf248e2
Rajesh Nambiar <rajesh.nambiar@seagate.com>
2018-04-24 05:28:02 +0100

    COSTOR-16: Some changes for shutdown script
    When shutdown script is run, randomly the client gets 503(ServiceUnavailable)
    from nginx. nginx fails to connect to s3server. Increased max_fails
    option in nginx to 3
    
    Change-Id: Ife0c085148ab8b663778490cf639b7422ebfa1bb

--- st/clitests/shutdown_spec.py
+++ st/clitests/shutdown_spec.py
@@ -49,12 +49,14 @@ for i, type in enumerate(config_types):
     # *********** Inform S3 that shutdown tests are in progress ******
     S3fiTest('s3cmd enable FI shutdown_system_tests_in_progress').\
         enable_fi("enable", "always", "shutdown_system_tests_in_progress").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     # ************ PUT/GET OBJECT TEST ***********
     S3fiTest('s3cmd enable FI put_object_action_fetch_bucket_info_shutdown_fail').\
         enable_fi("enable", "always", "put_object_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not upload 4MB file').\
         upload_test("seagatebucket", "4MBfile", 4000000).\
         execute_test(negative_case=True).command_should_fail().\
@@ -68,7 +70,8 @@ for i, type in enumerate(config_types):
 
     S3fiTest('s3cmd enable FI put_object_action_create_object_shutdown_fail').\
         enable_fi("enable", "always", "put_object_action_create_object_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not upload 4MB file').\
         upload_test("seagatebucket", "4MBfile", 4000000).\
         execute_test(negative_case=True).command_should_fail().\
@@ -82,7 +85,8 @@ for i, type in enumerate(config_types):
 
     S3fiTest('s3cmd enable FI put_object_action_consume_incoming_content_shutdown_fail').\
         enable_fi_enablen("enable", "put_object_action_consume_incoming_content_shutdown_fail", "2").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not upload 4MB file').\
         upload_test("seagatebucket", "4MBfile", 4000000).\
         execute_test(negative_case=True).command_should_fail().\
@@ -96,7 +100,8 @@ for i, type in enumerate(config_types):
 
     S3fiTest('s3cmd enable FI put_object_action_save_metadata_pass').\
         enable_fi("enable", "always", "put_object_action_save_metadata_pass").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can upload 4MB file').\
         upload_test("seagatebucket", "4MBfile", 4000000).\
         execute_test().command_is_successful()
@@ -109,7 +114,8 @@ for i, type in enumerate(config_types):
 
     S3fiTest('s3cmd enable FI get_object_action_fetch_bucket_info_shutdown_fail').\
         enable_fi("enable", "always", "get_object_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not download 4MB file').\
         download_test("seagatebucket", "4MBfile").\
         execute_test(negative_case=True).command_should_fail()
@@ -123,13 +129,15 @@ for i, type in enumerate(config_types):
     # ************ PUT/GET BUCKET TEST ***********
     S3fiTest('s3cmd enable FI put_bucket_action_validate_request_shutdown_fail').\
         enable_fi("enable", "always", "put_bucket_action_validate_request_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not create bucket').create_bucket("seagate_bucket").\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("put_bucket_action_validate_request_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd should not list bucket').list_buckets().execute_test().\
         command_is_successful().\
         command_response_should_not_have('s3://seagate_bucket')
@@ -142,7 +150,8 @@ for i, type in enumerate(config_types):
         execute_test().command_is_successful()
     S3fiTest('s3cmd enable FI get_bucket_action_get_next_objects_shutdown_fail').\
         enable_fi("enable", "always", "get_bucket_action_get_next_objects_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not list objects').list_objects('seagatebucket').\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
@@ -158,65 +167,75 @@ for i, type in enumerate(config_types):
 
     S3fiTest('s3cmd enable FI get_service_action_get_next_buckets_shutdown_fail').\
         enable_fi("enable", "always", "get_service_action_get_next_buckets_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not list buckets').list_buckets().\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("get_service_action_get_next_buckets_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     # ************ ACL/POLICY TEST ***************
     S3fiTest('s3cmd enable FI put_bucket_acl_action_fetch_bucket_info_shutdown_fail').\
         enable_fi("enable", "always", "put_bucket_acl_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not set acl on bucket').\
         setacl_bucket("seagatebucket","read:123").\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("put_bucket_acl_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     S3cmdTest('s3cmd can upload 3k file').\
         upload_test("seagatebucket", "3kfile", 3000).\
         execute_test().command_is_successful()
     S3fiTest('s3cmd enable FI put_object_acl_action_fetch_bucket_info_shutdown_fail').\
         enable_fi("enable", "always", "put_object_acl_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not set acl on object').\
         setacl_object("seagatebucket","3kfile", "read:123").\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("put_object_acl_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can delete 3kfile').\
         delete_test("seagatebucket", "3kfile").\
         execute_test().command_is_successful()
 
     S3fiTest('s3cmd enable FI put_bucket_policy_action_get_metadata_shutdown_fail').\
         enable_fi("enable", "always", "put_bucket_policy_action_get_metadata_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not set policy on bucket').\
         setpolicy_bucket("seagatebucket","policy.txt").\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("put_bucket_policy_action_get_metadata_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     # ************ MULTI-PART TEST ***************
     S3fiTest('s3cmd enable FI post_multipartobject_action_create_object_shutdown_fail').\
         enable_fi("enable", "always", "post_multipartobject_action_create_object_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not upload 18MB file').\
         upload_test("seagatebucket", "18MBfile1", 18000000).\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("post_multipartobject_action_create_object_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     # S3fiTest('s3cmd enable FI post_multipartobject_action_save_upload_metadata_shutdown_fail').\
     #    enable_fi("enable", "always", "post_multipartobject_action_save_upload_metadata_shutdown_fail").\
@@ -231,53 +250,61 @@ for i, type in enumerate(config_types):
 
     S3fiTest('s3cmd enable FI put_multiobject_action_fetch_bucket_info_shutdown_fail').\
         enable_fi("enable", "always", "put_multiobject_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not upload 19MB file').\
         upload_test("seagatebucket", "19MBfile", 19000000).\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("put_multiobject_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     S3fiTest('s3cmd enable FI put_multiobject_action_consume_incoming_content_shutdown_fail').\
         enable_fi_enablen("enable", "put_multiobject_action_consume_incoming_content_shutdown_fail", "2").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     S3cmdTest('s3cmd can not upload 17MB file').\
         upload_test("seagatebucket", "17MBfile", 17000000).\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("put_multiobject_action_consume_incoming_content_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     # ************ CHUNK UPLOAD TEST ***************
     S3fiTest('s3cmd enable FI put_multiobject_action_fetch_bucket_info_shutdown_fail').\
         enable_fi("enable", "always", "put_multiobject_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     JClientTest('Jclient can not upload 18MB file (multipart) in chunked mode').\
         put_object_multipart("seagatebucket", "18MBfilec", 18000000, 15, chunked=True).\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("put_multiobject_action_fetch_bucket_info_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     S3fiTest('s3cmd enable FI put_chunk_upload_object_action_consume_incoming_content_shutdown_fail').\
         enable_fi_enablen("enable", "put_chunk_upload_object_action_consume_incoming_content_shutdown_fail", "2").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
     JClientTest('Jclient can not upload 4MB file in chunked mode').\
         put_object("seagatebucket", "4MBfilec", 4000000, chunked=True).\
         execute_test(negative_case=True).command_should_fail().\
         command_error_should_have("ServiceUnavailable")
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("put_chunk_upload_object_action_consume_incoming_content_shutdown_fail").\
-        execute_test().command_is_successful()
+        execute_test().command_is_successful().\
+        command_response_should_not_have('ServiceUnavailable')
 
     # *********** Done with shutdown tests, Inform S3 ********
     S3fiTest('s3cmd can disable Fault injection').\
         disable_fi("shutdown_system_tests_in_progress").execute_test()\
-        .command_is_successful()
+        .command_is_successful().command_response_should_not_have('ServiceUnavailable')
 
     # ************ Delete bucket TEST ************
     S3cmdTest('s3cmd can delete bucket').delete_bucket("seagatebucket").\
