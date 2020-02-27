d688e76670412c10a39dc30c90fc1cb7a37cec92
Shalaka <shalaka.dharap@seagate.com>
2020-02-27 16:08:01 +0530

    EOS5611 - IEM alerts integration with S3 server
    Change-Id: Ia40248b426fe9cca61b1f38288fd7286428d5c50

--- auth/server/src/main/java/com/seagates3/util/IEMUtil.java
+++ auth/server/src/main/java/com/seagates3/util/IEMUtil.java
@@ -19,80 +19,73 @@
 
 package com.seagates3.util;
 
-import com.seagates3.authserver.AuthServerConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.lang.management.ManagementFactory;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Date;
-
 public class IEMUtil {
 
-    public enum Level {INFO, ERROR, WARN}
-    private static Logger logger = LoggerFactory.getLogger(BinaryUtil.class.getName());
+  public
+   enum Level {
+     INFO,
+     ERROR,
+     WARN
+   } private static Logger logger =
+       LoggerFactory.getLogger(BinaryUtil.class.getName());
 
-    public static final String LDAP_EX = "048001001";
-    public static final String UTF8_UNAVAILABLE = "048002001";
-    public static final String HMACSHA256_UNAVAILABLE = "048002002";
-    public static final String HMACSHA1_UNAVAILABLE = "048002003";
-    public static final String SHA256_UNAVAILABLE = "048002004";
-    public static final String UNPARSABLE_DATE = "048003001";
-    public static final String CLASS_NOT_FOUND_EX = "048004001";
-    public static final String NO_SUCH_METHOD_EX = "048004002";
-    public static final String XML_SCHEMA_VALIDATION_ERROR = "048005001";
+  public
+   static final String LDAP_EX = "0040010001";
+  public
+   static final String UTF8_UNAVAILABLE = "0040020001";
+  public
+   static final String HMACSHA256_UNAVAILABLE = "0040020002";
+  public
+   static final String HMACSHA1_UNAVAILABLE = "0040020003";
+  public
+   static final String SHA256_UNAVAILABLE = "0040020004";
+  public
+   static final String UNPARSABLE_DATE = "0040030001";
+  public
+   static final String CLASS_NOT_FOUND_EX = "0040040001";
+  public
+   static final String NO_SUCH_METHOD_EX = "0040040002";
+  public
+   static final String XML_SCHEMA_VALIDATION_ERROR = "0040050001";
+  public
+   static final String INVALID_REST_URI_ERROR = "0040060001";
 
-    public static final String INVALID_REST_URI_ERROR = "048006001";
-
-
-    public static void log(Level level, String eventCode,
-                           String eventCodeString, String data) {
-        String iem = generateIemString(eventCode, eventCodeString, data);
+  public
+   static void log(Level level, String eventCode, String eventCodeString,
+                   String data) {
+     String iem = generateIemString(eventCode, eventCodeString, level);
         switch (level) {
-            case ERROR:
-                logger.error(iem);
-                break;
-            case WARN:
-                logger.warn(iem);
-                break;
-            case INFO:
-                logger.info(iem);
+          case ERROR:
+            logger.error(iem);
+            break;
+          case WARN:
+            logger.warn(iem);
+            break;
+          case INFO:
+            logger.info(iem);
         }
     }
 
-    private static String generateIemString(String eventCode, String eventCodeString,
-                                            String data) {
-        String node = String.format("\"node\": \"%s\"", getHostName());
-        String time = String.format("\"time\": \"%s\"", new Date().toString());
-        String jvm = ManagementFactory.getRuntimeMXBean().getName();
-        String pid = String.format("\"pid\": %s", jvm.substring(0, jvm.indexOf("@")));
-        String jsonData = String.format("{ %s, %s, %s, %s }", time, node, pid, getLocation());
-        if (data != null && !data.isEmpty()) {
-            jsonData = String.format("{ %s, %s, %s, %s, %s }", time, node, pid, getLocation(), data);
+   private
+    static String generateIemString(String eventCode, String eventCodeString,
+                                    Level level) {
+      String severity = null;
+      switch (level) {
+        case ERROR:
+          severity = "E";
+          break;
+        case WARN:
+          severity = "W";
+          break;
+        case INFO:
+          severity = "I";
         }
-        String iem = String.format("IEC: %s: %s: %s", eventCode, eventCodeString, jsonData);
-
+        String iem =
+            String.format("IEC:%sS%s:%s", severity, eventCode, eventCodeString);
         return iem;
     }
-
-    private static String getHostName() {
-        String hostname = "";
-
-        try {
-            hostname = InetAddress.getLocalHost().getHostName();
-        } catch (UnknownHostException e) {
-            logger.info("Exception occurred while retrieving hostname.");
-        }
-
-        return hostname;
-    }
-
-    private static String getLocation() {
-        StackTraceElement traceElement = Thread.currentThread().getStackTrace()[4];
-        String location = String.format("\"file\": \"%s\", \"line\": %d",
-                traceElement.getFileName(), traceElement.getLineNumber());
-
-        return location;
-    }
 }
+
--- server/s3_iem.h
+++ server/s3_iem.h
@@ -33,55 +33,55 @@ extern S3Option *g_option_instance;
 // 2. For each IEM: timestamp, nodename, pid, filename & line is included in
 //    the JSON data format by default. If IEM event has any additional data to
 //    send, then pass it as `json_fmt` param.
-#define s3_iem(loglevel, event_code, event_description, json_fmt, ...)         \
-  do {                                                                         \
-    std::string timestamp = s3_get_timestamp();                                \
-    s3_syslog(loglevel, "IEC: " event_code ": " event_description              \
-                        ": { \"time\": \"%s\", \"node\": \"%s\", \"pid\": "    \
-                        "%d, \"file\": \"%s\", \"line\": %d" json_fmt " }",    \
-              timestamp.c_str(), g_option_instance->get_s3_nodename().c_str(), \
-              getpid(), __FILE__, __LINE__, ##__VA_ARGS__);                    \
-    s3_log(S3_LOG_INFO, "",                                                    \
-           "IEC: " event_code ": " event_description                           \
-           ": { \"time\": \"%s\", \"node\": \"%s\", \"pid\": "                 \
-           "%d, \"file\": \"%s\", \"line\": %d" json_fmt " }",                 \
-           timestamp.c_str(), g_option_instance->get_s3_nodename().c_str(),    \
-           getpid(), __FILE__, __LINE__, ##__VA_ARGS__);                       \
+#define s3_iem(loglevel, event_code, event_description, json_fmt, ...)      \
+  do {                                                                      \
+    std::string timestamp = s3_get_timestamp();                             \
+    s3_log(S3_LOG_INFO, "",                                                 \
+           "IEC: " event_code ": " event_description                        \
+           ": { \"time\": \"%s\", \"node\": \"%s\", \"pid\": "              \
+           "%d, \"file\": \"%s\", \"line\": %d" json_fmt " }",              \
+           timestamp.c_str(), g_option_instance->get_s3_nodename().c_str(), \
+           getpid(), __FILE__, __LINE__, ##__VA_ARGS__);                    \
+    if (loglevel == LOG_ALERT) {                                            \
+      s3_syslog(loglevel, "IEC:AS" event_code ":" event_description);       \
+    } else {                                                                \
+      s3_syslog(LOG_ERR, "IEC:ES" event_code ":" event_description);        \
+    }                                                                       \
   } while (0)
 
 // IEM helper macros
 // S3 Server IEM Event codes, description string & json data format
-#define S3_IEM_AUTH_CONN_FAIL "047001001"
+#define S3_IEM_AUTH_CONN_FAIL "0030010001"
 #define S3_IEM_AUTH_CONN_FAIL_STR "Auth server connection failed"
 #define S3_IEM_AUTH_CONN_FAIL_JSON ""
 
-#define S3_IEM_CHUNK_PARSING_FAIL "047002001"
+#define S3_IEM_CHUNK_PARSING_FAIL "0030020001"
 #define S3_IEM_CHUNK_PARSING_FAIL_STR "Chunk parsing failed"
 #define S3_IEM_CHUNK_PARSING_FAIL_JSON ""
 
-#define S3_IEM_CLOVIS_CONN_FAIL "047003001"
+#define S3_IEM_CLOVIS_CONN_FAIL "0030030001"
 #define S3_IEM_CLOVIS_CONN_FAIL_STR "Clovis connection failed"
 #define S3_IEM_CLOVIS_CONN_FAIL_JSON ""
 
-#define S3_IEM_COLLISION_RES_FAIL "047004001"
+#define S3_IEM_COLLISION_RES_FAIL "0030040001"
 #define S3_IEM_COLLISION_RES_FAIL_STR "Collision resolution failed"
 #define S3_IEM_COLLISION_RES_FAIL_JSON ""
 
-#define S3_IEM_FATAL_HANDLER "047005001"
+#define S3_IEM_FATAL_HANDLER "0030050001"
 #define S3_IEM_FATAL_HANDLER_STR "Fatal handler triggered"
 #define S3_IEM_FATAL_HANDLER_JSON ", \"signal_number\": %d"
 
-#define S3_IEM_DELETE_OBJ_FAIL "047006001"
+#define S3_IEM_DELETE_OBJ_FAIL "0030060001"
 #define S3_IEM_DELETE_OBJ_FAIL_STR \
   "Delete object failed causing stale data in Mero"
 #define S3_IEM_DELETE_OBJ_FAIL_JSON ""
 
-#define S3_IEM_DELETE_IDX_FAIL "047006002"
+#define S3_IEM_DELETE_IDX_FAIL "0030060002"
 #define S3_IEM_DELETE_IDX_FAIL_STR \
   "Delete index failed causing stale data in Mero"
 #define S3_IEM_DELETE_IDX_FAIL_JSON ""
 
-#define S3_IEM_METADATA_CORRUPTED "047006003"
+#define S3_IEM_METADATA_CORRUPTED "0030060003"
 #define S3_IEM_METADATA_CORRUPTED_STR "Metadata corrupted"
 #define S3_IEM_METADATA_CORRUPTED_JSON ""
 
