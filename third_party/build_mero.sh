d3a417bfab0aed95bcbe31cee51d2d1f0855cc41
Amit Kumar <amit.kumar@seagate.com>
2020-04-17 00:45:03 -0600

    Bump mero version in thirdparty to commit id: ec8372ff2814f32e0653492502c23f4702cab2f3
    Recent mero crash fix: http://gerrit.mero.colo.seagate.com/c/mero/+/19276
    
    Change-Id: I9a6cbd6dd41aea333f9bc584d65677985fd5e9f9

--- third_party/build_mero.sh
+++ third_party/build_mero.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 # Script to build mero.
 # git repo: http://es-gerrit.xyus.xyratex.com:8080/mero
-# branch: master commit: 8e0c53e8e3725981f1176c8bd74bf70997bab0d8
+# branch: master commit: ec8372ff2814f32e0653492502c23f4702cab2f3
 
 cd mero
 # Uncomment following line to compile mero with both KVS and Cassandra
