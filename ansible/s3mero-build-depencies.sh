5f75119bd6db64981297199cedb9d17ab6e38cc0
Sachitanand Shelake <65209830+sachitanands@users.noreply.github.com>
2020-07-09 10:56:03 +0530

    EOS-10289:S3: replace gerrit clone with git clone (#41)
    * EOS-10289:S3: replace gerrit clone with git clone
    
    * modified approach for checking motr script

--- ansible/s3mero-build-depencies.sh
+++ ansible/s3mero-build-depencies.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 set -x
-git clone http://gerrit.mero.colo.seagate.com/mero
-./mero/scripts/install-build-deps
-rm -rf mero
+
+SCRIPT_PATH=$(readlink -f "$0")
+SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
+
+motr_script_path=$SCRIPT_DIR/../third_party/mero/scripts
+if [[ -d "$motr_script_path" && -f "$motr_script_path/install-build-deps" ]];
+then
+    sh $motr_script_path/install-build-deps
+else
+    # third_party submodules are not loaded yet hence do fresh clone and
+    # run install-build-deps script.
+    # This needs GitHub id and Personal Access Token
+    git clone https://github.com/Seagate/cortx-motr.git
+    ./cortx-motr/scripts/install-build-deps
+    rm -rf cortx-motr
+fi
