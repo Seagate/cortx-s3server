d7717f9222c046355f5a7a74d7d2771f180eeb34
Amit Kumar <amit.kumar@seagate.com>
2020-07-09 00:27:08 -0600

    Fixed run_all_ut script to run UTs in jenkins build.
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>

--- s3recovery/scripts/run_all_ut.sh
+++ s3recovery/scripts/run_all_ut.sh
@@ -1,4 +1,21 @@
 #!/bin/sh
+# COPYRIGHT 2020 SEAGATE LLC
+
+# THIS DRAWING/DOCUMENT, ITS SPECIFICATIONS, AND THE DATA CONTAINED
+# HEREIN, ARE THE EXCLUSIVE PROPERTY OF SEAGATE TECHNOLOGY
+# LIMITED, ISSUED IN STRICT CONFIDENCE AND SHALL NOT, WITHOUT
+# THE PRIOR WRITTEN PERMISSION OF SEAGATE TECHNOLOGY LIMITED,
+# BE REPRODUCED, COPIED, OR DISCLOSED TO A THIRD PARTY, OR
+# USED FOR ANY PURPOSE WHATSOEVER, OR STORED IN A RETRIEVAL SYSTEM
+# EXCEPT AS ALLOWED BY THE TERMS OF SEAGATE LICENSES AND AGREEMENTS.
+
+# YOU SHOULD HAVE RECEIVED A COPY OF SEAGATE'S LICENSE ALONG WITH
+# THIS RELEASE. IF NOT PLEASE CONTACT A SEAGATE REPRESENTATIVE
+# http://www.seagate.com/contact
+
+# Original author: Amit Kumar  <amit.kumar@seagate.com>
+# Original creation date: 08-July-2020
+
 #Script to run UT's of s3recovery tool
 set -e
 
@@ -18,12 +35,10 @@ trap 'abort' 0
 printf "\nRunning s3recovery tool UT's...\n"
 
 SCRIPT_PATH=$(readlink -f "$0")
-printf $SCRIPT_PATH
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
-printf $SCRIPT_DIR
-#Update python path to source modules and run unit tests.
 
-PYTHONPATH=${PYTHONPATH}:${SCRIPT_DIR}/.. python36 -m unittest ${SCRIPT_DIR}/../ut/*.py -v
+#Update python path to source modules and run unit tests.
+PYTHONPATH=${PYTHONPATH}:${SCRIPT_DIR}/.. python36 -m pytest $SCRIPT_DIR/../ut/*.py -v
 
 echo "s3recovery tool UT's runs successfully"
 
