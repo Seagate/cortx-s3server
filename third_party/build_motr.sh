38bf2219125874ee69f646579370ffd8b5a58aae
Sachitanand Shelake <sachitanand.shelake@seagate.com>
2020-07-14 03:40:40 -0600

    jenkins-build failure fix

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -3,7 +3,7 @@
 # github repo: https://github.com/Seagate/cortx-motr
 # branch: master commit: a03ba35788f5650678e1086ce8c1edf0644f20c1
 
-cd mero
+cd motr
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 ./scripts/m0 rebuild
