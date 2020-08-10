5124d6022bee5a59c3e237363d0ed1535b2a0338
Nilesh Govande <nilesh.govande@seagate.com>
2020-08-10 04:48:17 -0600

    Moved motr to f0857d97ff9190de4a055781d9ec70ee52c62ef9
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
-# branch: master commit: 320fafb6f63f4d78cbee8c35fd2b0f5f2e087a23
+# branch: master commit: f0857d97ff9190de4a055781d9ec70ee52c62ef9
 
 cd motr
 # Uncomment following line to compile motr with both KVS and Cassandra
