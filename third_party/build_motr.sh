067826984f18496babf450f8d75f0fe40f969763
Sachitanand Shelake <sachitanand.shelake@seagate.com>
2020-08-18 05:49:45 -0600

    Bump motr version with clovis rename commit id
    Signed-off-by: Sachitanand Shelake <sachitanand.shelake@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
-# branch: master commit: f0857d97ff9190de4a055781d9ec70ee52c62ef9
+# branch: master commit: 798d0732e9e0f5d6df5fb4be6c923ef2c6e12cac
 
 cd motr
 # Uncomment following line to compile motr with both KVS and Cassandra
