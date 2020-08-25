ec5bd130ef9127ba2197a06d6bf626b328f7cf28
Siddhivinayak Shanbhag <siddhivinayak.shanbhag@seagate.com>
2020-08-25 01:18:50 -0600

    Copyright fix

--- s3backgrounddelete/scripts/delete_kv.py
+++ s3backgrounddelete/scripts/delete_kv.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3.6
 #
 # Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
 #
@@ -20,7 +21,6 @@
 
     python36 delete_kv.py index_id key it's like 
     python36 delete_kv.py sys.argv[1] sys.argv[2] """
-#!/usr/bin/python3.6
 import sys
 from s3backgrounddelete.cortx_s3_config import CORTXS3Config
 from s3backgrounddelete.cortx_s3_kv_api import CORTXS3KVApi
