83a7fbddea45e6a479226b55ad3762fe75cc0c5f
Siddhivinayak Shanbhag <siddhivinayak.shanbhag@seagate.com>
2020-09-07 03:42:35 -0600

    Bump motr

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
-# branch: master commit: 30cb30adf5f8d4aabbc3ba6b66b1f790f6718bfb
+# branch: master commit: 683b2c3b25489122829008bd54f4798fd8b3878a
 
 cd motr
 # Uncomment following line to compile motr with both KVS and Cassandra
