2254c410fa45378cd1d3fd408c27c773a6687fd2
shalakadharap <64534897+shalakadharap@users.noreply.github.com>
2020-09-17 21:40:49 +0530

    [LDR R2] EOS-12882 - Script for reverting ldap sync replication (#190)
    * EOS12882-Script for reverting ldap sync replication
    
    by Shalaka Dharap <shalaka.dharap@seagate.com>
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>

--- scripts/ldap/replication/revertReplicationScript.sh
+++ scripts/ldap/replication/revertReplicationScript.sh
@@ -20,13 +20,10 @@
 ##################################
 # Configure replication 
 ##################################
-usage() { echo "Usage: [-h <provide file containing hostnames of nodes in cluster>],[-p <ldap admin password>]" 1>&2; exit 1; }
+usage() { echo "Usage: [-p <ldap admin password>]" 1>&2; exit 1; }
 
-while getopts ":h:p:" o; do
+while getopts ":p:" o; do
     case "${o}" in
-        h)
-            host_list=${OPTARG}
-            ;;
         p)
             password=${OPTARG}
             ;;
@@ -37,12 +34,12 @@ while getopts ":h:p:" o; do
 done
 shift $((OPTIND-1))
 
-if [ -z ${host_list} ] || [ -z ${password} ]
+if [ -z ${password} ]
 then
     usage
     exit 1
 fi
-
+host_list=/opt/seagate/cortx/s3/install/ldap/replication/host_list
 #Below function will check if all provided hosts are valid or not
 checkHostValidity()
 {
@@ -75,70 +72,41 @@ getServerIdWithSalt()
     read -ra ID <<< "$nodeId"
     id=${ID[1]}
 }
-#Below will save host list at /opt/seagate/cortx/s3/install/ldap/replication/ for future use in reverting replication
-saveHostList()
-{
-    while read host; do
-    echo $host >> /opt/seagate/cortx/s3/install/ldap/replication/host_list
-    done <$host_list
-}
 
+
+#olcServerId script
 checkHostValidity
-saveHostList
-#update serverID
 if hash salt 2>/dev/null; then
     getServerIdWithSalt
 else
     getServerIdFromHostFile
 fi
-sed -e "s/\${serverid}/$id/" serverIdTemplate.ldif > scriptServerId.ldif
-ldapmodify -Y EXTERNAL  -H ldapi:/// -f scriptServerId.ldif
-rm scriptServerId.ldif
+sed -e "s/\${serverid}/$id/" revertServerIdTemplate.ldif > scriptRevertServerId.ldif
+ldapmodify -Y EXTERNAL  -H ldapi:/// -f scriptRevertServerId.ldif
+rm scriptRevertServerId.ldif
 
-ldapadd -Y EXTERNAL -H ldapi:/// -f syncprov_mod.ldif
-
-ldapadd -Y EXTERNAL -H ldapi:/// -f syncprov.ldif
+rm -rf /etc/openldap/slapd.d/cn\=config/cn\=module{2}.ldif
+rm -rf /etc/openldap/slapd.d/cn\=config/olcDatabase\={2}mdb/olcOverlay\={2}syncprov.ldif
 
 #update replicaiton config
 
 rid=1
 while read host; do
-sed -e "s/\${rid}/$rid/" -e "s/\${provider}/$host/" -e "s/\${credentials}/$password/" configTemplate.ldif > scriptConfig.ldif
-if [ ${rid} -eq 2 ] && [ ${id} -eq 1 ]
-then
-    echo "-" >> scriptConfig.ldif
-    echo "add: olcMirrorMode" >> scriptConfig.ldif
-     echo "olcMirrorMode: TRUE" >> scriptConfig.ldif
-fi
-if [ ${rid} -eq 1 ] && [ ${id} -ne 1 ]
-then
-    echo "-" >> scriptConfig.ldif
-    echo "add: olcMirrorMode" >> scriptConfig.ldif
-    echo "olcMirrorMode: TRUE" >> scriptConfig.ldif
-fi
-ldapmodify -Y EXTERNAL  -H ldapi:/// -f scriptConfig.ldif
-rm scriptConfig.ldif
-rid=`expr ${rid} + 1`
+  sed -e "s/\${rid}/$rid/" -e "s/\${provider}/$host/" -e "s/\${credentials}/$password/" revertConfigTemplate.ldif > revertScriptConfig.ldif
+  ldapmodify -Y EXTERNAL  -H ldapi:/// -f revertScriptConfig.ldif
+  rm revertScriptConfig.ldif
+  rid=`expr ${rid} + 1`
 done <$host_list
-
+ldapmodify -Y EXTERNAL  -H ldapi:/// -f revertMirrorModeConfig.ldif
 iteration=1
 # Update mdb file
 while read host; do
-sed -e "s/\${rid}/$rid/" -e "s/\${provider}/$host/" -e "s/\${credentials}/$password/" dataTemplate.ldif > scriptData.ldif
-if [ ${iteration} -eq 2 ] && [ ${id} -eq 1 ]
-then
-    echo "-" >> scriptData.ldif
-    echo "add: olcMirrorMode" >> scriptData.ldif
-    echo "olcMirrorMode: TRUE" >> scriptData.ldif
-fi
-if [ ${iteration} -eq 1 ] && [ ${id} -ne 1 ]
-then
-    echo "-" >> scriptData.ldif
-    echo "add: olcMirrorMode" >> scriptData.ldif
-    echo "olcMirrorMode: TRUE" >> scriptData.ldif
-fi
-ldapmodify -Y EXTERNAL  -H ldapi:/// -f scriptData.ldif
-rm scriptData.ldif
-rid=`expr ${rid} + 1`
-iteration=`expr ${iteration} + 1`
+  sed -e "s/\${rid}/$rid/" -e "s/\${provider}/$host/" -e "s/\${credentials}/$password/" revertDataTemplate.ldif > revertScriptData.ldif
+  ldapmodify -Y EXTERNAL  -H ldapi:/// -f revertScriptData.ldif
+  rm revertScriptData.ldif
+  rid=`expr ${rid} + 1`
+  iteration=`expr ${iteration} + 1`
 done <$host_list
+ldapmodify -Y EXTERNAL  -H ldapi:/// -f revertMirrorModeData.ldif
+rm $host_list
+systemctl restart slapd
