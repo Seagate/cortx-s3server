9910570a98c06457adfb3112c15731e97153f6ed
Rajesh Nambiar <rajesh.nambiar@seagate.com>
2020-09-24 20:50:40 +0530

    EOS-13578: S3 log changes, so as to use less space (#230)
    Health check (HEAD request) comes frequently, optimized
    logging to take less log space. Did few other changes also
    relating to logging
    
    Signed-off-by: Rajesh Nambiar <rajesh.nambiar@seagate.com>

--- server/s3_bucket_action_base.cc
+++ server/s3_bucket_action_base.cc
@@ -45,7 +45,7 @@ S3BucketAction::~S3BucketAction() {
 }
 
 void S3BucketAction::fetch_bucket_info() {
-  s3_log(S3_LOG_INFO, request_id, "Fetching bucket metadata\n");
+  s3_log(S3_LOG_INFO, request_id, "Entering\n");
   if (s3_fi_is_enabled("fail_fetch_bucket_info")) {
     s3_fi_enable_once("motr_kv_get_fail");
   }
