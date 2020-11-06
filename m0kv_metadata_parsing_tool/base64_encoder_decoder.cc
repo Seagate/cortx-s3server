529f18a2cc42450c9f4567d00ff30c4a62967641
Saumitra Kulkarni <saumitra.kulkarni@seagate.com>
2020-11-05 21:55:51 -0700

    Added iterators name
    Signed-off-by: Saumitra Kulkarni <saumitra.kulkarni@seagate.com>

--- m0kv_metadata_parsing_tool/base64_encoder_decoder.cc
+++ m0kv_metadata_parsing_tool/base64_encoder_decoder.cc
@@ -54,41 +54,40 @@ Logic :
 
 std::string base64_decode(std::string const& encoded_string) {
   int in_len = encoded_string.size();
-  // These are the iterators to traverse through specific block from given string.
-  int i = 0;
+  int pos_iterator = 0; // This is postional iterator to traverse through string.
   int in_ = 0;
   unsigned char char_array_4[4], char_array_3[3];
   std::string ret;
 
   while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
-    char_array_4[i++] = encoded_string[in_]; in_++;
-    if (i ==4) {
-      for (i = 0; i <4; i++)
-        char_array_4[i] = base64_chars.find(char_array_4[i]);
+    char_array_4[pos_iterator++] = encoded_string[in_]; in_++;
+    if (pos_iterator ==4) {
+      for (pos_iterator = 0; pos_iterator <4; pos_iterator++)
+        char_array_4[pos_iterator] = base64_chars.find(char_array_4[pos_iterator]);
 
       char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
       char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
       char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-      for (i = 0; (i < 3); i++)
-        ret += char_array_3[i];
-      i = 0;
+      for (pos_iterator = 0; (pos_iterator < 3); pos_iterator++)
+        ret += char_array_3[pos_iterator];
+      pos_iterator = 0;
     }
   }
 
-  if (i) {
-    int j = 0;
-    for (j = i; j <4; j++)
-      char_array_4[j] = 0;
+  if (pos_iterator) {
+    int null_chars  = 0; // No. of zeroes to append if number of characters are less than 3 in char_array_3
+    for (null_chars = pos_iterator; null_chars <4; null_chars++)
+      char_array_4[null_chars] = 0;
 
-    for (j = 0; j <4; j++)
-      char_array_4[j] = base64_chars.find(char_array_4[j]);
+    for (null_chars = 0; null_chars <4; null_chars++)
+      char_array_4[null_chars] = base64_chars.find(char_array_4[null_chars]);
 
     char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
     char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
     char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
 
-    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+    for (null_chars = 0; (null_chars < pos_iterator - 1); null_chars++) ret += char_array_3[null_chars];
   }
   return ret;
 }
@@ -104,41 +103,40 @@ Logic :
 
 std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
   std::string ret;
-  // These are the iterators to traverse through specific block from given string.
-  int i = 0;
-  int j = 0;
   
+  int pos_iterator = 0; // This is postional iterator to traverse through string.
   unsigned char char_array_3[3];
   unsigned char char_array_4[4];
 
   while (in_len--) {
-    char_array_3[i++] = *(bytes_to_encode++);
-    if (i == 3) {
+    char_array_3[pos_iterator++] = *(bytes_to_encode++);
+    if (pos_iterator == 3) {
       char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
       char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
       char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
       char_array_4[3] = char_array_3[2] & 0x3f;
 
-      for(i = 0; (i <4) ; i++)
-        ret += base64_chars[char_array_4[i]];
-      i = 0;
+      for(pos_iterator = 0; (pos_iterator <4) ; pos_iterator++)
+        ret += base64_chars[char_array_4[pos_iterator]];
+      pos_iterator = 0;
     }
   }
 
-  if (i)
+  if (pos_iterator)
   {
-    for(j = i; j < 3; j++)
-      char_array_3[j] = '\0';
+    int null_chars  = 0; // No. null chars to append if number of characters are less than 4 in char_array_4
+    for(null_chars = pos_iterator; null_chars < 3; null_chars++)
+      char_array_3[null_chars] = '\0';
 
     char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
     char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
     char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
     char_array_4[3] = char_array_3[2] & 0x3f;
 
-    for (j = 0; (j < i + 1); j++)
-      ret += base64_chars[char_array_4[j]];
+    for (null_chars = 0; (null_chars < pos_iterator + 1); null_chars++)
+      ret += base64_chars[char_array_4[null_chars]];
 
-    while((i++ < 3))
+    while((pos_iterator++ < 3))
       ret += '=';
 
   }
