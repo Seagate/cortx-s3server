3f50fb9ca4deb61d02688e0b091457b444482eb2
Eugene Brazhnikov <evgeniy.brazhnikov@seagate.com>
2020-11-17 12:36:26 +0300

    EOS-14459: base64 refactoring (#378)
    * EOS-14459: draft
    
    Signed-off-by: evgeniy-brazhnikov <evgeniy.brazhnikov@seagate.com>
    
    * EOS-14459: update
    
    Signed-off-by: evgeniy-brazhnikov <evgeniy.brazhnikov@seagate.com>
    
    * EOS-14459: reduce tests
    
    Signed-off-by: evgeniy-brazhnikov <evgeniy.brazhnikov@seagate.com>
    
    Co-authored-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>

--- server/base64.cc
+++ server/base64.cc
@@ -18,99 +18,119 @@
  *
  */
 
+#include <cassert>
+#include <cctype>
+#include <string>
 #include "base64.h"
-#include <iostream>
 
-static const std::string base64_chars =
-             "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-             "abcdefghijklmnopqrstuvwxyz"
-             "0123456789+/";
+const char base64_chars[] =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    "abcdefghijklmnopqrstuvwxyz"
+    "0123456789+/";
 
+std::string base64_encode(unsigned char const* bytes_to_encode,
+                          unsigned in_len) {
 
-static inline bool is_base64(unsigned char c) {
-  return (isalnum(c) || (c == '+') || (c == '/'));
-}
-
-std::string base64_encode(unsigned char const* bytes_to_encode, unsigned int in_len) {
   std::string ret;
-  int i = 0;
-  int j = 0;
-  unsigned char char_array_3[3];
-  unsigned char char_array_4[4];
+  ret.reserve((in_len + 2) / 3 << 2);  // exact size
 
-  while (in_len--) {
-    char_array_3[i++] = *(bytes_to_encode++);
-    if (i == 3) {
-      char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-      char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-      char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-      char_array_4[3] = char_array_3[2] & 0x3f;
+  unsigned modulo_3 = 0;
+  int for_next = 0;  // -Wall
 
-      for(i = 0; (i <4) ; i++)
-        ret += base64_chars[char_array_4[i]];
-      i = 0;
+  for (unsigned i = 0; i < in_len; ++i) {
+    const int cur_ch = *(bytes_to_encode + i);
+
+    switch (modulo_3) {
+      case 0:
+        ret += base64_chars[cur_ch >> 2 & 0x3F];
+        for_next = (cur_ch & 3) << 4;
+        break;
+      case 1:
+        assert(!(for_next & ~0x30));
+        ret += base64_chars[for_next | (cur_ch >> 4 & 0xF)];
+        for_next = (cur_ch & 0x0F) << 2;
+        break;
+      case 2:
+        assert(!(for_next & ~0x3C));
+        ret += base64_chars[for_next | (cur_ch >> 6 & 3)];
+        ret += base64_chars[cur_ch & 0x3F];
+        break;
+      default:
+        assert(0);
+    }
+    if (++modulo_3 > 2) {
+      modulo_3 = 0;
     }
   }
+  assert(modulo_3 < 3);
 
-  if (i)
-  {
-    for(j = i; j < 3; j++)
-      char_array_3[j] = '\0';
+  if (modulo_3) {
+    assert(!(for_next & ~0x3F));
+    ret += base64_chars[for_next];
+    ret += '=';
 
-    char_array_4[0] = (char_array_3[0] & 0xfc) >> 2;
-    char_array_4[1] = ((char_array_3[0] & 0x03) << 4) + ((char_array_3[1] & 0xf0) >> 4);
-    char_array_4[2] = ((char_array_3[1] & 0x0f) << 2) + ((char_array_3[2] & 0xc0) >> 6);
-    char_array_4[3] = char_array_3[2] & 0x3f;
-
-    for (j = 0; (j < i + 1); j++)
-      ret += base64_chars[char_array_4[j]];
-
-    while((i++ < 3))
+    if (1 == modulo_3) {
       ret += '=';
-
-  }
-
-  return ret;
-
-}
-
-std::string base64_decode(std::string const& encoded_string) {
-  int in_len = encoded_string.size();
-  int i = 0;
-  int j = 0;
-  int in_ = 0;
-  unsigned char char_array_4[4], char_array_3[3];
-  std::string ret;
-
-  while (in_len-- && ( encoded_string[in_] != '=') && is_base64(encoded_string[in_])) {
-    char_array_4[i++] = encoded_string[in_]; in_++;
-    if (i ==4) {
-      for (i = 0; i <4; i++)
-        char_array_4[i] = base64_chars.find(char_array_4[i]);
-
-      char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-      char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-      char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
-
-      for (i = 0; (i < 3); i++)
-        ret += char_array_3[i];
-      i = 0;
     }
   }
+  return ret;
+}
 
-  if (i) {
-    for (j = i; j <4; j++)
-      char_array_4[j] = 0;
+std::string base64_decode(const std::string& encoded_string) {
 
-    for (j = 0; j <4; j++)
-      char_array_4[j] = base64_chars.find(char_array_4[j]);
+  std::string ret;
+  ret.reserve(((encoded_string.length() + 3) >> 2) * 3);  // roughly
 
-    char_array_3[0] = (char_array_4[0] << 2) + ((char_array_4[1] & 0x30) >> 4);
-    char_array_3[1] = ((char_array_4[1] & 0xf) << 4) + ((char_array_4[2] & 0x3c) >> 2);
-    char_array_3[2] = ((char_array_4[2] & 0x3) << 6) + char_array_4[3];
+  unsigned modulo_4 = 0;
+  int current_byte = 0;  // -Wall
 
-    for (j = 0; (j < i - 1); j++) ret += char_array_3[j];
+  for (const char ch : encoded_string) {
+
+    if (isspace(ch)) {
+      if (modulo_4) {
+        break;  // Bad input
+      } else {
+        continue;  // base64 text can be formatted
+      }
+    }
+    int decoded = 0;
+
+    if (ch >= 'A' && ch <= 'Z')
+      decoded = ch - 'A';
+    else if (ch >= 'a' && ch <= 'z')
+      decoded = ch - ('a' - 26);
+    else if (ch >= '0' && ch <= '9')
+      decoded = ch + (52 - '0');
+    else if ('+' == ch)
+      decoded = 62;
+    else if ('/' == ch)
+      decoded = 63;
+    else {
+      // Illegal base64 character
+      break;
+    }
+    switch (modulo_4) {
+      case 0:
+        current_byte = decoded << 2;
+        break;
+      case 1:
+        assert(!(current_byte & 3));
+        ret += static_cast<char>(current_byte | (decoded >> 4 & 3));
+        current_byte = decoded << 4;
+        break;
+      case 2:
+        assert(!(current_byte & 0x0F));
+        ret += static_cast<char>(current_byte | (decoded >> 2 & 0x0F));
+        current_byte = decoded << 6;
+        break;
+      case 3:
+        assert(!(current_byte & 0x3F));
+        ret += static_cast<char>(current_byte | (decoded & 0x3F));
+        break;
+      default:
+        assert(0);
+    }
+    if (++modulo_4 > 3) modulo_4 = 0;
   }
-
   return ret;
 }
---
+++ ut/base64_test.cc
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * For any questions about this software or licensing,
+ * please email opensource@seagate.com or cortx-questions@seagate.com.
+ *
+ */
+
+#include <memory>
+#include <string>
+#include "gtest/gtest.h"
+#include "base64.h"
+
+const unsigned char data[] = {'M', 'a', 'n'};
+
+TEST(Base64, Encode) {
+  EXPECT_TRUE(base64_encode(data, 3) == "TWFu");
+  EXPECT_TRUE(base64_encode(data, 2) == "TWE=");
+  EXPECT_TRUE(base64_encode(data, 1) == "TQ==");
+}
+
+TEST(Base64, Decode) {
+  EXPECT_TRUE(base64_decode("TWFu") == "Man");
+  EXPECT_TRUE(base64_decode("TWE=") == "Ma");
+  EXPECT_TRUE(base64_decode("TQ==") == "M");
+}
+
+const char encoded_test_text[] =
+    "TWFuIGlzIGRpc3Rpbmd1aXNoZWQsIG5vdCBvbmx5IGJ5IGhpcyByZWFzb24sIGJ1dCBieSB0"
+    "aGlzIHNpbmd1bGFyIHBhc3Npb24gZnJvbSBvdGhlciBhbmltYWxzLCB3aGljaCBpcyBhIGx1"
+    "c3Qgb2YgdGhlIG1pbmQsIHRoYXQgYnkgYSBwZXJzZXZlcmFuY2Ugb2YgZGVsaWdodCBpbiB0"
+    "aGUgY29udGludWVkIGFuZCBpbmRlZmF0aWdhYmxlIGdlbmVyYXRpb24gb2Yga25vd2xlZGdl"
+    "LCBleGNlZWRzIHRoZSBzaG9ydCB2ZWhlbWVuY2Ugb2YgYW55IGNhcm5hbCBwbGVhc3VyZS4=";
+
+const char decoded_test_text[] =
+    "Man is distinguished, not only by his reason, but by this singular "
+    "passion from other animals, which is a lust of the mind, that by a "
+    "perseverance of delight in the continued and indefatigable generation "
+    "of knowledge, exceeds the short vehemence of any carnal pleasure.";
+
+TEST(Base64, Gobbs) {
+  EXPECT_TRUE(base64_encode((const unsigned char *)decoded_test_text,
+                            sizeof(decoded_test_text) - 1) ==
+              encoded_test_text);
+  EXPECT_TRUE(base64_decode(encoded_test_text) == decoded_test_text);
+}
+
+const char triple_ascii_encoded[] =
+    "AAECAwQFBgcICQoLDA0ODxAREhMUFRYXGBkaGxwdHh8gISIjJCUmJygpKissLS4v"
+    "MDEyMzQ1Njc4OTo7PD0+P0BBQkNERUZHSElKS0xNTk9QUVJTVFVWV1hZWltcXV5f"
+    "YGFiY2RlZmdoaWprbG1ub3BxcnN0dXZ3eHl6e3x9fn+AgYKDhIWGh4iJiouMjY6P"
+    "kJGSk5SVlpeYmZqbnJ2en6ChoqOkpaanqKmqq6ytrq+wsbKztLW2t7i5uru8vb6/"
+    "wMHCw8TFxsfIycrLzM3Oz9DR0tPU1dbX2Nna29zd3t/g4eLj5OXm5+jp6uvs7e7v"
+    "8PHy8/T19vf4+fr7/P3+/wABAgMEBQYHCAkKCwwNDg8QERITFBUWFxgZGhscHR4f"
+    "ICEiIyQlJicoKSorLC0uLzAxMjM0NTY3ODk6Ozw9Pj9AQUJDREVGR0hJSktMTU5P"
+    "UFFSU1RVVldYWVpbXF1eX2BhYmNkZWZnaGlqa2xtbm9wcXJzdHV2d3h5ent8fX5/"
+    "gIGCg4SFhoeIiYqLjI2Oj5CRkpOUlZaXmJmam5ydnp+goaKjpKWmp6ipqqusra6v"
+    "sLGys7S1tre4ubq7vL2+v8DBwsPExcbHyMnKy8zNzs/Q0dLT1NXW19jZ2tvc3d7f"
+    "4OHi4+Tl5ufo6err7O3u7/Dx8vP09fb3+Pn6+/z9/v8AAQIDBAUGBwgJCgsMDQ4P"
+    "EBESExQVFhcYGRobHB0eHyAhIiMkJSYnKCkqKywtLi8wMTIzNDU2Nzg5Ojs8PT4/"
+    "QEFCQ0RFRkdISUpLTE1OT1BRUlNUVVZXWFlaW1xdXl9gYWJjZGVmZ2hpamtsbW5v"
+    "cHFyc3R1dnd4eXp7fH1+f4CBgoOEhYaHiImKi4yNjo+QkZKTlJWWl5iZmpucnZ6f"
+    "oKGio6SlpqeoqaqrrK2ur7CxsrO0tba3uLm6u7y9vr/AwcLDxMXGx8jJysvMzc7P"
+    "0NHS09TV1tfY2drb3N3e3+Dh4uPk5ebn6Onq6+zt7u/w8fLz9PX29/j5+vv8/f7/";
+
+TEST(Base64, TripleAscii) {
+  enum {
+    BUFFER_SIZE = 768
+  };
+  std::unique_ptr<unsigned char[]> buffer(new unsigned char[BUFFER_SIZE]);
+
+  for (unsigned ch = 0; ch < BUFFER_SIZE; ++ch) {
+    buffer[ch] = ch;
+  }
+  std::string s_encoded = base64_encode(buffer.get(), BUFFER_SIZE);
+  const auto encoded_length = s_encoded.length();
+
+  EXPECT_EQ(encoded_length, sizeof(triple_ascii_encoded) - 1);
+  EXPECT_FALSE(memcmp(s_encoded.c_str(), triple_ascii_encoded, encoded_length));
+
+  std::string s_decoded = base64_decode(s_encoded);
+
+  EXPECT_EQ(s_decoded.length(), BUFFER_SIZE);
+  EXPECT_FALSE(memcmp(s_decoded.c_str(), buffer.get(), BUFFER_SIZE));
+}
