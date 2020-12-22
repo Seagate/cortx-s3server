e8661dfde7649d75f5a363db1d0854d731056cf2
pratyush-seagate <pratyush.k.khan@seagate.com>
2020-12-22 06:30:50 -0700

    EOS-15862 [MiniProvisioner] Add s3confstore to use cortx-utils::conrtx.conf 	Integrate conf_store load() procedure from py-utils package with s3confstore utility 	modified:   s3cortxutils/s3confstore/s3confstore/cortx_s3_confstore.py 	Test: 		1. Pass invalid file url and file with invalid json 		2. Use a temporary unit test named "test_load" to verify the integrated API is working as expected. This will be removed later to ut. 	Prerequisites: 		1. cortx-py-utils-1.0.0-<>_<>.noarch.rpm needs to be installed externally before compile and run this change (from main of cortx-utils repo). 	Test: 		# /usr/local/bin/s3confstore --load "my-index-name" --path "JSON:///etc/myconf.json" 			constructor() 			in subroutine load() 			load conf: JSON:///etc/myconf.json into index: my-index-name 			test load conf: JSON:///etc/myconf.json into index: my-index-name
    		# /usr/local/bin/s3confstore --test_load "my-index-name" --path "JSON:///etc/myconf.json"
    			constructor()
    			in subroutine test_load()
    			test_load passed!
    			test load conf: JSON:///etc/myconf.json into index: my-index-name
    
    Signed-off-by: pratyush-seagate <pratyush.k.khan@seagate.com>

--- s3cortxutils/s3confstore/s3confstore/cortx_s3_confstore.py
+++ s3cortxutils/s3confstore/s3confstore/cortx_s3_confstore.py
@@ -18,25 +18,68 @@
 #
 import argparse
 # from cortx.utils.conf_store import Conf
+from cortx.utils.conf_store import ConfStore
+from urllib.parse import urlparse
+import os.path
+import json
+
+conf_store = ConfStore()
+
+def load_config(index, backend_url):
+  """Instantiate and Load Config into constore"""
+  conf_store.load(index, backend_url)
+  return conf_store
+
 
 class S3CortxConfStore:
 
   def __init__(self):
     print("constructor()")
 
-  def load(self):
+  def load(self, index, path):
     print("in subroutine load()")
+    result_data = load_config(index, path)
+
+  def test_load(self, index, path):
+    """This will be removed to ut directory later"""
+    print("in subroutine test_load()")
+    with open(urlparse(path).path) as f:
+      jdata = json.load(f)
+    load_config(index, path)
+    result_data = conf_store.get(index, next(iter(jdata)), default_val=None)
+    if result_data != None:
+      print("test_load passed!")
+    else:
+      print("test_load failed!")
 
   def run(self):
     parser = argparse.ArgumentParser(description='Cortx-Utils ConfStore')
     parser.add_argument("--load", help='load the backing storage in an index')
+    parser.add_argument("--test_load", help='test load the backing storage in an index')
     parser.add_argument("--path", help='example: JSON:///etc/cortx/myconf.json')
     args = parser.parse_args()
 
-    if args.load and args.path:
-      index = args.load
+    if args.path:
+      if os.path.isfile(urlparse(args.path).path) != True:
+        print("config file: {} does not exist".format(args.path))
+        exit(-1)
+      else:
+        try:
+          with open(urlparse(args.path).path) as f:
+            json.load(f)
+        except ValueError as e:
+          print("config file: {} must use valid JSON format: {}".format(urlparse(args.path).path, e))
+          exit(-1)
+
+    if args.load or args.test_load:
       path = args.path
-      print("load conf: {} into index: {}".format(path, index))
-      self.load()
+      if args.load:
+        index = args.load
+        self.load(index, path)
+        print("load conf: {} into index: {}".format(path, index))
+      else:
+        index = args.test_load
+        self.test_load(index, path)
+        print("test load conf: {} into index: {}".format(path, index))
     else: 
       pass
