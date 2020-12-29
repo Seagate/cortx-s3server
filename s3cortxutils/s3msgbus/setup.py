5d0de8fde4a84d49a0171f7d0c9c0f61cd845c4f
nileshgovande <63101371+nileshgovande@users.noreply.github.com>
2020-12-29 15:23:40 +0530

    EOS-15684:S3 Message Bus Wrapper Library (#584)
    Co-authored-by: Atita Shirwaikar <atita.shirwaikar@seagate.com>

---
+++ s3cortxutils/s3msgbus/setup.py
@@ -0,0 +1,53 @@
+#
+# Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+import os
+from setuptools import setup
+files = ["config/*","VERSION"]
+
+# Load the version
+s3msgbus_version = "1.0.0"
+current_script_path = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(current_script_path, 'VERSION')) as version_file:
+    s3msgbus_version = version_file.read().strip()
+
+setup(
+  # Application name
+  name="s3msgbus",
+
+  # version number
+  version=s3msgbus_version,
+
+  # Author details
+  author="Seagate",
+
+  # Packages
+  packages=["s3msgbus"],
+
+  # Include additional files into the package
+  include_package_data=True,
+
+  # Details
+  scripts =['s3msgbus/s3msgbus'],
+
+  # license="LICENSE.txt",
+
+  description="Wrapper for Message Bus Interface cortx-utils::MsgBus",
+
+  package_data = { 's3msgbus': files}
+)
