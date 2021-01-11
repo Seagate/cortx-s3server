f0153417a02fb776ceab6e49f7ece73523fe14e5
pratyush-seagate <pratyush.k.khan@seagate.com>
2021-01-11 00:41:41 -0700

    EOS-16257: Build s3confstore and run UTs with jenkins build job         Change description: 		Added build step in rebuildall.sh script (just like s3recovery and s3backgrounddelete) for s3confstore. 		Added UT for s3confstore and made sure they run as part of jenkins job via runalltest.sh         List of files: 		modified:   rebuildall.sh 		modified:   runalltest.sh 		new file:   s3cortxutils/s3confstore/scripts/run_all_ut.sh 		new file:   s3cortxutils/s3confstore/ut/s3confstore_unit_tests.py         Testing:         UT:                 1. using rebuildall.sh, using console logs, verified s3confstore is being compiled. 		2. using ./jenkins-build.sh and ./runalltest.sh, verified s3confstore_unit_tests.py is being executed successfully.                 Note: pip3 install  ./cortx_py_utils-1.0.0-py3-none-any.whl needed to install cortx-py-utils before running s3confstore unit test.
    Signed-off-by: pratyush-seagate <pratyush.k.khan@seagate.com>

--- rebuildall.sh
+++ rebuildall.sh
@@ -46,6 +46,7 @@ usage() {
   echo '          --no-motrkvscli-build    : Do not build motrkvscli tool, Default (false)'
   echo '          --no-s3background-build    : Do not build s3background process, Default (false)'
   echo '          --no-s3recoverytool-build    : Do not build s3recoverytool process, Default (false)'
+  echo '          --no-s3confstoretool-build    : Do not build s3confstoretool process, Default (false)'
   echo '          --no-s3addbplugin-build    : Do not build s3 addb plugin library, Default (false)'
   echo '          --no-auth-build            : Do not build Auth Server, Default (false)'
   echo '          --no-jclient-build         : Do not build jclient, Default (false)'
@@ -138,7 +139,7 @@ get_motr_pkg_config_rpm() {
 # read the options
 OPTS=`getopt -o h --long no-motr-rpm,use-build-cache,no-check-code,no-clean-build,\
 no-s3ut-build,no-s3mempoolut-build,no-s3mempoolmgrut-build,no-s3server-build,\
-no-motrkvscli-build,no-s3background-build,no-s3recoverytool-build,\
+no-motrkvscli-build,no-s3background-build,no-s3recoverytool-build,no-s3confstoretool-build,\
 no-s3addbplugin-build,no-auth-build,no-jclient-build,no-jcloudclient-build,\
 no-s3iamcli-build,no-java-tests,no-install,just-gen-build-file,valgrind_memcheck,\
 help -n 'rebuildall.sh' -- "$@"`
@@ -156,6 +157,7 @@ no_s3server_build=0
 no_motrkvscli_build=0
 no_s3background_build=0
 no_s3recoverytool_build=0
+no_s3confstoretool_build=0
 no_s3addbplugin_build=0
 no_auth_build=0
 no_jclient_build=0
@@ -180,6 +182,7 @@ while true; do
     --no-motrkvscli-build) no_motrkvscli_build=1; shift ;;
     --no-s3background-build) no_s3background_build=1; shift ;;
     --no-s3recoverytool-build) no_s3recoverytool_build=1; shift ;;
+    --no-s3confstoretool-build) no_s3confstoretool_build=1; shift ;;
     --no-s3addbplugin-build) no_s3addbplugin_build=1; shift ;;
     --no-auth-build) no_auth_build=1; shift ;;
     --no-jclient-build) no_jclient_build=1; shift ;;
@@ -517,6 +520,17 @@ then
   if [ $no_s3recoverytool_build -eq 0 ]
   then
     cd s3recovery
+    if [ $no_clean_build -eq 0 ]
+    then
+      python36 setup.py install --force
+    else
+      python36 setup.py install
+    fi
+    cd -      
+  fi
+  if [ $no_s3confstoretool_build -eq 0 ]
+  then
+    cd s3cortxutils/s3confstore
     if [ $no_clean_build -eq 0 ]
     then
       python36 setup.py install --force
