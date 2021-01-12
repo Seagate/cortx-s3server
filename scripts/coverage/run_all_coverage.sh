bab9333b17cb10c8b56eb31783cfc417ddd0f560
root <root@ssc-vm-1751.colo.seagate.com>
2021-01-12 06:09:40 -0700

    EOS-16099 : Update run_all_coverage.sh
    Signed-off-by: root <root@ssc-vm-1751.colo.seagate.com>

--- scripts/coverage/run_all_coverage.sh
+++ scripts/coverage/run_all_coverage.sh
@@ -21,12 +21,18 @@
 set -e
 set +o posix
 
+#Check if codacy token as an argument is passed
+if [[ $# -eq 0 ]] ; then
+    echo 'Error: No arguments supplied,please pass codacy token as an argument to the script'
+    exit 1
+fi
+
 # variables
 SCRIPT_PATH=$(readlink -f "$0")
 SCRIPT_DIR=$(dirname "$SCRIPT_PATH")
 SRC_DIR="$(dirname "$(dirname "$SCRIPT_DIR" )")"
 # Default codacy project token for cortx-s3server
-CODACY_PROJECT_TOKEN=7a64ef82e1a049ab898fe838f1320594
+CODACY_PROJECT_TOKEN=$1
 # Default branch for uploading coverage report
 BRANCH="main"
 
