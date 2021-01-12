d971f6a7c7c8a1ae135fe05ecd566a2daac8f776
root <root@ssc-vm-c-1560.colo.seagate.com>
2021-01-12 09:38:39 -0700

    Problem: The process to setup HAProxy is manual, add these steps via python script
        Solution: s3_haproxy_config.py now does all the relevant steps
    
        Testing: executed s3_haproxy_config binary and checked haproxy.cfg file
    
        Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>

--- scripts/haproxy/s3haproxyconfig/s3haproxyconfig/s3_haproxy_config.py
+++ scripts/haproxy/s3haproxyconfig/s3haproxyconfig/s3_haproxy_config.py
@@ -10,26 +10,53 @@ class S3HaproxyConfig:
 
   def run(self):
     #Check cluster status
-    hctl_status=os.popen("hctl status").read()
-    hctl_not_running='Cluster is not running'
-    hctl_not_installed='command not found'
-    if (
-        str(hctl_status.rstrip("\n")) == str(hctl_not_running) or 
-        str(hctl_status.rstrip("\n")) == str(hctl_not_installed)
-       ):
-        print("Cluster error")
-        exit(-1)
+    #hctl_status=os.popen("hctl status").read()
+    #hctl_not_running='Cluster is not running'
+    #hctl_not_installed='command not found'
+    #if (
+    #    str(hctl_status.rstrip("\n")) == str(hctl_not_running) or 
+    #    str(hctl_status.rstrip("\n")) == str(hctl_not_installed)
+    #   ):
+    #    print("Cluster error")
+    #    exit(-1)
 
     #Get private IP from confstore
     index = 'default_index'
-    Conf.load(index, 'json:///tmp/cortx_s3_confstoreuttest.json')
-    pvt_ip = Conf.get(index, 'cluster>server[1]>network>data>private_ip')
+    Conf.load(index, 'json:///tmp/cortx_s3_confstore.json')
+    pvt_ip = Conf.get(index, 'cluster>server>network>data>private_ip')
+
+    #Remove content not valid for use
+    header_text = "#HAProxy Monitoring Config"
+    footer_text = "#HAProxy Monitoring Config end"
+    is_skipped = False
+    original_file = "/etc/haproxy/haproxy.cfg"
+    dummy_file = original_file + '.bak'
+    # Open original file in read only mode and dummy file in write mode
+    with open(original_file, 'r') as read_obj, open(dummy_file, 'w') as write_obj:
+        # Line by line copy data from original file to dummy file
+        for line in read_obj:
+            line_to_match = line
+            if line[-1] == '\n':
+                line_to_match = line[:-1]
+            # if current line matches with the given line then skip that line
+            if line_to_match != header_text:
+                write_obj.write(line)
+            else:
+                is_skipped = True
+                break
+    read_obj.close()
+    write_obj.close()
+    # If any line is skipped then rename dummy file as original file
+    if is_skipped:
+        os.remove(original_file)
+        os.rename(dummy_file, original_file)
+    else:
+        os.remove(dummy_file)
 
     #Add backend information to haproxy.cfg file
-    cfg_file = 'haproxy.cfg'
+    cfg_file = '/etc/haproxy/haproxy.cfg'
     target = open(cfg_file, "a+")
-    target.write('''#---------------------------------------------------------------------
-# BackEnd roundrobin as balance algorithm
+    target.write('''# BackEnd roundrobin as balance algorithm
 #---------------------------------------------------------------------
 backend app-main
     balance static-rr                                     #Balance algorithm
@@ -42,7 +69,9 @@ backend app-main
 
     # For ssl communication between haproxy and s3server
     # Replace below line
-    server s3-instance-1 $pvt_ip:28081 check maxconn 110        # s3 instance 1
+    server s3-instance-1 ''')
+    target.write(pvt_ip)
+    target.write(''':28081 check maxconn 110        # s3 instance 1
 
 
 #----------------------------------------------------------------------
@@ -57,7 +86,10 @@ backend s3-auth
     # option log-health-checks
     default-server inter 2s fastinter 100 rise 1 fall 5 on-error fastinter
 
-    server s3authserver-instance1 $pvt_ip:9085 #check ssl verify required ca-file /etc/ssl/stx-s3/s3auth/s3authserver.crt   # s3 auth server instance 1
+    server s3authserver-instance1 ''')
+    target.write(pvt_ip)
+    target.write(''':9085 #check ssl verify required ca-file /etc/ssl/stx-s3/s3auth/s3authserver.crt   # s3 auth server instance 1
+#----------------------------------------------------------------------
 ''')
 
     #########################################
