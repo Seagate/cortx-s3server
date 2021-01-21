749cdb3447c5be0928a6709b9be1b9d49f2311f5
vimalraghwani20 <vimal.raghwani@seagate.com>
2021-01-21 05:25:48 -0700

    EOS:16218:S3 revert version to 1.0.0 Signed-off-by: vimalraghwani20 <vimal.raghwani@seagate.com>

--- s3cortxutils/s3msgbus/setup.py
+++ s3cortxutils/s3msgbus/setup.py
@@ -21,7 +21,7 @@ from setuptools import setup
 files = ["config/*","VERSION"]
 
 # Load the version
-s3msgbus_version = "2.0.0"
+s3msgbus_version = "1.0.0"
 current_script_path = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(current_script_path, 'VERSION')) as version_file:
     s3msgbus_version = version_file.read().strip()
