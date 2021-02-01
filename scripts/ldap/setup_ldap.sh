d14f97893652fe7acb5a5c614c88efceb491dcc3
Amit Kumar <amit.kumar@seagate.com>
2021-02-01 00:34:25 -0700

    EOS:16994 - Add error handling in s3_setup script
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>
    
    Added more print statements to give imformation on which step is running.
    
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>
    
    Fix conflict lines pushed to s3_setup by mistake
    
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>
    
    fixed whitespace in s3cipher command
    
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>

--- scripts/ldap/setup_ldap.sh
+++ scripts/ldap/setup_ldap.sh
@@ -189,11 +189,8 @@ ldapmodify -Y EXTERNAL -H ldapi:/// -w $ROOTDNPASSWORD -f $INSTALLDIR/resultssiz
 systemctl enable slapd
 systemctl restart slapd
 
-echo "Encrypting Authserver LDAP password"
+echo "Encrypting Authserver LDAP password.."
 /opt/seagate/cortx/auth/scripts/enc_ldap_passwd_in_cfg.sh -l $LDAPADMINPASS -p /opt/seagate/cortx/auth/resources/authserver.properties
-
-echo "Restart S3authserver"
-systemctl restart s3authserver
 
 if [[ $usessl == true ]]
 then
