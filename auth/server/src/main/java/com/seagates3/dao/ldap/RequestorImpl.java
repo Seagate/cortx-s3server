5dd2483824d7e821b2d84f8b294fd61a7d0f9b12
shalakadharap <64534897+shalakadharap@users.noreply.github.com>
2021-02-09 21:01:02 +0530

    EOS-15268 Performance issue - Auth call takes more time (#653)
    * EOS-15268 Performance issue - Auth call takes more time
    * Update auth_spec_negative_and_fi.py
    * Update runallsystest.sh
    * Update auth_spec_signature_calculation.py
    * Update auth_spec_signature_calculation.py
    * Update runallsystest.sh
    * Update authpassencryptcli_spec.py

--- auth/server/src/main/java/com/seagates3/dao/ldap/RequestorImpl.java
+++ auth/server/src/main/java/com/seagates3/dao/ldap/RequestorImpl.java
@@ -96,7 +96,6 @@ public class RequestorImpl implements RequestorDAO {
 
                 String accountName = getAccountName(entry.getDN());
                 requestor.setAccount(getAccount(accountName));
-                lc.abandon(ldapResults);
             } else {
                 LOGGER.error("Failed to find access key details of userId: "
                         + accessKey.getUserId());
