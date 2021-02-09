c59556aba7da0e978ddc1fddc79e3183ce6a1520
Dattaprasad Govekar <65955475+DPG17@users.noreply.github.com>
2021-02-09 18:01:37 +0530

    Br/dg/s3fault/eos 17129 (#678)
    * First uncompiled changes
    
    Signed-off-by: Dattaprasad <dattaprasad.govekar@seagate.com>
    
    * EOS-17129: Common metadata changes for s3 fault tolerence.
     - Incorporated review comments.
    
    Signed-off-by: Dattaprasad <dattaprasad.govekar@seagate.com>

--- server/s3_object_action_base.h
+++ server/s3_object_action_base.h
@@ -47,6 +47,7 @@ class S3ObjectAction : public S3Action {
 
  protected:
   std::shared_ptr<S3ObjectMetadata> object_metadata;
+  std::shared_ptr<S3ObjectExtendedMetadata> extended_obj_metadata;
   std::shared_ptr<S3BucketMetadata> bucket_metadata;
 
   // Certain APIs like CopyObject, would need more than single
@@ -76,6 +77,8 @@ class S3ObjectAction : public S3Action {
   virtual void fetch_bucket_info_success();
   virtual void fetch_additional_bucket_info_success();
   virtual void fetch_additional_object_info_success();
+  virtual void fetch_ext_object_info_success();
+  void fetch_ext_object_info_failed();
 
   // Sets appropriate Fault points for any shutdown tests.
   void setup_fi_for_shutdown_tests();
