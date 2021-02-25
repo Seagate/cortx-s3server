3ae9e6cf59c56da375be05fcccf5e7e402716de2
pratyush-seagate <pratyush.k.khan@seagate.com>
2021-02-25 07:34:26 -0700

    EOS-17950: Mini Provisioner: s3_setup Test phase enhancement         Change description:                 Changes as described in ticket EOS-17950 to place the s3-sanity tests in python version of _s3_setup.         Added/modified/deleted files: 		modified:   installhelper.sh 		deleted:    s3-sanity-test.sh 		modified:   scripts/provisioning/s3_setup 		modified:   scripts/provisioning/testcmd.py 		modified:   scripts/s3-sanity/s3-sanity-test.sh 	Unit test: 		Individual unit tests of scripts/s3-sanity/s3-sanity-test.sh with various combinations of input params. 		Run "s3_setup config" with these changes and verified steps worked. Performed complete mini-provisioner s3 steps.
    Signed-off-by: pratyush-seagate <pratyush.k.khan@seagate.com>

--- scripts/s3-sanity/s3-sanity-test.sh
+++ scripts/s3-sanity/s3-sanity-test.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 #
 # Copyright (c) 2020 Seagate Technology LLC and/or its Affiliates
 #
@@ -17,81 +18,301 @@
 # please email opensource@seagate.com or cortx-questions@seagate.com.
 #
 
-#!/bin/bash
-
-set -e
 
 #########################
 # S3 sanity test script #
 #########################
 
-USAGE="USAGE: bash $(basename "$0") [--help]
+ldappasswd=''
+s3iamclitestson=false
+s3cmdtestson=false
+anys3iamclitestsfailed=false
+anys3cmdtestsfailed=false
+anytestsfailed=false
+
+s3cmdconfigfile="/tmp/.s3cfg"
+test_file_input="/tmp/SanityObjectToDeleteAfterUse.input$$"
+test_output_file="/tmp/SanityObjectToDeleteAfterUse.out$$"
+
+set -e
+set -p
+
+USAGE="USAGE: bash $(basename "$0")
 Run S3 sanity test.
+
+PRE-REQUISITES: s3cmd and s3iamcli need to be present
+
 where:
-    --help      display this help and exit
+    -h     display this help and exit
+    -c     clean s3 resources if present
+    -e     specify the s3 server endpoint, pass 127.0.0.1 if running on s3 server node
+           else provide data interface ip which needs to be in /etc/hosts
+    -p     specify ldap password in text form, mandatory
 
 Operations performed:
+  * Create Account
+  * Create User
   * Create Bucket
-  * List Buckets
   * Put Object
-  * Get Object
   * Delete Object
-  * Delete Bucket"
+  * Delete Bucket
+  * Delete User
+  * Delete Account
+  * ldapsearch s3-background-delete-svc account"
 
-case "$1" in
-    --help )
-        echo "$USAGE"
-        exit 0
-        ;;
-esac
+die_with_error ()
+{
+  echo "$@" 1>&2
+  exit -1
+}
 
+update_config() {
+  if [ "$s3iamclitestson" == false ];then
+    echo "Both s3iamcli and s3cmd tests are disabled, nothing to config."
+    return
+  fi
 
-# Security credentialis
-read -p "Enter Access Key: " access_key
-read -p "Enter Secret Key: " secret_key
+  if [ "$s3cmdtestson" == true ];then
+    echo "[default]" > $s3cmdconfigfile || die_with_error "unable to update $s3cmdconfigfile"
+    echo "host_base = s3.seagate.com" >> $s3cmdconfigfile || die_with_error "unable to update $s3cmdconfigfile"
+    echo "host_bucket = s3.seagate.com" >> $s3cmdconfigfile || die_with_error "unable to update $s3cmdconfigfile"
+    echo "use_https = False" >> $s3cmdconfigfile || die_with_error "unable to update $s3cmdconfigfile"
+  fi
 
-read -p "Enter test bucket name: " bucket_name
-read -p "Enter test object name: " object_name
-read -p "Enter test object size in MB: " object_size
+  if [ ! -z "$end_point" ];then
+    s3iamcli listaccounts --ldapuser sgiamadmin --ldappasswd "$ldappasswd" --no-ssl >/dev/null 2>&1 || echo "configured s3iamcli"
+    echo "using s3endpoint $end_point"
+    ls /root/.sgs3iamcli/config.yaml 2> /dev/null || die_with_error "S3iamcli configuration file is missing"
 
-echo -e "\n\n***** S3: SANITY TEST *****"
+    if [ "$s3cmdtestson" == true ];then
+      if [ ! -e $s3cmdconfigfile ];then
+        die_with_error "S3cmd configuration file $s3cmdconfigfile is missing!"
+      fi
+      sed -i "s/host_base =.*/host_base = $end_point/g" $s3cmdconfigfile || die_with_error "unable to update $s3cmdconfigfile"
+    fi
 
-TEST_CMD="s3cmd --access_key=$access_key --secret_key=$secret_key"
+    sed -i "s/IAM:.*/IAM: http:\/\/$end_point:9080/g" /root/.sgs3iamcli/config.yaml || die_with_error "unable to update /root/.sgs3iamcli/config.yaml"
+    sed -i "s/IAM_HTTPS:.*/IAM_HTTPS: https:\/\/$end_point:9443/g" /root/.sgs3iamcli/config.yaml || die_with_error "unable to update /root/.sgs3iamcli/config.yaml"
+    sed -i "s/VERIFY_SSL_CERT:.*/VERIFY_SSL_CERT: false/g" /root/.sgs3iamcli/config.yaml || die_with_error "unable to update /root/.sgs3iamcli/config.yaml"
+  fi
+}
 
-echo -e "\nList Buckets: "
-$TEST_CMD ls || { echo "Failed" && exit 1; }
+restore_config() {
+  if [ "$s3iamclitestson" == false ];then
+    echo "Both s3iamcli and s3cmd tests are disabled, nothing to restore."
+    return
+  fi
 
-echo -e "\nCreate bucket - '$bucket_name': "
-$TEST_CMD mb "s3://$bucket_name" || { echo "Failed" && exit 1; }
+  ls /root/.sgs3iamcli/config.yaml 2> /dev/null || die_with_error "S3iamcli configuration file is missing"
+  rm -rf /root/.sgs3iamcli
 
-# create a test file
-test_file_input=/tmp/"$object_name".input
-test_output_file=/tmp/"$object_name".out
+  if [ "$s3cmdtestson" == true ];then
+    ls $s3cmdconfigfile 2> /dev/null || die_with_error "S3cmd configuration file $s3cmdconfigfile is missing"
+    ls $test_file_input 2> /dev/null || die_with_error "Input file $test_file_input is missing"
+    ls $test_output_file 2> /dev/null || die_with_error "Output file  $test_output_file is missing"
+    sed -i "s/host_base =.*/host_base = s3.seagate.com/g" $s3cmdconfigfile 
+    rm -f $s3cmdconfigfile $test_file_input $test_output_file || die_with_error "rm -f failed in $s3cmdconfigfile $test_file_input $test_output_file"
+  fi
+}
 
-dd if=/dev/urandom of=$test_file_input bs=1MB count=$object_size
-content_md5_before=$(md5sum $test_file_input | cut -d ' ' -f 1)
+cleanup() {
+  update_config
+  if [ "$s3iamclitestson" == false ];then
+    echo "Both s3iamcli and s3cmd tests are disabled, nothing to cleanup."
+    return
+  fi
 
-echo -e "\nUpload object '$test_file_input' to '$bucket_name': "
-$TEST_CMD put $test_file_input "s3://$bucket_name/$object_name" || { echo "Failed" && exit 1; }
+  output=$(s3iamcli resetaccountaccesskey -n SanityAccountToDeleteAfterUse --ldapuser sgiamadmin --ldappasswd "$ldappasswd" --no-ssl) 2> /dev/null || echo ""
+  if [[ $output == *"Name or service not known"*  || $output == *"Invalid argument"* ]];then
+    restore_config
+    die_with_error "Provide the Appropriate endpoint"
+  elif [[ $output == *"NoSuchEntity"* ]];then
+    echo "Sanity Account doesn't exist for cleanup"
+    return
+  else
+    access_key=$(echo -e "$output" | tr ',' '\n' | grep "AccessKeyId" | awk '{print $3}')
+    secret_key=$(echo -e "$output" | tr ',' '\n' | grep "SecretKey" | awk '{print $3}')
 
-echo -e "\nList uploaded object in '$bucket_name': "
-$TEST_CMD ls "s3://$bucket_name/$object_name" || { echo "Failed" && exit 1; }
+    if [ "$s3cmdtestson" == true ];then
+      TEST_CMD="s3cmd --access_key=$access_key --secret_key=$secret_key -c $s3cmdconfigfile"
+      $TEST_CMD del "s3://sanitybucket/SanityObjectToDeleteAfterUse" >/dev/null 2>&1 || echo ""
+      $TEST_CMD rb "s3://sanitybucket" >/dev/null 2>&1 || echo ""
+    fi
 
-echo -e "\nDownload object '$object_name' from '$bucket_name': "
-$TEST_CMD get "s3://$bucket_name/$object_name" $test_output_file || { echo "Failed" && exit 1; }
-content_md5_after=$(md5sum $test_output_file | cut -d ' ' -f 1)
+    s3iamcli deleteuser -n SanityUserToDeleteAfterUse --access_key "$access_key" --secret_key "$secret_key" --no-ssl 2> /dev/null || echo ""
+    s3iamcli deleteaccount -n SanityAccountToDeleteAfterUse --access_key "$access_key" --secret_key "$secret_key" --no-ssl 2> /dev/null || echo ""
+  fi
 
-echo -en "\nData integrity check: "
-[[ $content_md5_before == $content_md5_after ]] && echo 'Passed.' || echo 'Failed.'
+  restore_config
+}
 
-echo -e "\nDelete object '$object_name' from '$bucket_name': "
-$TEST_CMD del "s3://$bucket_name/$object_name" || { echo "Failed" && exit 1; }
+while getopts ":p:e:c" o; do
+    case "${o}" in
+        e)
+            end_point=${OPTARG}
+            ;;
+        c)
+            externalcleanup=true
+            ;;
+        p)
+            ldappasswd=${OPTARG}
+            ;;
+        *)
+            echo "$USAGE"
+            exit -1
+            ;;
+    esac
+done
+shift $((OPTIND-1))
 
-echo -e "\nDelete bucket - '$bucket_name': "
-$TEST_CMD rb "s3://$bucket_name" || { echo "Failed" && exit 1; }
-set +e
+trap "cleanup" ERR
 
-# delete test files file
-rm -f $test_file_input $test_output_file
+if [ -z "$ldappasswd" ]
+then
+  die_with_error "Please provide -p <ldappasswd>"
+fi
 
-echo -e "\n\n***** S3: SANITY TEST SUCCESSFULLY COMPLETED *****\n"
+# Check if s3iamcli is installed
+if command -v s3iamcli >/dev/null 2>&1;then
+  echo -e "\nCheck s3iamcli...OK, S3IAMCLI tests are enabled."
+  s3iamclitestson=true
+
+  # Check if s3cmd is installed
+  if command -v s3cmd >/dev/null 2>&1;then
+    echo -e "\nCheck S3CMD...OK, S3CMD data path tests are enabled."
+    s3cmdtestson=true
+  else
+    echo -e "\nS3CMD is not found, S3CMD (data path) tests are disabled."
+  fi
+else
+  echo -e "\nS3IAMCLI is not found, both S3IAMCLI and S3CMD tests are disabled."
+fi
+
+systemctl restart s3authserver
+
+cleanup
+
+if [ "$externalcleanup" = true ];then
+  exit 0
+fi
+
+echo -e "\n\n*** S3 Sanity ***"
+
+if [ "$s3iamclitestson" == true ];then
+  echo -e "\n\n**** Create Account *******"
+  output=$(s3iamcli createaccount -n SanityAccountToDeleteAfterUse  -e SanityAccountToDeleteAfterUse@sanitybucket.com --ldapuser sgiamadmin --ldappasswd "$ldappasswd" --no-ssl)
+  access_key=$(echo -e "$output" | tr ',' '\n' | grep "AccessKeyId" | awk '{print $3}')
+  secret_key=$(echo -e "$output" | tr ',' '\n' | grep "SecretKey" | awk '{print $3}')
+  s3iamcli CreateUser -n SanityUserToDeleteAfterUse --access_key "$access_key" --secret_key "$secret_key" --no-ssl 2> /dev/null
+  if [ $? -ne 0 ];then
+    anys3iamclitestsfailed=true
+    restore_config
+    die_with_error "s3iamcli CreateUser failed"
+  fi
+
+  if [ "$s3cmdtestson" == true ];then
+    TEST_CMD="s3cmd --access_key=$access_key --secret_key=$secret_key -v -c $s3cmdconfigfile"
+    echo -e "\nCreate bucket - 'sanitybucket': "
+    $TEST_CMD mb "s3://sanitybucket"
+    if [ $? -ne 0 ];then
+      echo "Create bucket - 'sanitybucket':  failed"
+      anys3cmdtestsfailed=true
+    fi
+
+    # create a test file
+    dd if=/dev/urandom of=$test_file_input bs=5MB count=1
+    if [ $? -ne 0 ];then
+      echo "dd failed"
+     anys3cmdtestsfailed=true
+    fi
+    content_md5_before=$(md5sum $test_file_input | cut -d ' ' -f 1)
+    if [ $? -ne 0 ];then
+      echo "md5sum failed"
+      anys3cmdtestsfailed=true
+    fi
+
+    echo -e "\nUpload '$test_file_input' to 'sanitybucket': "
+    $TEST_CMD put $test_file_input "s3://sanitybucket/SanityObjectToDeleteAfterUse"
+    if [ $? -ne 0 ];then
+      echo "Upload '$test_file_input' to 'sanitybucket': failed"
+      anys3cmdtestsfailed=true
+    fi
+
+    echo -e "\nList uploaded SanityObjectToDeleteAfterUse in 'sanitybucket': "
+    $TEST_CMD ls "s3://sanitybucket/SanityObjectToDeleteAfterUse"
+    if [ $? -ne 0 ];then
+      echo "List uploaded SanityObjectToDeleteAfterUse in 'sanitybucket': failed"
+      anys3cmdtestsfailed=true
+    fi
+
+    echo -e "\nDownload 'SanityObjectToDeleteAfterUse' from 'sanitybucket': "
+    $TEST_CMD get "s3://sanitybucket/SanityObjectToDeleteAfterUse" $test_output_file
+    if [ $? -ne 0 ];then
+      echo "Download 'SanityObjectToDeleteAfterUse' from 'sanitybucket': failed"
+      anys3cmdtestsfailed=true
+    fi
+
+    content_md5_after=$(md5sum $test_output_file | cut -d ' ' -f 1)
+    echo -en "\nData integrity check: "
+    [[ $content_md5_before == $content_md5_after ]] && echo 'Passed.'
+    echo -e "\nDelete 'SanityObjectToDeleteAfterUse' from 'sanitybucket': "
+    $TEST_CMD del "s3://sanitybucket/SanityObjectToDeleteAfterUse"
+    if [ $? -ne 0 ];then
+      echo "s3://sanitybucket/SanityObjectToDeleteAfterUse failed"
+      anys3cmdtestsfailed=true
+    fi
+
+    echo -e "\nDelete bucket - 'sanitybucket': "
+    $TEST_CMD rb "s3://sanitybucket"
+    if [ $? -ne 0 ];then
+      echo "Delete bucket - 'sanitybucket': failed"
+      anys3cmdtestsfailed=true
+    fi
+
+    if [ "$anys3cmdtestsfailed" == false ];then
+      echo -e "\n\n***** S3: SANITY DATA TESTS SUCCESSFULLY COMPLETED *****\n"
+    else
+      echo -e "\n\n***** S3: SANITY DATA TESTS COMPLETED WITH FAILURE*****\n"
+      anytestsfailed=true
+    fi
+  fi
+
+  echo -e "\nDelete User - 'SanityUserToDeleteAfterUse': "
+  s3iamcli deleteuser -n SanityUserToDeleteAfterUse --access_key "$access_key" --secret_key "$secret_key" --no-ssl 2> /dev/null
+  if [ $? -ne 0 ];then
+    anys3iamclitestsfailed=true
+    echo "s3iamcli deleteuser failed"
+  fi
+
+  echo -e "\nDelete Account - 'SanityAccountToDeleteAfterUse': "
+  s3iamcli deleteaccount -n SanityAccountToDeleteAfterUse --access_key "$access_key" --secret_key "$secret_key" --no-ssl
+  if [ $? -ne 0 ];then
+    anys3iamclitestsfailed=true
+    echo "s3iamcli deleteaccount failed"
+  fi
+
+  if [ "$anys3iamclitestsfailed" == false ];then
+    echo -e "\n\n***** S3: SANITY IAMCLI TESTS SUCCESSFULLY COMPLETED *****\n"
+  else
+    echo -e "\n\n***** S3: SANITY IAMCLI TESTS COMPLETED WITH FAILURE*****\n"
+    anytestsfailed=true
+  fi
+
+  set +e
+fi
+
+cmd_out=$(ldapsearch -b "o=s3-background-delete-svc,ou=accounts,dc=s3,dc=seagate,dc=com" -x -w $ldappasswd -D "cn=sgiamadmin,dc=seagate,dc=com" -H ldap://) || echo ""
+if [[ $cmd_out == *"No such object"* ]];then
+  echo -e "\n\n***** S3: SANITY ldapsearch TESTS COMPLETED WITH FAILURE, failed to find s3background delete account!*****\n"
+  anytestsfailed=true
+else
+  echo -e "\n\n***** S3: SANITY ldapsearch TESTS SUCCESSFULLY COMPLETED *****\n"
+fi
+
+restore_config
+
+if [ "$anytestsfailed" == false ];then
+  echo -e "\n\n***** S3: SANITY ALL TESTS SUCCESSFULLY COMPLETED *****\n"
+else
+  die_with_error "\n\n***** S3: SANITY ALL TESTS COMPLETED WITH FAILURE*****\n"
+fi
