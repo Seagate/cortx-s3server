e79361fe55fec3170158712457df16efd6e594da
Amit Kumar <64570828+amitwac2608@users.noreply.github.com>
2021-02-26 10:27:51 +0530

    EOS:16997 - Refactor cleanupcmd python file: Move delete ldap account code to ldapaccountaction file. (#728)
    * EOS:16997 - Refactor cleanupcmd python file: Move delete ldap account code to ldapaccountaction file.
    
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>
    
    * Added requirements.txt file and removed toml from validation checks
    
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>

--- scripts/ldap/ldapaccountaction.py
+++ scripts/ldap/ldapaccountaction.py
@@ -20,24 +20,12 @@
 
 import ldap
 import sys
+from ldap.ldapobject import SimpleLDAPObject
 import ldap.modlist as modlist
 from s3cipher.cortx_s3_cipher import CortxS3Cipher
 
 LDAP_USER = "cn={},dc=seagate,dc=com"
 LDAP_URL = "ldapi:///"
-
-# supported ldap action and input params dict
-g_supported_ldap_action_table = {
-  'CreateBGDeleteAccount': {
-  '--account_name': "s3-background-delete-svc",
-  '--account_id': "67891",
-  '--canonical_id': "C67891",
-  '--mail': "s3-background-delete-svc@seagate.com",
-  '--s3_user_id': "450",
-  '--const_cipher_secret_str': "s3backgroundsecretkey",
-  '--const_cipher_access_str': "s3backgroundaccesskey"
-  }
-}
 
 # map of dn names required for account creation.
 g_dn_names = {
@@ -65,18 +53,14 @@ class LdapAccountAction:
   """Perform given supported LDAP account action."""
   ldapuser = None
   ldappasswd = None
-  ldap_account_action = None
+  ldap_conn = None
 
-  def __init__(self, ldapuser: str, ldappasswd: str, account_action: str):
+  def __init__(self, ldapuser: str, ldappasswd: str):
     """Constructor."""
     try:
       self.ldapuser = ldapuser.strip()
       self.ldappasswd = ldappasswd.strip()
 
-      if account_action not in g_supported_ldap_action_table.keys():
-        raise Exception(f'Unsupported ldap account action: {account_action}')
-
-      self.ldap_account_action = account_action.strip()
     except Exception as e:
       sys.stderr.write(f'initialization failed, err: {e}')
       raise e
@@ -84,11 +68,218 @@ class LdapAccountAction:
   def __add_keys_to_dictionary(self, input_params:dict):
     """Adds access and secret keys to dictionary."""
     access_key, secret_key = self.__generate_access_secret_keys(
-                                      input_params['--const_cipher_secret_str'],
-                                      input_params['--const_cipher_access_str'])
+                                      input_params['const_cipher_secret_str'],
+                                      input_params['const_cipher_access_str'])
 
-    input_params['--access_key'] = access_key
-    input_params['--secret_key'] = secret_key
+    input_params['access_key'] = access_key
+    input_params['secret_key'] = secret_key
+
+  def __create_account_prepare_params(self, index_key:str, input_params:dict):
+    """Builds params for creating 'account'."""
+    dn = self.__get_dn(index_key)
+    attrs = self.__get_attr(index_key)
+
+    attrs['o'] = [input_params['account_name'].encode('utf-8')]
+    attrs['accountid'] = [input_params['account_id'].encode('utf-8')]
+    attrs['mail'] = [input_params['mail'].encode('utf-8')]
+    attrs['canonicalId'] = [input_params['canonical_id'].encode('utf-8')]
+
+    dn = dn.format(input_params['account_name'])
+
+    return dn, attrs
+
+  def __create_default_prepare_params(self, index_key:str, input_params:dict):
+    """Builds params for generic case."""
+    dn = self.__get_dn(index_key)
+    attrs = self.__get_attr(index_key)
+    dn = dn.format(input_params['account_name'])
+    return dn, attrs
+
+  def __create_s3userid_prepare_params(self, index_key:str, input_params:dict):
+    """Builds params for creating 's3userid'."""
+    dn = self.__get_dn(index_key)
+    attrs = self.__get_attr(index_key)
+
+    arn = "arn:aws:iam::{}:root".format(input_params['account_id'])
+
+    attrs['s3userid'] = [input_params['s3_user_id'].encode('utf-8')]
+    attrs['arn'] = [arn.encode('utf-8')]
+
+    dn = dn.format(input_params['s3_user_id'], input_params['account_name'])
+    return dn, attrs
+
+  def __create_accesskey_prepare_params(self, index_key:str, input_params:dict):
+    """Builds params for creating 'accesskey'."""
+    dn = self.__get_dn(index_key)
+    attrs = self.__get_attr(index_key)
+
+    attrs['ak'] = [input_params['access_key'].encode('utf-8')]
+    attrs['s3userid'] = [input_params['s3_user_id'].encode('utf-8')]
+    attrs['sk'] = [input_params['secret_key'].encode('utf-8')]
+
+    dn = dn.format(input_params['access_key'])
+    return dn, attrs
+
+  def __connect_to_ldap_server(self):
+    """Establish connection to ldap server."""
+    from ldap import initialize
+    from ldap import VERSION3
+    from ldap import OPT_REFERRALS
+
+    self.ldap_conn = initialize(LDAP_URL)
+    self.ldap_conn.protocol_version = VERSION3
+    self.ldap_conn.set_option(OPT_REFERRALS, 0)
+    self.ldap_conn.simple_bind_s(LDAP_USER.format(self.ldapuser), self.ldappasswd)
+
+  def __disconnect_from_ldap(self):
+    """Disconnects from ldap."""
+    self.ldap_conn.unbind_s()
+
+  def create_account(self, input_params: dict) -> None:
+    """Creates account in ldap db."""
+    self.__add_keys_to_dictionary(input_params)
+    try:
+      self.__connect_to_ldap_server()
+
+      if not self.__is_account_present(self.ldap_conn, input_params['account_name']):
+        # create the account
+        dn, attrs = self.__create_account_prepare_params('account', input_params)
+        ldif = modlist.addModlist(attrs)
+        self.ldap_conn.add_s(dn, ldif)
+
+        # create the account's sub-ordinate objects
+        dn, attrs = self.__create_default_prepare_params('policies', input_params)
+        ldif = modlist.addModlist(attrs)
+        self.ldap_conn.add_s(dn, ldif)
+
+        dn, attrs = self.__create_default_prepare_params('groups', input_params)
+        ldif = modlist.addModlist(attrs)
+        self.ldap_conn.add_s(dn, ldif)
+
+        dn, attrs = self.__create_default_prepare_params('users', input_params)
+        ldif = modlist.addModlist(attrs)
+        self.ldap_conn.add_s(dn, ldif)
+
+        dn, attrs = self.__create_s3userid_prepare_params('s3userid', input_params)
+        ldif = modlist.addModlist(attrs)
+        self.ldap_conn.add_s(dn, ldif)
+
+        dn, attrs = self.__create_default_prepare_params('roles', input_params)
+        ldif = modlist.addModlist(attrs)
+        self.ldap_conn.add_s(dn, ldif)
+
+        dn, attrs = self.__create_accesskey_prepare_params('access_key', input_params)
+        ldif = modlist.addModlist(attrs)
+        self.ldap_conn.add_s(dn, ldif)
+      else:
+        sys.stdout.write(f"ldap account: {input_params['account_name']} already exists.\n")
+
+      self.__disconnect_from_ldap()
+    except Exception as e:
+      if self.ldap_conn:
+        self.__disconnect_from_ldap()
+      raise e
+
+
+  def delete_account(self, input_params: dict) -> None:
+    """
+    Delete ldap account with given input params.
+    @input_params: dictionary of format:
+    {'account_name': {'s3userId': 'user_id' } }
+    """
+    try:
+      self.__connect_to_ldap_server()
+
+      for acc in input_params.keys():
+        if not self.__is_account_present(self.ldap_conn, acc):
+          sys.stdout.write(f'LDAP account: {acc} does not exist, skipping deletion.\n')
+          continue
+        acc_attr_dict = input_params[acc]
+
+        # delete the access key
+        self.__delete_access_key(self.ldap_conn, acc_attr_dict['s3userId'])
+
+        # delete roles
+        self.__delete_dn(self.ldap_conn, f'ou=roles,o={acc},ou=accounts,dc=s3,dc=seagate,dc=com')
+
+        # delete s3userid
+        self.__delete_dn(self.ldap_conn,
+                    f"s3UserId={acc_attr_dict['s3userId']},ou=users,o={acc},ou=accounts,dc=s3,dc=seagate,dc=com")
+
+        # delete users
+        self.__delete_dn(self.ldap_conn, f'ou=users,o={acc},ou=accounts,dc=s3,dc=seagate,dc=com')
+
+        # delete groups
+        self.__delete_dn(self.ldap_conn, f'ou=groups,o={acc},ou=accounts,dc=s3,dc=seagate,dc=com')
+
+        # delete policies
+        self.__delete_dn(self.ldap_conn, f'ou=policies,o={acc},ou=accounts,dc=s3,dc=seagate,dc=com')
+
+        # delete the account
+        self.__delete_dn(self.ldap_conn, f'o={acc},ou=accounts,dc=s3,dc=seagate,dc=com')
+
+      self.__disconnect_from_ldap()
+    except Exception as e:
+      sys.stderr.write(f'Failed to delete account: {acc}\n')
+      raise e
+
+  @staticmethod
+  def __is_account_present(ldap_conn: SimpleLDAPObject, account_name: str):
+    """Checks if account is present in ldap db."""
+    try:
+      ldap_conn.search_s(f"o={account_name},ou=accounts,dc=s3,dc=seagate,dc=com", ldap.SCOPE_SUBTREE)
+    except ldap.NO_SUCH_OBJECT:
+      return False
+    except Exception as e:
+      sys.stderr.write(f'INFO: Failed to find ldap account: {account_name}, error: {str(e)}\n')
+      raise e
+    return True
+
+  @staticmethod
+  def __delete_dn(ldap_conn: SimpleLDAPObject, dn: str):
+    """Delete given DN from ldap."""
+    try:
+      ldap_conn.delete_s(dn)
+    except ldap.NO_SUCH_OBJECT:
+      pass
+    except Exception as e:
+      sys.stderr.write(f'Failed to delete DN: {dn}\n')
+      raise e
+
+  @staticmethod
+  def __delete_access_key(ldap_conn: SimpleLDAPObject, userid: str):
+    """Delete access key of given s3userid."""
+    try:
+      access_key = LdapAccountAction.__get_accesskey(ldap_conn, userid)
+      ldap_conn.delete_s(f'ak={access_key},ou=accesskeys,dc=s3,dc=seagate,dc=com')
+    except ldap.NO_SUCH_OBJECT:
+      pass
+    except Exception as e:
+      sys.stderr.write(f'failed to delete access key of userid: {userid}\n')
+      raise e
+
+  @staticmethod
+  def __get_accesskey(ldap_conn: SimpleLDAPObject, s3userid: str) -> str:
+    """Get accesskey of the given userid."""
+    access_key = None
+
+    from ldap import SCOPE_SUBTREE
+
+    result_list = ldap_conn.search_s('ou=accesskeys,dc=s3,dc=seagate,dc=com',
+                                    SCOPE_SUBTREE,
+                                    filterstr='(ObjectClass=accessKey)')
+    for (_, attr_dict) in result_list:
+      if s3userid == attr_dict['s3UserId'][0].decode():
+        access_key = attr_dict['ak'][0].decode()
+        break
+    return access_key
+
+  @staticmethod
+  def __generate_access_secret_keys(const_secret_string, const_access_string):
+    """Generates access and secret keys."""
+    cortx_access_key = CortxS3Cipher(None, True, 22, const_access_string).generate_key()
+    cortx_secret_key = CortxS3Cipher(None, False, 40, const_secret_string).generate_key()
+    return cortx_access_key, cortx_secret_key
 
   @staticmethod
   def __get_attr(index_key):
@@ -106,140 +297,8 @@ class LdapAccountAction:
 
     raise Exception("Key Not Present")
 
-  def __create_account_prepare_params(self, index_key:str, input_params:dict):
-    """Builds params for creating 'account'."""
-    dn = self.__get_dn(index_key)
-    attrs = self.__get_attr(index_key)
-
-    attrs['o'] = [input_params['--account_name'].encode('utf-8')]
-    attrs['accountid'] = [input_params['--account_id'].encode('utf-8')]
-    attrs['mail'] = [input_params['--mail'].encode('utf-8')]
-    attrs['canonicalId'] = [input_params['--canonical_id'].encode('utf-8')]
-
-    dn = dn.format(input_params['--account_name'])
-
-    return dn, attrs
-
-  def __create_default_prepare_params(self, index_key:str, input_params:dict):
-    """Builds params for generic case."""
-    dn = self.__get_dn(index_key)
-    attrs = self.__get_attr(index_key)
-    dn = dn.format(input_params['--account_name'])
-    return dn, attrs
-
-  def __create_s3userid_prepare_params(self, index_key:str, input_params:dict):
-    """Builds params for creating 's3userid'."""
-    dn = self.__get_dn(index_key)
-    attrs = self.__get_attr(index_key)
-
-    arn = "arn:aws:iam::{}:root".format(input_params['--account_id'])
-
-    attrs['s3userid'] = [input_params['--s3_user_id'].encode('utf-8')]
-    attrs['arn'] = [arn.encode('utf-8')]
-
-    dn = dn.format(input_params['--s3_user_id'], input_params['--account_name'])
-    return dn, attrs
-
-  def __create_accesskey_prepare_params(self, index_key:str, input_params:dict):
-    """Builds params for creating 'accesskey'."""
-    dn = self.__get_dn(index_key)
-    attrs = self.__get_attr(index_key)
-
-    attrs['ak'] = [input_params['--access_key'].encode('utf-8')]
-    attrs['s3userid'] = [input_params['--s3_user_id'].encode('utf-8')]
-    attrs['sk'] = [input_params['--secret_key'].encode('utf-8')]
-
-    dn = dn.format(input_params['--access_key'])
-    return dn, attrs
-
   @staticmethod
-  def __generate_access_secret_keys(const_secret_string, const_access_string):
-    """Generates access and secret keys."""
-    cortx_access_key = CortxS3Cipher(None, True, 22, const_access_string).generate_key()
-    cortx_secret_key = CortxS3Cipher(None, False, 40, const_secret_string).generate_key()
-    return cortx_access_key, cortx_secret_key
-
-  @staticmethod
-  def __connect_to_ldap_server(ldapuser, ldappasswd):
-    """Establish connection to ldap server."""
-    ldap_connection = ldap.initialize(LDAP_URL)
-    ldap_connection.protocol_version = ldap.VERSION3
-    ldap_connection.set_option(ldap.OPT_REFERRALS, 0)
-    ldap_connection.simple_bind_s(LDAP_USER.format(ldapuser), ldappasswd)
-    return ldap_connection
-
-  @staticmethod
-  def __is_account_present(account_name, ldap_connection):
-    """Checks if account is present in ldap db."""
-    try:
-      ldap_connection.search_s(f"o={account_name},ou=accounts,dc=s3,dc=seagate,dc=com", ldap.SCOPE_SUBTREE)
-    except ldap.NO_SUCH_OBJECT:
-      return False
-    except Exception as e:
-      sys.stderr.write(f'INFO: Failed to find ldap account: {account_name}, error: {str(e)}\n')
-      raise e
-    return True
-
-  @staticmethod
-  def __disconnect_from_ldap(ldap_connection):
-    """Disconnects from ldap."""
-    ldap_connection.unbind_s()
-
-  def create_account(self):
-    """Creates account in ldap db."""
-    ldap_connection = None
-    input_params = g_supported_ldap_action_table[self.ldap_account_action]
-    try:
-      ldap_connection = self.__connect_to_ldap_server(self.ldapuser,
-                                              self.ldappasswd)
-      self.__add_keys_to_dictionary(input_params)
-
-      if not self.__is_account_present(input_params['--account_name'], ldap_connection):
-        # create the account
-        dn, attrs = self.__create_account_prepare_params('account', input_params)
-        ldif = modlist.addModlist(attrs)
-        ldap_connection.add_s(dn, ldif)
-
-        # create the account's sub-ordinate objects
-        dn, attrs = self.__create_default_prepare_params('policies', input_params)
-        ldif = modlist.addModlist(attrs)
-        ldap_connection.add_s(dn, ldif)
-
-        dn, attrs = self.__create_default_prepare_params('groups', input_params)
-        ldif = modlist.addModlist(attrs)
-        ldap_connection.add_s(dn, ldif)
-
-        dn, attrs = self.__create_default_prepare_params('users', input_params)
-        ldif = modlist.addModlist(attrs)
-        ldap_connection.add_s(dn, ldif)
-
-        dn, attrs = self.__create_s3userid_prepare_params('s3userid', input_params)
-        ldif = modlist.addModlist(attrs)
-        ldap_connection.add_s(dn, ldif)
-
-        dn, attrs = self.__create_default_prepare_params('roles', input_params)
-        ldif = modlist.addModlist(attrs)
-        ldap_connection.add_s(dn, ldif)
-
-        dn, attrs = self.__create_accesskey_prepare_params('access_key', input_params)
-        ldif = modlist.addModlist(attrs)
-        ldap_connection.add_s(dn, ldif)
-      else:
-        sys.stdout.write(f"ldap account: {input_params['--account_name']} already exists.\n")
-
-      self.__disconnect_from_ldap(ldap_connection)
-    except Exception as e:
-      if ldap_connection:
-        self.__disconnect_from_ldap(ldap_connection)
-      raise e
-
-  @staticmethod
-  def supported_ldap_actions():
-    """Get supported actions."""
-    print(f'supported ldap actions: {list(g_supported_ldap_action_table.keys())}')
-
-  @staticmethod
-  def print_create_account_results(result:dict):
+  def print_create_account_results(result:dict) -> None:
     """Prints results of create account action."""
-    print(f"AccountId = {result['--account_id']}, CanonicalId = {result['--canonical_id']},"
-        f" RootUserName = root, AccessKeyId = {result['--access_key']}, SecretKey = {result['--secret_key']}")
+    print(f"AccountId = {result['account_id']}, CanonicalId = {result['canonical_id']},"
+        f" RootUserName = root, AccessKeyId = {result['access_key']}, SecretKey = {result['secret_key']}")
