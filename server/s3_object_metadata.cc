2a44408b209fcffd41c652c501e1feef30d881c3
Maksym Medvied <max.medved@seagate.com>
2021-03-22 22:31:50 +0300

    s3server: change logs from INFO to DEBUG for json conversions
    Signed-off-by: Maksym Medvied <max.medved@seagate.com>

--- server/s3_object_metadata.cc
+++ server/s3_object_metadata.cc
@@ -606,8 +606,8 @@ std::string S3ObjectMetadata::to_json() {
 
   Json::FastWriter fastWriter;
   std::string s = fastWriter.write(root);
-  s3_log(S3_LOG_INFO, request_id,
-	 "S3ObjectMetadata::to_json() = %s\n", s.c_str());
+  s3_log(S3_LOG_DEBUG, request_id, "S3ObjectMetadata::to_json() = %s\n",
+         s.c_str());
   return s;
 }
 
@@ -629,8 +629,8 @@ std::string S3ObjectMetadata::version_entry_to_json() {
 
   Json::FastWriter fastWriter;
   std::string s = fastWriter.write(root);
-  s3_log(S3_LOG_INFO, request_id,
-	 "S3ObjectMetadata::version_entry_to_json() = %s\n", s.c_str());
+  s3_log(S3_LOG_DEBUG, request_id,
+         "S3ObjectMetadata::version_entry_to_json() = %s\n", s.c_str());
   return s;
 }
 
@@ -660,7 +660,7 @@ std::string S3ObjectMetadata::version_entry_to_json() {
 int S3ObjectMetadata::from_json(std::string content) {
   s3_log(S3_LOG_DEBUG, request_id, "Called with content [%s]\n",
          content.c_str());
-  s3_log(S3_LOG_INFO, request_id, "S3ObjectMetadata::from_json(): s=%s\n",
+  s3_log(S3_LOG_DEBUG, request_id, "S3ObjectMetadata::from_json(): s=%s\n",
          content.c_str());
   Json::Value newroot;
   Json::Reader reader;
