6e082217eeb62f328ae3c014cbbae9fa30c86d03
Maksym Medvied <max.medved@seagate.com>
2021-03-22 22:57:57 +0300

    s3server: remove json-related logs
    They are not needed anymore.
    
    Signed-off-by: Maksym Medvied <max.medved@seagate.com>

--- server/s3_object_metadata.cc
+++ server/s3_object_metadata.cc
@@ -606,10 +606,8 @@ std::string S3ObjectMetadata::to_json() {
   root["create_timestamp"] = current_time.get_isoformat_string();
 
   Json::FastWriter fastWriter;
-  std::string s = fastWriter.write(root);
-  s3_log(S3_LOG_DEBUG, request_id, "S3ObjectMetadata::to_json() = %s\n",
-         s.c_str());
-  return s;
+  return fastWriter.write(root);
+  ;
 }
 
 // Streaming to json
@@ -629,10 +627,8 @@ std::string S3ObjectMetadata::version_entry_to_json() {
   root["create_timestamp"] = current_time.get_isoformat_string();
 
   Json::FastWriter fastWriter;
-  std::string s = fastWriter.write(root);
-  s3_log(S3_LOG_DEBUG, request_id,
-         "S3ObjectMetadata::version_entry_to_json() = %s\n", s.c_str());
-  return s;
+  return fastWriter.write(root);
+  ;
 }
 
 /*
@@ -660,8 +656,6 @@ std::string S3ObjectMetadata::version_entry_to_json() {
 
 int S3ObjectMetadata::from_json(std::string content) {
   s3_log(S3_LOG_DEBUG, request_id, "Called with content [%s]\n",
-         content.c_str());
-  s3_log(S3_LOG_DEBUG, request_id, "S3ObjectMetadata::from_json(): s=%s\n",
          content.c_str());
   Json::Value newroot;
   Json::Reader reader;
