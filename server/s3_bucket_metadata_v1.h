969690151baae789681c9122e129d0342dec3238
evgeniy-brazhnikov <evgeniy.brazhnikov@seagate.com>
2021-04-06 06:30:29 -0600

    EOS-16554: for UTs
    Signed-off-by: evgeniy-brazhnikov <evgeniy.brazhnikov@seagate.com>

--- server/s3_bucket_metadata_v1.h
+++ server/s3_bucket_metadata_v1.h
@@ -164,6 +164,8 @@ class S3BucketMetadataV1 : public S3BucketMetadata {
   // for UTs only
   struct m0_uint128 get_bucket_metadata_list_index_oid();
   void set_bucket_metadata_list_index_oid(struct m0_uint128 id);
+
+ protected:
   void set_state(S3BucketMetadataState state);
 
  public:
