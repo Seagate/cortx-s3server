9abc586f41119a948cb9eb42dde4681b4602a0aa
Dmitrii Surnin <dmitrii.surnin@seagate.com>
2021-04-20 06:52:41 -0600

    Fix fi ut crash
    Signed-off-by: Dmitrii Surnin <dmitrii.surnin@seagate.com>

--- server/s3_fi_common.h
+++ server/s3_fi_common.h
@@ -138,8 +138,6 @@ int s3_di_fi_is_enabled(const char *tag);
 
 #else /* ENABLE_FAULT_INJECTION */
 
-inline int s3_fi_is_enabled(const char *tag) { return false; }
-
 inline void s3_fi_enable_once(const char *tag) {}
 
 inline void s3_fi_enable_random(const char *tag, uint32_t p) {}
@@ -147,6 +145,8 @@ inline void s3_fi_enable_random(const char *tag, uint32_t p) {}
 inline void s3_fi_enable_each_nth_time(const char *tag, uint32_t n) {}
 
 inline void s3_fi_enable_off_n_on_m(const char *tag, uint32_t n, uint32_t m) {}
+
+int s3_fi_is_enabled(const char *tag);
 
 void s3_fi_enable(const char *tag);
 
