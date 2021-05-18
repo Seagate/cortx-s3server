a77e3c13bac1cba025fdcf1d2bb13c1c5f5c8344
Sachitanand Shelake <65209830+sachitanands@users.noreply.github.com>
2021-05-18 15:23:55 +0530

    GitIssue-881: remove pip installtion through easy_install and add lnet validator (#896)
    * GitIssue-881: remove pip installtion through easy_install
    
    Signed-off-by: Sachitanand Shelake <sachitanand.shelake@seagate.com>
    
    * Added lnet validation and configuration in init.sh
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * Added sleep
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    Co-authored-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    Co-authored-by: nileshgovande <63101371+nileshgovande@users.noreply.github.com>

--- scripts/env/dev/init.sh
+++ scripts/env/dev/init.sh
@@ -143,6 +143,9 @@ else
   unsupported_os
 fi
 
+# validate and configure lnet
+sh ${S3_SRC_DIR}/scripts/env/common/configure_lnet.sh
+
 if [[ $# -eq 0 ]] ; then
   source ${S3_SRC_DIR}/scripts/env/common/setup-yum-repos.sh
   #install pre-requisites on dev vm
@@ -153,7 +156,6 @@ else
           a)
               is_open_source=true
               yum install createrepo -y
-              easy_install pip
               read -p "Git Access Token:" git_access_token
               source ${S3_SRC_DIR}/scripts/env/common/create-cortx-repo.sh -G $git_access_token
               # install configobj
