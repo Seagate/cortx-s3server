4ab6b2091989fc857c333bf70d49de4436e2309a
root <root@ssc-vm-4852.colo.seagate.com>
2021-05-31 18:33:04 -0600

    EOS-20528 : S3 Copy objects STs to verify the sanity of the objects before upload and after download
    Signed-off-by: root <root@ssc-vm-4852.colo.seagate.com>

--- st/clitests/awss3api_spec.py
+++ st/clitests/awss3api_spec.py
@@ -19,6 +19,8 @@
 
 import os
 import yaml
+import hashlib
+import shutil
 from framework import Config
 from framework import S3PyCliTest
 from awss3api import AwsTest
@@ -109,6 +111,119 @@ def create_object_list_file(file_name, obj_list=[], quiet_mode="false"):
 
 def delete_object_list_file(file_name):
     os.remove(file_name)
+
+def get_md5_sum(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+#******** Create Bucket ********
+AwsTest('Aws can create bucket').create_bucket("copyobjectbucket").execute_test().command_is_successful()
+
+# creating dir for files.
+upload_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "copy_object_test_upload")
+os.makedirs(upload_dir, exist_ok=True)
+filesize = [1024, 5120, 32768]
+start = 0
+
+# Create root level files under upload_dir.
+file_list_before_upload = ['1kfile', '5kfile', '32kfile']
+for root_file in file_list_before_upload:
+    file_to_create = os.path.join(upload_dir, root_file)
+    key = root_file
+    with open(file_to_create, 'wb+') as fout:
+        fout.write(os.urandom(filesize[start]))
+    start = start + 1
+
+#calculating md5sum in dict.
+md5sum_dict_before_upload = {}
+for file in file_list_before_upload:
+    path = os.path.join(upload_dir, file)
+    md5sum_dict_before_upload[file] = get_md5_sum(path)
+
+#Recursively upload all file from upload_dir to 'seagatebucket'.
+AwsTest('Aws Upload folder recursively').upload_objects("copyobjectbucket", upload_dir)\
+        .execute_test().command_is_successful()
+
+# Create a new destination bucket.
+AwsTest('Aws can create destination bucket for CopyObject API')\
+    .create_bucket("destination-bucket")\
+    .execute_test().command_is_successful()
+
+#Copy objects to destination bucket.
+AwsTest('Aws can copy object to different bucket')\
+    .copy_object("copyobjectbucket/1kfile", "destination-bucket", "1kfile")\
+    .execute_test().command_is_successful()
+
+AwsTest('Aws can copy object to different bucket')\
+    .copy_object("copyobjectbucket/5kfile", "destination-bucket", "5kfile")\
+    .execute_test().command_is_successful()
+
+AwsTest('Aws can copy object to different bucket')\
+    .copy_object("copyobjectbucket/32kfile", "destination-bucket", "32kfile")\
+    .execute_test().command_is_successful()
+
+#creating download_dir to download objects.
+download_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), "copy_object_test_download")
+os.makedirs(download_dir, exist_ok=True)
+
+#Download keys from destination bucket to local download_dir.
+AwsTest('Aws can Download folder recursively').download_objects("destination-bucket", download_dir)\
+        .execute_test().command_is_successful()
+
+#getting files from download_dir.
+file_list_after_download = os.listdir(download_dir)
+
+#calculating md5sum after downloading the objects.
+md5sum_dict_after_download = {}
+for file in file_list_after_download:
+    path = os.path.join(download_dir, file)
+    md5sum_dict_after_download[file] = get_md5_sum(path)
+
+print("***************Checksum test start****************")
+corruption = False
+for file in md5sum_dict_before_upload:
+    if md5sum_dict_before_upload[file] != md5sum_dict_after_download[file]:
+        corruption = True
+        break
+if corruption:
+    print("***************Checksum test Failed****************")
+else:
+    print("***************Checksum test completed ****************")
+
+#deleting objects from source bucket.
+AwsTest('Aws can delete 1kfile from source bucket').delete_object("copyobjectbucket", "1kfile")\
+    .execute_test().command_is_successful()
+
+AwsTest('Aws can delete 5kfile from source bucket').delete_object("copyobjectbucket", "5kfile")\
+    .execute_test().command_is_successful()
+
+AwsTest('Aws can delete 32kfile from source bucket').delete_object("copyobjectbucket", "32kfile")\
+    .execute_test().command_is_successful()
+
+#deleting source bucket.
+AwsTest('Aws can delete source bucket').delete_bucket("copyobjectbucket")\
+    .execute_test().command_is_successful()
+
+#deleting objects from destination bucket.
+AwsTest('Aws can delete 1kfile from destination bucket').delete_object("destination-bucket", "1kfile")\
+    .execute_test().command_is_successful()
+
+AwsTest('Aws can delete 5kfile from destination bucket').delete_object("destination-bucket", "5kfile")\
+    .execute_test().command_is_successful()
+
+AwsTest('Aws can delete 32kfile from destination bucket').delete_object("destination-bucket", "32kfile")\
+    .execute_test().command_is_successful()
+
+#deleting destination bucket.
+AwsTest('Aws can delete destination bucket').delete_bucket("destination-bucket")\
+    .execute_test().command_is_successful()
+
+#removing the directories.
+shutil.rmtree(upload_dir)
+shutil.rmtree(download_dir)
 
 #******** Create Bucket ********
 AwsTest('Aws can create bucket').create_bucket("seagatebucket").execute_test().command_is_successful()
