8b5c019a205dad7b697e5322ad1c29a746a9c649
Shalaka Dharap <shalaka.dharap@seagate.com>
2021-06-08 22:53:27 -0600

    CopyObject Tagging fix
    Signed-off-by: Amit Kumar <amit.kumar@seagate.com>

--- auth/server/src/main/java/com/seagates3/policy/PolicyAuthorizedS3Actions.java
+++ auth/server/src/main/java/com/seagates3/policy/PolicyAuthorizedS3Actions.java
@@ -13,42 +13,51 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
-public class PolicyAuthorizedS3Actions {
+public
+class PolicyAuthorizedS3Actions {
 
-	private static final String RESTRICTED_PUBLIC_ACCESS_ACTIONS = "/PolicyAuthorizedS3Actions.json";
+ private
+  static final String RESTRICTED_PUBLIC_ACCESS_ACTIONS =
+      "/PolicyAuthorizedS3Actions.json";
 
-	private static Set<String> actionsSet = new HashSet<>();
-	private static final Logger LOGGER = LoggerFactory.getLogger(PolicyAuthorizedS3Actions.class.getName());
-	private static PolicyAuthorizedS3Actions instance = null;
+ private
+  static Set<String> actionsSet = new HashSet<>();
+ private
+  static final Logger LOGGER =
+      LoggerFactory.getLogger(PolicyAuthorizedS3Actions.class.getName());
+ private
+  static PolicyAuthorizedS3Actions instance = null;
 
-	private PolicyAuthorizedS3Actions() {
-		init();
-	}
+ private
+  PolicyAuthorizedS3Actions() { init(); }
 
-	public static PolicyAuthorizedS3Actions getInstance() {
-		if (instance == null) {
-			instance = new PolicyAuthorizedS3Actions();
-		}
-		return instance;
-	}
+ public
+  static PolicyAuthorizedS3Actions getInstance() {
+    if (instance == null) {
+      instance = new PolicyAuthorizedS3Actions();
+    }
+    return instance;
+  }
 
-	public static void init() {
-		try {
-			InputStream in = PolicyAuthorizedS3Actions.class.getResourceAsStream(RESTRICTED_PUBLIC_ACCESS_ACTIONS);
-			InputStreamReader reader = new InputStreamReader(in, "UTF-8");
-			JsonParser jsonParser = new JsonParser();
-			JsonObject element = (JsonObject) jsonParser.parse(reader);
-			for (JsonElement action : element.get("actions").getAsJsonArray()) {
-				actionsSet.add(action.getAsString());
-			}
-		} catch (IOException e) {
-			LOGGER.error("Exception during json parsing", e);
-		}
-	}
+ public
+  static void init() {
+    try {
+      InputStream in = PolicyAuthorizedS3Actions.class.getResourceAsStream(
+          RESTRICTED_PUBLIC_ACCESS_ACTIONS);
+      InputStreamReader reader = new InputStreamReader(in, "UTF-8");
+      JsonParser jsonParser = new JsonParser();
+      JsonObject element = (JsonObject)jsonParser.parse(reader);
+      for (JsonElement action : element.get("actions").getAsJsonArray()) {
+        actionsSet.add(action.getAsString());
+      }
+    }
+    catch (IOException e) {
+      LOGGER.error("Exception during json parsing", e);
+    }
+  }
 
-	public boolean isOnlyPolicyAuthorizationRequired(String action) {
-		return actionsSet.contains(action);
-	}
-
+ public
+  boolean isOnlyPolicyAuthorizationRequired(String action) {
+    return actionsSet.contains(action);
+  }
 }
-
