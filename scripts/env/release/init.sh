3def70a16cef23750dc46b8967c2e10cb8080cf7
vimalraghwani20 <68695688+vimalraghwani20@users.noreply.github.com>
2021-06-10 18:31:23 +0530

    EOS-21137: Removed python numpy 1.19.5 package (#971)
    * EOS-21137: Removed python numpy 1.19.5 package
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-21137: Removed all py-utils dependent packages.
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>

--- scripts/env/release/init.sh
+++ scripts/env/release/init.sh
@@ -57,6 +57,11 @@ install_cortx_py_utils() {
   rpm2cpio cortx-py-utils-*.rpm | cpio -idv "./opt/seagate/cortx/utils/conf/python_requirements.txt"
   rpm2cpio cortx-py-utils-*.rpm | cpio -idv "./opt/seagate/cortx/utils/conf/python_requirements.ext.txt"
 
+  # uninstall cortx-py-utils prerequisite
+  # This will help to remove dependent packages which are not compatible with cortx-py-utils
+  pip3 uninstall -r "$PWD/opt/seagate/cortx/utils/conf/python_requirements.txt" -y
+  pip3 uninstall -r "$PWD/opt/seagate/cortx/utils/conf/python_requirements.ext.txt" -y
+
   # install cortx-py-utils prerequisite
   pip3 install -r "$PWD/opt/seagate/cortx/utils/conf/python_requirements.txt" --ignore-installed
   pip3 install -r "$PWD/opt/seagate/cortx/utils/conf/python_requirements.ext.txt" --ignore-installed
