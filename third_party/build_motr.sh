107797881b99b27e03fcc41fbc46c0237457a9e7
SeagateChaDeepak <68716704+SeagateChaDeepak@users.noreply.github.com>
2021-06-14 12:56:02 +0530

    Simple GET and PUT working Integration (#974)
    * S3:EOS-19713 Tests passing for write true.
    
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>
    
    * Sync of DI flags on all config files
    
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>
    
    * Framework for R2 DI test and stuff
    
    * Rebase and Start for Stub
    
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>
    
    * Code for test added
    
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>
    
    * S3:DI_R2 Changes for GET Operation.
    
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>
    
    * Code Formatting
    
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>
    
    * Adding Test
    
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>
    
    * Store and Retrieve Changes.
    
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>
    
    * Print Statement removal
    
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>
    
    * Added unsafe attributes.
    
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>
    
    Co-authored-by: Nilesh Govande <nilesh.govande@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -5,6 +5,7 @@ cd motr
 # branch: master commit: 70701eea1dd1e20fd7c6bdc5bdd1203795d09958
 
 cd motr
+git checkout EOS-20955 
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 PROCESSORS_AVAILABLE=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
