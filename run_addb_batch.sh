f1fdb98833ef4fae7eca9e3b451017c4978f078e
Dmitrii Surnin <dmitrii.surnin@seagate.com>
2021-06-28 09:32:01 -0600

    Add addb tests runners
    Signed-off-by: Dmitrii Surnin <dmitrii.surnin@seagate.com>

---
+++ run_addb_batch.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+set -ex
+
+addb_dir="/var/log/addb_res/"
+s3bpath="../s3bench/s3bench"
+
+s3bpath=$(realpath -e "$s3bpath")
+mkdir -p "$addb_dir"
+
+test_plan=$(cat <<-EOF
+[
+{"clients": 1, "size": "100b", "reads": 10000},
+{"clients": 5, "size": "10Kb", "reads": 10000}]
+EOF
+         )
+
+tests_num=$(echo "$test_plan" |  jq -r ". | length")
+
+ext="-o ${addb_dir}/test_report.csv -t csv"
+
+for i in $(seq 0 $(( $tests_num - 1 )))
+do
+    clients=$(echo "$test_plan" |  jq -r ".[$i].clients")
+    size=$(echo "$test_plan" |  jq -r ".[$i].size")
+    reads=$(echo "$test_plan" |  jq -r ".[$i].reads")
+
+    echo "i=$i clients=$clients size=$size reads=$reads"
+    ./run_addb_test.sh --s3bench_path "$s3bpath" --addb_path "$addb_dir" -c $clients -s $size -r $reads --s3bench_ext $ext
+
+    ext="-o ${addb_dir}/test_report.csv -t csv+"
+done
---
+++ run_addb_test.sh
@@ -0,0 +1,55 @@
+#!/usr/bin/env bash
+
+set -ex
+
+s3bench_path=${s3bench_path:-"../s3bench/s3bench"}
+addb_path=${addb_path:-"/var/log/addb_res/"}
+
+s3bench_ext=""
+clients=0
+size=""
+reads=0
+
+
+while [ "$1" != "" ]; do
+    case "$1" in
+        --s3bench_path ) shift;
+                         s3bench_path=$(realpath -e "$1")
+                         ;;
+        --addb_path ) shift;
+                      addb_path="$1"
+                      ;;
+        --s3bench_ext ) shift;
+                        s3bench_ext="$1"
+                        ;;
+        -c ) shift;
+             clients=$1
+             ;;
+        -s ) shift;
+             size=$1
+             ;;
+        -r ) shift;
+             reads=$1
+             ;;
+        * ) echo "Invalid argument passed";
+            exit 1
+            ;;
+    esac
+    shift
+done
+
+addb_path=$(realpath -e "$addb_path")
+dt=$(date +"%s")
+test_name="addb_c${clients}_s${size}_cnt${reads}_d${dt}"
+
+test_dir="$addb_path/$test_name"
+mkdir -p "$test_dir"
+
+s3b="$s3bench_path -endpoint https://s3.seagate.com -label $test_name -skipCleanup -numClients $clients -numSamples $clients -objectSize $size -sampleReads $reads $s3bench_ext"
+
+./jenkins-build.sh --skip_build --skip_tests --restart_haproxy --fake_obj --fake_kvs
+
+eval "$s3b"
+
+./jenkins-build.sh --cleanup_only --remove_m0trace --collect_addb "$test_dir"
+cp "./s3bench-${test_name}.log" "$test_dir"
