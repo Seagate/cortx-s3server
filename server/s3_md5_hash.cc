3d599056986f1f722657646da6445099f1874889
Rajesh Nambiar <rajesh.nambiar@seagate.com>
2021-06-29 16:00:45 +0530

    EOS-19710: Avoid MD5 calls and other changes (#1013)
    Avoid MD5 calls, port multipart etag to here, change unit size to atleast 16k
    
    Signed-off-by: Rajesh Nambiar <rajesh.nambiar@seagate.com>

--- server/s3_md5_hash.cc
+++ server/s3_md5_hash.cc
@@ -21,12 +21,14 @@
 #include "base64.h"
 
 #include "s3_md5_hash.h"
+#include "motr_helpers.h"
 #include "s3_option.h"
 #include "s3_log.h"
 #include <assert.h>
 
-MD5hash::MD5hash(bool call_init) { Reset(call_init); }
-MD5hash::MD5hash(std::shared_ptr<MotrAPI> motr_api) { Reset(motr_api); }
+MD5hash::MD5hash(std::shared_ptr<MotrAPI> motr_api, bool call_init) {
+  Reset(motr_api, call_init);
+}
 
 void MD5hash::save_motr_unit_checksum(unsigned char *current_digest) {
   memcpy((void *)&md5ctx, (void *)current_digest, sizeof(MD5_CTX));
@@ -44,13 +46,19 @@ void *MD5hash::get_prev_write_checksum() { return (void *)&md5ctx; }
 bool MD5hash::is_checksum_saved() { return checksum_saved; }
 
 int MD5hash::Update(const char *input, size_t length) {
-  if (input == NULL) {
+  if (input == NULL || status < 0) {
     return -1;
   }
-  if (status > 0) {
-    status = MD5_Update(&md5ctx, input, length);
-  }
-  if (status < 1) {
+  pi_update_bufvec.ov_buf[0] = (void *)input;
+  pi_update_bufvec.ov_vec.v_count[0] = length;
+  pi_update_bufvec.ov_vec.v_nr = 1;
+  struct m0_md5_inc_context_pi s3_update_pi;
+  s3_update_pi.hdr.pi_type = M0_PI_TYPE_MD5_INC_CONTEXT;
+  memcpy(s3_update_pi.prev_context, &md5ctx, sizeof(MD5_CTX));
+  status = s3_motr_api->motr_client_calculate_pi(
+      (struct m0_generic_pi *)&s3_update_pi, NULL, &pi_update_bufvec,
+      M0_PI_SKIP_CALC_FINAL, (unsigned char *)&md5ctx, NULL);
+  if (status < 0) {
     return -1;  // failure
   }
   return 0;  // success
@@ -58,12 +66,12 @@ int MD5hash::Update(const char *input, size_t length) {
 
 int MD5hash::s3_motr_client_calculate_pi(m0_generic_pi *pi,
                                          struct m0_pi_seed *seed,
-                                         m0_bufvec *pi_bufvec,
+                                         m0_bufvec *s3pi_bufvec,
                                          enum m0_pi_calc_flag pi_flag,
                                          unsigned char *current_digest,
                                          unsigned char *pi_value_without_seed) {
   int rc = s3_motr_api->motr_client_calculate_pi(
-      pi, seed, pi_bufvec, pi_flag, current_digest, pi_value_without_seed);
+      pi, seed, s3pi_bufvec, pi_flag, current_digest, pi_value_without_seed);
   return rc;
 }
 
@@ -141,34 +149,23 @@ int MD5hash::Finalize() {
   if (is_finalized) {
     return 0;
   }
-  if (!native_call) {
-    if (!is_initialized) {
-      status = s3_motr_api->motr_client_calculate_pi(
-          (struct m0_generic_pi *)&s3_md5_inc_digest_pi, NULL, &pi_bufvec,
-          M0_PI_CALC_UNIT_ZERO, (unsigned char *)&md5ctx, NULL);
-      if (status < 0) {
-        return -1;  // failure
-      }
-    }
-    memcpy(s3_md5_inc_digest_pi.prev_context, &md5ctx, sizeof(MD5_CTX));
+  pi_bufvec.ov_vec.v_nr = 0;
+  if (!is_initialized) {
     status = s3_motr_api->motr_client_calculate_pi(
-        (struct m0_generic_pi *)&s3_md5_inc_digest_pi, NULL, &pi_bufvec, flag,
-        (unsigned char *)&md5ctx, md5_digest);
+        (struct m0_generic_pi *)&s3_md5_inc_digest_pi, NULL, &pi_bufvec,
+        M0_PI_CALC_UNIT_ZERO, (unsigned char *)&md5ctx, NULL);
     if (status < 0) {
       return -1;  // failure
     }
-  } else {
-    if (!is_initialized) {
-      status = MD5_Init(&md5ctx);
-    }
-    if (status > 0) {
-      status = MD5_Final(md5_digest, &md5ctx);
-      if (status < 1) {
-        return -1;  // failure
-      }
-    } else {
-      return -1;
-    }
+  }
+  memcpy(s3_md5_inc_digest_pi.prev_context, &md5ctx, sizeof(MD5_CTX));
+  // Pass M0_PI_SKIP_CALC_FINAL flag, finalized gets called and stored in
+  // md5_digest (without seed)
+  status = s3_motr_api->motr_client_calculate_pi(
+      (struct m0_generic_pi *)&s3_md5_inc_digest_pi, NULL, &pi_bufvec,
+      M0_PI_SKIP_CALC_FINAL, (unsigned char *)&md5ctx, md5_digest);
+  if (status < 0) {
+    return -1;  // failure
   }
   is_finalized = true;
   return 0;
@@ -178,26 +175,29 @@ void MD5hash::Finalized() { is_finalized = true; }
 
 void MD5hash::Initialized() { is_initialized = true; }
 
-void MD5hash::Reset(bool call_init) {
-  if (call_init) {
-    status = MD5_Init(&md5ctx);
-    native_call = true;
-    is_initialized = true;
-  }
-  is_finalized = false;
-}
-
-void MD5hash::Reset(std::shared_ptr<MotrAPI> motr_api) {
+void MD5hash::Reset(std::shared_ptr<MotrAPI> motr_api,
+                    bool call_init_for_update) {
   if (motr_api) {
-    s3_motr_api = std::move(motr_api);
+    s3_motr_api = motr_api;
   } else {
     s3_motr_api = std::make_shared<ConcreteMotrAPI>();
   }
+  status = 0;
   s3_md5_inc_digest_pi = {0};
   seed = {0};
   s3_md5_inc_digest_pi.hdr.pi_type = M0_PI_TYPE_MD5_INC_CONTEXT;
+  pi_update_bufvec = {0};
   pi_bufvec.ov_vec.v_nr = 0;
-  flag = (m0_pi_calc_flag)M0_PI_SKIP_CALC_FINAL;
+  if (call_init_for_update) {
+    pi_update_bufvec.ov_vec.v_nr = 0;
+    pi_update_bufvec.ov_vec.v_count =
+        (m0_bcount_t *)calloc(1, sizeof(m0_bcount_t));
+    pi_update_bufvec.ov_buf = (void **)calloc(1, sizeof(void *));
+    status = s3_motr_api->motr_client_calculate_pi(
+        (struct m0_generic_pi *)&s3_md5_inc_digest_pi, NULL, &pi_update_bufvec,
+        M0_PI_CALC_UNIT_ZERO, (unsigned char *)&md5ctx, NULL);
+    is_initialized = true;
+  }
   is_finalized = false;
 }
 
@@ -224,4 +224,13 @@ std::string MD5hash::get_md5_base64enc_string() {
     return std::string();  // failure
   }
   return base64_encode(md5_digest, MD5_DIGEST_LENGTH);
+}
+
+MD5hash::~MD5hash() {
+  if (pi_update_bufvec.ov_vec.v_count) {
+    free(pi_update_bufvec.ov_vec.v_count);
+  }
+  if (pi_update_bufvec.ov_buf) {
+    free(pi_update_bufvec.ov_buf);
+  }
 }
