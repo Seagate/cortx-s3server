27dd1c9fe59d86461d10b8f360dd051e3b93087e
Deepak Mahale <deepak.mahale@seagate.com>
2021-07-12 02:34:26 -0600

    S3:EOS-21376 buildmotr.sh changes.
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -2,12 +2,8 @@
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
 # branch: master commit: 70701eea1dd1e20fd7c6bdc5bdd1203795d09958
-rm -rf motr
-git clone --recursive https://github.com/imvenkip/cortx-motr-1.git
-mv cortx-motr-1 motr
->>>>>>> S3:EOS-21169 C++ changes for bg delete rest api
+
 cd motr
-git checkout EOS-20930-1
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 PROCESSORS_AVAILABLE=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
