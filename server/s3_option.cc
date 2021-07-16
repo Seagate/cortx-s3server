3a2411e3fb730ece00af0a5c4302706bde09ebb7
vimalraghwani20 <68695688+vimalraghwani20@users.noreply.github.com>
2021-07-16 11:03:24 +0530

    EOS:19646, EOS-22166 - S3 Server IEM Framework  (#1011)
    * EOS-19646: Added script to configure IEM on dev VM
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Added read me for configure IEM
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Initial code changes for S3 IEM Framework
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Added port in to config file
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Added call to s3_iem
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Fixed code formating
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Code check fix
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: UT
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Force alert generated
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Message format changed to actual
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Fixed the json string
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-22166: Readme updated
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-22166: Added #ifdef for IEM framework
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: code check fixed
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-22166: Review comments addressed
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-22166: Code check format
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Fixed minor issue
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-19646: Reverted the mock IEM
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>

--- server/s3_option.cc
+++ server/s3_option.cc
@@ -112,6 +112,9 @@ bool S3Option::load_section(std::string section_name,
                                "S3_AUDIT_LOGGER_KAFKA_WEB_PATH");
       audit_logger_kafka_web_path =
           s3_option_node["S3_AUDIT_LOGGER_KAFKA_WEB_PATH"].as<std::string>();
+      s3_iem_host = s3_option_node["S3_IEM_HOST"].as<std::string>();
+      s3_iem_port = s3_option_node["S3_IEM_PORT"].as<int>();
+      s3_iem_path = s3_option_node["S3_IEM_PATH"].as<std::string>();
       S3_OPTION_ASSERT_AND_RET(s3_option_node, "S3_LOG_FILE_MAX_SIZE");
       log_file_max_size_mb = s3_option_node["S3_LOG_FILE_MAX_SIZE"].as<int>();
       S3_OPTION_ASSERT_AND_RET(s3_option_node, "S3_LOG_ENABLE_BUFFERING");
@@ -1036,6 +1039,12 @@ std::string S3Option::get_audit_logger_rsyslog_msgid() {
 std::string S3Option::get_audit_logger_kafka_web_path() {
   return audit_logger_kafka_web_path;
 }
+
+std::string S3Option::get_iem_host() { return s3_iem_host; }
+
+int S3Option::get_iem_port() { return s3_iem_port; }
+
+std::string S3Option::get_iem_path() { return s3_iem_path; }
 
 unsigned short S3Option::get_s3_grace_period_sec() {
   return s3_grace_period_sec;
