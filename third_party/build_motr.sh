7b78a26ff52e2e126d06ca08ff45554a1cb3f23c
Nilesh Govande <nilesh.govande@seagate.com>
2021-07-19 23:13:06 -0600

    Graduating to new motr version
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -xe
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
-# branch: master commit: 2ca587c61fdbd2b2b71c79ab0ebd8761e496f3ab
+# branch: master commit: 7ca7a3858fafaea8cc66363d2ae0ff9e8897dfd2
 
 cd motr
-git checkout EOS-20955 
+# git checkout EOS-20955 
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 PROCESSORS_AVAILABLE=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
