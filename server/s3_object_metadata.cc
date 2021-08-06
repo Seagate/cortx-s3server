c28817c3c0235c8f10ad320c13c437217b0d6fb0
Pranav Diliprao Pawar <68326838+pranavPawar07@users.noreply.github.com>
2021-08-06 09:58:20 +0530

    [EOS-23164] : [Multipart Restructure] S3 multipart upload (Redesign) results in metadata "size" with value 0 (#1080)
    * changed size from 0 to content_length
    
    Signed-off-by: Pranav Diliprao Pawar <pranav.pawar@seagate.com>
    
    * added comment
    
    Signed-off-by: Pranav Diliprao Pawar <pranav.pawar@seagate.com>

--- server/s3_object_metadata.cc
+++ server/s3_object_metadata.cc
@@ -225,7 +225,9 @@ S3ObjectMetadata::S3ObjectMetadata(
     mote_kv_writer_factory = std::make_shared<S3MotrKVSWriterFactory>();
   }
   obj_type = S3ObjectMetadataType::simple;
-  primary_obj_size = 0;
+  // This will be initially object size, in case of object fragment this will be
+  // fragment size.
+  primary_obj_size = request->get_content_length();
   initialize(is_multipart, upload_id);
 }
 
