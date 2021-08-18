b936a30d3257ccda1e59197b22bd5d06c31ee25d
SeagateChaDeepak <68716704+SeagateChaDeepak@users.noreply.github.com>
2021-08-18 10:04:28 +0530

    Code Formatting Changes (#1096)
    Signed-off-by: Deepak Mahale <deepak.mahale@seagate.com>

--- server/s3_motr_context.cc
+++ server/s3_motr_context.cc
@@ -223,20 +223,20 @@ struct s3_motr_rw_op_context *create_basic_rw_op_ctx(
            __func__, motr_buf_count, unit_size, allocate_bufs);
     return NULL;
   }
-    rc = m0_bufvec_alloc(ctx->pi_bufvec, buffers_per_motr_unit, sizeof(void *));
-    if (rc != 0) {
-      s3_bufvec_free_aligned(ctx->data, unit_size, allocate_bufs);
-      free(ctx->data);
-      free(ctx->ext);
-      free(ctx->pi_bufvec);
-      free(ctx);
-      s3_log(S3_LOG_FATAL, "", "%s Exit with NULL - possible out-of-memory\n",
-             __func__);
-      return NULL;
-    }
+  rc = m0_bufvec_alloc(ctx->pi_bufvec, buffers_per_motr_unit, sizeof(void *));
+  if (rc != 0) {
+    s3_bufvec_free_aligned(ctx->data, unit_size, allocate_bufs);
+    free(ctx->data);
+    free(ctx->ext);
+    free(ctx->pi_bufvec);
+    free(ctx);
+    s3_log(S3_LOG_FATAL, "", "%s Exit with NULL - possible out-of-memory\n",
+           __func__);
+    return NULL;
+  }
 
-    rc = m0_bufvec_alloc(ctx->attr, motr_checksums_buf_count,
-                         get_sizeof_pi_info(ctx));
+  rc = m0_bufvec_alloc(ctx->attr, motr_checksums_buf_count,
+                       get_sizeof_pi_info(ctx));
 #if 0
   } else {
     rc = m0_bufvec_alloc(ctx->attr, motr_buf_count, 1);
