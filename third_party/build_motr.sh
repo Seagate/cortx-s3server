bd72038f29d3b79a435af9a46f6e02f429b1f8c6
Dattaprasad <dattaprasad.govekar@seagate.com>
2021-09-17 02:57:57 -0600

    EOS-24938: Setting M0_ENF_META before entity open/delete Motr calls - Raised Motr revision to 345023e777881895962e5af3d392722879423cbf
    Signed-off-by: Dattaprasad <dattaprasad.govekar@seagate.com>
    Signed-off-by: Dattaprasad Govekar <dattaprasad.govekar@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -1,9 +1,10 @@
 #!/bin/sh -xe
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
-# motr commit: 3a429ad5 (main branch)
+# motr commit: 345023e777881895962e5af3d392722879423cbf (main branch)
 
 cd motr
+
 # Uncomment following line to compile motr with both KVS and Cassandra
 # export CONFIGURE_OPTS=--with-cassandra
 PROCESSORS_AVAILABLE=$(cat /proc/cpuinfo | grep '^processor' | wc -l)
