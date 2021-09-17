721252b80591d3e2cbe9a4ab6e537e7e0674990d
shalakadharap <64534897+shalakadharap@users.noreply.github.com>
2021-09-17 19:36:12 +0530

    EOS23401- Handling for --service parameter in s3 provisioning (#1282)
    * EOS23401- Handling for --service parameter in s3 provisioning
    
    * Fixed
    
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>
    
    * Fixed
    
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>
    
    * Fixed
    
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>
    
    Co-authored-by: Nilesh Govande <nilesh.govande@seagate.com>

--- scripts/provisioning/preparecmd.py
+++ scripts/provisioning/preparecmd.py
@@ -26,16 +26,16 @@ class PrepareCmd(SetupCmd):
   """Prepare Setup Cmd."""
   name = "prepare"
 
-  def __init__(self, config: str, module: str = None):
+  def __init__(self, config: str, services: str = None):
     """Constructor."""
     try:
-      super(PrepareCmd, self).__init__(config, module)
+      super(PrepareCmd, self).__init__(config, services)
     except Exception as e:
       raise e
 
   def process(self):
     """Main processing function."""
-    self.logger.info(f"Processing phase = {self.name}, config = {self.url}, module = {self.module}")
+    self.logger.info(f"Processing phase = {self.name}, config = {self.url}, service = {self.services}")
     self.logger.info("validations started")
     self.phase_prereqs_validate(self.name)
     self.phase_keys_validate(self.url, self.name)
