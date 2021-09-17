de1442ce393f764b0d13f7e2467a962e1eca3ab5
MOHAMMED SHAHID <mohammed.shahid@seagate.com>
2021-09-17 10:55:00 +0530

    EOS-24655 - Kubernetes haproxy micro-provisioner code changes. (#1261)
    * EOS-24655 - Kubernetes haproxy micro-provisioner code changes.
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * Codacy warning fix.
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * EOS-24655: Update for the logic; add 5 s3 instances.
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * Add s3 bgdelete contents to haproxy config.
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * EOS-24655: remove unneeded import
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24655 - Instead of hard-coding, update the s3 server instances
                based on confstore value.
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * EOS-24655 - Fix codacy bug.
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * EOS-24655: fix error in success condition on PEM copying
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    Co-authored-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>

---
+++ scripts/provisioning/starthaproxy.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -x
+#
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+# Copy the SSL certificate file
+if ! [ -f "/etc/cortx/s3/stx/stx.pem" ]; then
+  if ! ( mkdir -p /etc/cortx/s3/stx/ && \
+         cp /etc/ssl/stx/stx.pem /etc/cortx/s3/stx/stx.pem ) \
+  then
+    echo "Failed to update SSL cert file /etc/cortx/s3/stx/stx.pem from /etc/ssl/stx/stx.pem."
+    exit 1
+  fi
+fi
+
+# Run the configured haproxy
+/usr/sbin/haproxy -Ws -f /etc/cortx/s3/haproxy.cfg -p /run/haproxy.pid 1>/var/log/cortx/s3/haproxy.log 2>&1
