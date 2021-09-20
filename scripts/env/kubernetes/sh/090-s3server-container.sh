c59f423af75309fc77a02ba26e3c7037234442b6
Ivan Tishchenko <ivan.tishchenko@seagate.com>
2021-09-20 12:49:25 +0300

    EOS-24857: Initial K8s automation - single node deployment (#1290)
    * EOS-24857: add kubernetes installation script
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: add script to fetch cortx-all image
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: add script to provision OpenLDAP
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: add script to create IO POD
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: add validation of VM stats
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: fix bug - node label command used wrong variable
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: Fix error in haproxy startup -- it was overwriting logs on every start
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: Add haproxy configuration steps
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: minimize user interaction
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: add PV and PVC definitions for /etc/cortx, /var/log/cortx, /var/data/cortx
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: Add separate configuration for motr image and s3 image.
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: Add script for AuthServer container config&start
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: Add script for Motr container config&start
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: Add script for S3 Server container config&start
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24857: Add scripts to configure aws s3 client and do basic IO test.
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>
    
    * EOS-24974: Move S3 containers configuration outside the container
    
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>

---
+++ scripts/env/kubernetes/sh/090-s3server-container.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+set -e # exit immediatly on errors
+
+source ./config.sh
+source ./env.sh
+source ./sh/functions.sh
+
+set -x # print each statement before execution
+
+add_separator CONFIGURING s3server CONTAINER.
+
+kube_run() {
+  kubectl exec -i depl-pod -c s3server -- "$@"
+}
+
+#kube_run sh -c 'echo "211072f61c4b4949839c624d6ed95115" > /etc/machine-id'
+
+#kube_run sh -c '/opt/seagate/cortx/s3/bin/s3_start --service s3server --index 1 &>/root/entry-point.log &'
+
+add_separator Waiting for Motr to become available
+
+while sleep 2; do
+  if [ -f /var/data/cortx/motr-hare-is-up.txt ]; then
+    break
+  fi
+  kubectl get pod depl-pod
+  kubectl exec -i depl-pod -c hare-motr -- ps ax | grep m0d
+done
+
+add_separator Checking if S3 is up
+
+sleep 5
+
+set +x
+if [ "`kube_run ps ax | grep 's3server --s3pidfile' | wc -l`" -ne 1 ]; then
+  echo
+  kubectl logs depl-pod s3server
+  echo
+  kube_run ps ax
+  echo
+  add_separator FAILED.  S3 Server does not seem to be running properly.
+  false
+fi
+set -x
+
+add_separator SUCCESSFULLY CONFIGURED s3server CONTAINER.
