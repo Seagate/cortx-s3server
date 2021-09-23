e56f744ae39e9a1f1921333df61e4237ff33ba41
Ivan Tishchenko <ivan.tishchenko@seagate.com>
2021-09-23 07:08:06 -0600

    EOS-24974: Clean up stale code
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>

--- scripts/env/kubernetes/sh/045-prepare-s3-containers-configs.sh
+++ scripts/env/kubernetes/sh/045-prepare-s3-containers-configs.sh
@@ -91,59 +91,9 @@ kube_run "$src_dir/s3server/s3-mini-prov.sh"
 # 'manual' step for machine-id (until proper solution is merged) FIXME
 kube_run sh -c 'cat /etc/machine-id > /etc/cortx/s3/machine-id'
 
-# ##############
-# # machine ID #
-# ##############
-# mkdir -p /etc/cortx/s3
-# echo "211072f61c4b4949839c624d6ed95115" > /etc/cortx/s3/machine-id
-# 
-# 
-# ###########
-# # haproxy #
-# ###########
-# 
-# mkdir -p /etc/cortx/s3/ \
-#          /etc/cortx/s3/stx/ \
-#          /etc/cortx/s3/haproxy/errors/
-# 
-# cp haproxy/haproxy.cfg /etc/cortx/s3/haproxy.cfg
-# cp haproxy/503.http    /etc/cortx/s3/haproxy/errors/503.http
-# cp haproxy/stx.pem     /etc/cortx/s3/stx/stx.pem
-# 
-# 
-# ##############
-# # authserver #
-# ##############
-# 
-# mkdir -p /etc/cortx/auth/resources
-# cp -r "$s3_repo_dir"/auth/resources/* /etc/cortx/auth/resources/
-# 
-# cp /etc/cortx/auth/resources/authserver.properties.sample \
-#    /etc/cortx/auth/resources/authserver.properties
-# cp /etc/cortx/auth/resources/keystore.properties.sample \
-#    /etc/cortx/auth/resources/keystore.properties
-# 
-# # Update needed properties in Auth config
-# 
-# # Generate encrypted ldap password for sgiam-admin on Pod
-# 
-# const_key=`kube_run s3cipher generate_key --const_key cortx`
-# encrypted_pwd=`kube_run  s3cipher encrypt --data "ldapadmin" --key "$const_key"`
-# 
-# # Copy encrypted ldap-passsword (ldapLoginPW) and openldap endpoint (key name
-# # ldapHost) to authserver.properties file.
-# 
-# sed -i "s|^ldapLoginPW *=.*|ldapLoginPW=$encrypted_pwd|;
-#         s|^ldapHost *=.*|ldapHost=$OPENLDAP_SVC|" \
-#     /etc/cortx/auth/resources/authserver.properties
-# 
 # #############
 # # S3 server #
 # #############
-# 
-# mkdir -p /etc/cortx/s3/conf
-# cp "$s3_repo_dir"/s3config.release.yaml.sample \
-#    /etc/cortx/s3/conf/s3config.yaml
 
 # Increase retry interval
 sed -i \
@@ -162,14 +112,6 @@ cat s3server/s3server-5 > /etc/cortx/s3/s3server-5
 # ##################
 # # S3 BG Services #
 # ##################
-# 
-# mkdir -p /var/log/cortx/s3/s3backgrounddelete \
-#          /etc/cortx/s3/s3backgrounddelete/
-# 
-# cp "$s3_repo_dir/s3backgrounddelete/s3backgrounddelete/config/s3_background_delete_config.yaml.sample" \
-#    /etc/cortx/s3/s3backgrounddelete/config.yaml
-# cp "$s3_repo_dir/s3backgrounddelete/s3backgrounddelete/config/s3_cluster.yaml.sample" \
-#    /etc/cortx/s3/s3backgrounddelete/s3_cluster.yaml
 
 set_bg_config_param() {
   key="$1"
@@ -186,21 +128,6 @@ set_bg_config_param  leak_processing_delay_in_mins     1
 set_bg_config_param  version_processing_delay_in_mins  1
 set_bg_config_param  consumer_sleep  1
 set_bg_config_param  purge_sleep     1
-
-# # create bgdelete account
-# 
-# access_key=`kube_run s3cipher generate_key --const_key s3backgroundaccesskey --key_len 22 --use_base64`
-# secret_key=`kube_run s3cipher generate_key --const_key s3backgroundsecretkey --key_len 40`
-# 
-# cat ldif/bgdelete_account.ldif.template | \
-#   sed -e "s,<access-key>,$access_key," \
-#       -e "s,<secret-key>,$secret_key," \
-#   > ldif/bgdelete_account.ldif
-# 
-# ldapadd -x -D "cn=admin,dc=seagate,dc=com" -w ldapadmin -f ldif/bgdelete_account.ldif -H "ldap://$OPENLDAP_SVC"
-# 
-# # create s3 topic 'bgdelete'
-# kube_run python3 "$src_dir/message-bus/create-bgdelete-topic.py"
 
 
 ###########################################################################
