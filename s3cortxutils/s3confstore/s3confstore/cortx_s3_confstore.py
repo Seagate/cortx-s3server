569406d6db8b4983e7217bb3cf20f230a15f565f
Mohammed Shahid <mohammed.shahid@seagate.com>
2021-09-24 01:40:59 -0600

    Changes to template files to accomodate array entries in new format
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>

--- s3cortxutils/s3confstore/s3confstore/cortx_s3_confstore.py
+++ s3cortxutils/s3confstore/s3confstore/cortx_s3_confstore.py
@@ -55,12 +55,12 @@ class S3CortxConfStore:
       # Update the index backend.
       Conf.save(self.default_index)
 
-  def get_all_keys(self):
+  def get_all_keys(self, recurse: bool = True):
     """Get all the key value pairs from confstore."""
     # TODO recurse flag will be deprecated in future.
     # Do changes in all places wherever its applicable
     # refer validate_config_files() and phase_keys_validate() in setupcmd.py
-    return Conf.get_keys(self.default_index, recurse = True)
+    return Conf.get_keys(self.default_index, recurse)
 
   def delete_key(self, key: str, save: bool = False):
     """Deletes the specified key."""
