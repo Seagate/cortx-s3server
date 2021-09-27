55a530bdb86ce2fcb1dfa56ddae671acea5559ea
MOHAMMED SHAHID <mohammed.shahid@seagate.com>
2021-09-27 12:49:42 +0530

    Changes to template files to accomodate array entries in new format (#1312)
    * Changes to template files to accomodate array entries in new format
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * Function call code fix
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * Parameter change in get_all_keys
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * Fixes to mini-prov job
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * Literal eval fix
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>

--- scripts/provisioning/configcmd.py
+++ scripts/provisioning/configcmd.py
@@ -443,14 +443,12 @@ class ConfigCmd(SetupCmd):
     return result
 
   def update_s3_bgdelete_bind_port(self, value_to_update, additional_param):
-    endpoint = self.parse_endpoint(value_to_update)
+    if isinstance(value_to_update, str):
+      value_to_update = literal_eval(value_to_update)
+    endpoint = self.get_endpoint_for_scheme(value_to_update, "http")
     if 'port' not in endpoint:
-      #fetching default value from s3_provisioner private
-      default_value = self.get_confvalue_with_defaults('CONFIG>CONFSTORE_S3_BGDELETE_CONSUMER_ENDPOINT')
-      endpoint = self.parse_endpoint(default_value)
-      if 'port' not in endpoint:
-        raise S3PROVError(f"BG Delete endpoint {value_to_update} does not have port specified.")
-    if ("K8" == str(self.get_confvalue_with_defaults('CONFIG>CONFSTORE_SETUP_TYPE'))) :    
+      raise S3PROVError(f"BG Delete endpoint {value_to_update} does not have port specified.")
+    if ("K8" == str(self.get_confvalue_with_defaults('CONFIG>CONFSTORE_SETUP_TYPE'))) :
       return int(endpoint['port']) -1
     else :
       return int(endpoint['port'])
@@ -582,7 +580,8 @@ class ConfigCmd(SetupCmd):
     self.logger.info("Update s3 bgdelete config file completed")
 
   def update_bgdelete_producer_endpoint(self, value_to_update, additional_param):
-    value_to_update = literal_eval(value_to_update)
+    if isinstance(value_to_update, str):
+      value_to_update = literal_eval(value_to_update)
     endpoint = self.get_endpoint_for_scheme(value_to_update, "http")
     if endpoint is None:
       raise S3PROVError(f"BG Producer endpoint for scheme 'http' is not specified")
