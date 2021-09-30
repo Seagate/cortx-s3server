b3c3312c9ecaba5ed6f7f49da9929047da401aa6
Vimal Raghwani <vimal.raghwani@seagate.com>
2021-09-29 22:46:43 -0600

    validation debug
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>

--- scripts/provisioning/setupcmd.py
+++ scripts/provisioning/setupcmd.py
@@ -372,9 +372,12 @@ class SetupCmd(object):
         key_yard = key_yard.replace("node-id", self.machine_id)
       if "nodes" in key_yard:
         storage_set_count = self.get_confvalue_with_defaults('CONFIG>CONFSTORE_STORAGE_SET_COUNT')
+        self.logger.info(f"storage_set_count : {storage_set_count}")
         index = 0
         while index < int(storage_set_count):
-          key_yard_server_nodes = self.get_confvalue(key_yard.replace("storage-set-count", str(index)))
+          key_yard_server_nodes_key = key_yard.replace("storage-set-count", str(index))
+          self.logger.info(f"key_yard_server_nodes_key : {key_yard_server_nodes_key}")
+          key_yard_server_nodes = self.get_confvalue(key_yard_server_nodes_key)
           if key_yard_server_nodes is None:
             raise Exception("Validation for server_nodes failed")
           index += 1
