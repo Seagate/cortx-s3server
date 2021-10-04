f30fd4acb28b6ff933adae4f98535963ccb47ed6
Ivan Tishchenko <ivan.tishchenko@seagate.com>
2021-10-04 07:54:55 -0600

    EOS-25151: Use locally saved cortx-utils RPM
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>

--- scripts/env/kubernetes/s3server/fix-container.sh
+++ scripts/env/kubernetes/s3server/fix-container.sh
@@ -29,4 +29,5 @@ sed -e 's,get_confkey,get_config,g' -i "/opt/seagate/cortx/s3/bin/s3_start"
 
 # FIXME: use hard-coded cortx-utils version; newer ones have some unknown changes
 rpm -e --nodeps cortx-py-utils
-yum install -y http://cortx-storage.colo.seagate.com/releases/cortx/github/integration-custom-ci/centos-7.9.2009/custom-build-223/cortx_iso/cortx-py-utils-2.0.0-423_411caf9.noarch.rpm
+#yum install -y http://cortx-storage.colo.seagate.com/releases/cortx/github/integration-custom-ci/centos-7.9.2009/custom-build-223/cortx_iso/cortx-py-utils-2.0.0-423_411caf9.noarch.rpm
+yum install -y http://ssc-vm-g2-rhev4-0613.colo.seagate.com/ivan.tishchenko/public2/-/raw/main/saved-rpms/cortx-py-utils-2.0.0-423_411caf9.noarch.rpm
