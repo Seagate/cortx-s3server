c0aee2738e7477037f058243cdb58603c03c4ac6
vimalraghwani20 <68695688+vimalraghwani20@users.noreply.github.com>
2021-10-12 16:30:47 +0530

    EOS-24355: Implementation for the log rotate config files for K8S (#1354)
    * EOS-24355: Inital code changes for the log rotate config files
    
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-24355: remvoed openldap log rotation
    
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-24355: implemented haproxy log rotate config file
    
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-24353: Fixed the logrotation for the s3,m0,addb
    
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-24353: Review comment addressed
    
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * Fix install path failures
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * Trial fix for install path
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * Fix for install path
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * Fixes for install path
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * More fixes for install path
    
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>
    
    * EOS-25330: Fixed the UT issues
    
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-24355: Fixed haproxy for LR
    
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    * EOS-24355 : Fixed haproxy error
    
    Signed-off-by: Vimal Raghwani <vimal.raghwani@seagate.com>
    
    Co-authored-by: Mohammed Shahid <mohammed.shahid@seagate.com>

--- installhelper.sh
+++ installhelper.sh
@@ -73,6 +73,7 @@ mkdir -p $S3_INSTALL_LOCATION/install/ldap/rsyslog.d
 mkdir -p $S3_INSTALL_LOCATION/install/ldap/replication
 mkdir -p $S3_INSTALL_LOCATION/install/ldap/replication/cleanup
 mkdir -p $S3_INSTALL_LOCATION/install/haproxy
+mkdir -p $S3_INSTALL_LOCATION/install/logrotate
 mkdir -p $S3_INSTALL_LOCATION/docs
 mkdir -p $S3_INSTALL_LOCATION/s3backgrounddelete
 mkdir -p $S3_INSTALL_LOCATION/conf
@@ -93,6 +94,9 @@ mkdir -p $S3_MINI_PROV_CFG_LOCATION
 
 # Copy the haproxy dependencies
 cp -R scripts/haproxy/* $S3_INSTALL_LOCATION/install/haproxy
+
+# Copy the logrotate dependencies
+cp -R scripts/s3-logrotate/* $S3_INSTALL_LOCATION/install/logrotate
 
 # Copy the provisioning config
 cp scripts/provisioning/setup.yaml $S3_INSTALL_LOCATION/conf
@@ -192,15 +196,6 @@ cp ./system/s3stopsystem.sh $S3_INSTALL_LOCATION/
 
 # Copy the s3 service file for systemctl multiple instance support.
 cp ./system/s3server@.service $SERVICE_FILE_LOCATION
-
-# Copy the s3 log rotate script for retaining recent modified log files
-cp -f scripts/s3-logrotate/s3logfilerollover.sh $S3_LOG_ROTATE_FILE_LOCATION
-
-# Copy m0trace log rotation script for retaining recent m0trace files
-cp -f scripts/s3-logrotate/s3m0tracelogfilerollover.sh $S3_LOG_ROTATE_FILE_LOCATION
-
-# Copy addb log rotation script for retaining recent addb files
-cp -f scripts/s3-logrotate/s3addblogfilerollover.sh $S3_LOG_ROTATE_FILE_LOCATION
 
 # Copy s3server support bundle script
 cp -f scripts/s3-support-bundles/s3_bundle_generate.sh $S3_INSTALL_LOCATION/scripts/
@@ -332,12 +327,6 @@ cp ./scripts/rsyslog-tcp-audit.conf $RSYSLOG_CFG_DIR_LOCATION
 
 # Copy elasticsearch config
 cp ./scripts/elasticsearch/elasticsearch.conf $RSYSLOG_CFG_DIR_LOCATION
-
-# Copy audit logrotate config
-cp ./scripts/s3-logrotate/s3auditlog $LOGROTATE_CFG_DIR_LOCATION
-
-# Copy openldap logrotate config
-cp ./scripts/ldap/logrotate/openldap $LOGROTATE_CFG_DIR_LOCATION
 
 # Copy the keepalived config
 cp ./scripts/keepalived/keepalived.conf.main $KEEPALIVED_CFG_DIR_LOCATION
---
+++ scripts/haproxy/logrotate/haproxylogfilerollover.sh
@@ -0,0 +1,95 @@
+#!/bin/sh
+#
+# Copyright (c) 2021 Seagate Technology LLC and/or its Affiliates
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# For any questions about this software or licensing,
+# please email opensource@seagate.com or cortx-questions@seagate.com.
+#
+
+# script is used to delete the old haproxy logs in <base_log_dir>/s3/<machine-id>/haproxy/ directory
+# script will retain first origional haproxy log file along with recent modified files of given count and remove rest of haproxy files
+# argument1: <number of latest haproxy log files to retain>
+# Default number of latest log files is 5
+# ./haproxylogfilerollover.sh -n 5
+
+usage() { echo "Usage: bash $(basename "$0")[--help|-h]
+                   [-n haproxyLogFileCount]
+Retain recent modified files of given count and first generated haproxy log file and remove rest of haproxy log files.
+
+where:
+-n            number of latest haproxy log files to retain (Default count for haproxy log files is 5)
+--help|-h     display this help and exit" 1>&2; exit 1; }
+
+# max haproxy files count in each haproxy log directory
+haproxy_log_files_max_count=5
+haproxy_config=$(s3confstore "yaml:///opt/seagate/cortx/s3/mini-prov/s3_prov_config.yaml" getkey --key="S3_HAPROXY_SYSCONF_SYMLINK")
+haproxy_log_file=$(s3confstore "properties://$haproxy_config" getkey --key="LOG_FILE")
+echo "haproxy log file: $haproxy_log_file"
+
+while getopts ":n:" option; do
+    case "${option}" in
+        n)
+            haproxy_log_files_max_count=${OPTARG}
+            if [ -z ${haproxy_log_files_max_count} ]
+            then
+              usage
+            fi
+            ;;
+        *)
+            usage
+            ;;
+    esac
+done
+
+echo "Max haproxy log file count: $haproxy_log_files_max_count"
+echo "Rotating haproxy log files"
+
+haproxy_log_dir=$(dirname $haproxy_log_file)
+echo "haproxy dir: $haproxy_log_dir"
+
+if [ -n "$haproxy_log_dir" ]
+then
+     # Find haproxy log files
+     haproxy_log_files=`find $haproxy_log_dir -maxdepth 1 -type f -name "haproxy.*"`
+     haproxy_log_files_count=`echo "$haproxy_log_files" | grep -v "^$" | wc -l`
+     echo "## found $haproxy_log_files_count file(s) in log directory($haproxy_log_dir) ##"
+     # check log files count is greater than max log file count or not
+     if [ $haproxy_log_files_count -gt $haproxy_log_files_max_count ]
+     then
+         # get files sort by date - oldest will come on top
+         remove_file_count=`expr $haproxy_log_files_count - $haproxy_log_files_max_count`
+         # remove oldest files except first generated haproxy log e.g.5 recent + 1 original file
+         remove_file_count=`expr $remove_file_count - 1`
+         if [ $remove_file_count -gt 0 ]
+         then
+            echo "## ($remove_file_count) haproxy log file(s) can be removed from log directory($haproxy_log_dir) ##"
+            # get the files sorted by time modified (most recently modified comes last), that is oldest files will come on top
+            # ignore first file since we need to maintain first haproxy log file using 'sed' command
+            files_to_remove=`ls -tr "$haproxy_log_dir" | grep haproxy |sed "1 d" | head -n $remove_file_count`
+            for file in $files_to_remove
+            do
+              rm -f "$haproxy_log_dir/$file"
+            done
+            echo "## deleted ($remove_file_count) haproxy log file(s) from log directory($haproxy_log_dir) ##"
+
+         else
+            echo "## No haproxy log files to remove ##1"
+         fi
+     else
+         echo "## No haproxy log files to remove ##2"
+     fi
+else
+    echo "## No haproxy log files to remove ##3"
+fi
