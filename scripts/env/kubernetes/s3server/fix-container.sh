b70d606a8948e6aa1a5d292f0d5e164fec23376b
Ivan Tishchenko <ivan.tishchenko@seagate.com>
2021-10-13 09:01:33 -0600

    EOS-25151: Save utils RPM in the repo itself
    Signed-off-by: Ivan Tishchenko <ivan.tishchenko@seagate.com>

--- scripts/env/kubernetes/s3server/fix-container.sh
+++ scripts/env/kubernetes/s3server/fix-container.sh
@@ -36,5 +36,4 @@ cat /etc/cortx/s3/machine-id > /etc/machine-id
 
 # FIXME: use hard-coded cortx-utils version; newer ones have some unknown changes
 rpm -e --nodeps cortx-py-utils
-#yum install -y http://cortx-storage.colo.seagate.com/releases/cortx/github/integration-custom-ci/centos-7.9.2009/custom-build-223/cortx_iso/cortx-py-utils-2.0.0-423_411caf9.noarch.rpm
-yum install -y http://ssc-vm-g2-rhev4-0613.colo.seagate.com/ivan.tishchenko/public2/-/raw/main/saved-rpms/cortx-py-utils-2.0.0-423_411caf9.noarch.rpm
+yum install -y "$src_dir"/message-bus/cortx-py-utils-2.0.0-423_411caf9.noarch.rpm
