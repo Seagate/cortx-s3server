9b830278510e4a3da360b3eab5e0601da3cdd328
Mohammed Shahid <mohammed.shahid@seagate.com>
2021-10-14 03:55:37 -0600

    Updating s3 to point to latest Motr
    Signed-off-by: Mohammed Shahid <mohammed.shahid@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
-# motr commit: cb34c00521817ef7927261be4eb5d84dc4b530b5 (main branch)
+# motr commit: a2aa1e8b2f3c2908e4fde862340fd45573646d74 (main branch)
 
 cd motr
 
