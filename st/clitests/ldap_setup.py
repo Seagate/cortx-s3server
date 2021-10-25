5a227d53a74786067361520adb750c0db1894a46
Shri Metta <shri.metta@seagate.com>
2021-10-25 18:44:28 +0530

    Updated ldap_setup to fix system test auth_spec_signature_calculation.
    Signed-off-by: Shri Metta <shri.metta@seagate.com>

--- st/clitests/ldap_setup.py
+++ st/clitests/ldap_setup.py
@@ -19,7 +19,8 @@
 
 import os
 import yaml
-from subprocess import call
+from subprocess import call, check_output
+import fileinput
 
 class LdapSetup:
     def __init__(self):
@@ -31,6 +32,15 @@ class LdapSetup:
 
     def ldap_init(self):
         ldap_init_file = os.path.join(self.test_data_dir, 'create_test_data.ldif')
+
+        for line in fileinput.input(ldap_init_file, inplace=True):
+            if 'sk: ' in line:
+                secret_key = line[4:].rstrip()
+                encrypted_secret_key = LdapSetup.__encrypt_secret_key(secret_key)
+                line = f"sk: {encrypted_secret_key}\n"
+
+            print(line, end='')
+
         cmd = "ldapadd -h %s -p %s -w %s -x -D %s -f %s" % (self.ldap_config['host'],
                 self.ldap_config['port'], self.ldap_config['password'],
                 self.ldap_config['login_dn'], ldap_init_file)
@@ -46,6 +56,12 @@ class LdapSetup:
                         self.ldap_config['port'], self.ldap_config['password'],
                         self.ldap_config['login_dn'], entry)
                 obj = call(cmd, shell=True)
+
+    @staticmethod
+    def __encrypt_secret_key(secret_key):
+        encrypt_cmd = ['java', '-jar', '/opt/seagate/cortx/auth/AuthPassEncryptCLI-1.0-0.jar', '-s', secret_key, '-e', 'aes']
+        completed_process = check_output(encrypt_cmd)
+        return completed_process.decode().rstrip()
 
 
 class LdapInfo:
