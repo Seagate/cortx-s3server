7c923c72ca61e1e08aabe6bb975ccec149dbdff9
Sanal Kaarthikeyan <89895724+sanalpk@users.noreply.github.com>
2021-12-08 22:49:48 +0530

    [versioning] EOS-24952: Changes for PutObject API in a Versioning Enabled Bucket (#1637)
    Add basic versioning support for PutObject API
    
    Signed-off-by: Keith Pine <keith.pine@seagate.com>
    
    Co-authored-by: Keith Pine <keith.pine@seagate.com>

--- server/s3_put_object_action.h
+++ server/s3_put_object_action.h
@@ -151,6 +151,10 @@ class S3PutObjectAction : public S3ObjectAction {
   FRIEND_TEST(S3PutObjectActionTest, VaidateSpecialCharTagsCase1);
   FRIEND_TEST(S3PutObjectActionTest, VaidateSpecialCharTagsCase2);
   FRIEND_TEST(S3PutObjectActionTest, FetchBucketInfo);
+  FRIEND_TEST(S3PutObjectActionTest, ValidateObjectKeyLengthPositiveCase);
+  FRIEND_TEST(S3PutObjectActionTest, ValidateObjectKeyLengthNegativeCase);
+  FRIEND_TEST(S3PutObjectActionTest, ValidateUserMetadataLengthNegativeCase);
+  FRIEND_TEST(S3PutObjectActionTest, ValidateMetadataLengthNegativeCase);
   FRIEND_TEST(S3PutObjectActionTest, FetchObjectInfoWhenBucketNotPresent);
   FRIEND_TEST(S3PutObjectActionTest, FetchObjectInfoWhenBucketFailedTolaunch);
   FRIEND_TEST(S3PutObjectActionTest, FetchObjectInfoWhenBucketFailed);
@@ -209,6 +213,11 @@ class S3PutObjectAction : public S3ObjectAction {
   FRIEND_TEST(S3PutObjectActionTest, SendFailedResponse);
   FRIEND_TEST(S3PutObjectActionTest, ConsumeIncomingContentRequestTimeout);
   FRIEND_TEST(S3PutObjectActionTest, DelayedDeleteOldObject);
+  FRIEND_TEST(S3PutObjectActionTest, ValidateMissingContentLength);
+  FRIEND_TEST(S3PutObjectActionTest, AddOidToProbableDeadListVersioningEnabled);
+  FRIEND_TEST(S3PutObjectActionTest,
+              AddOidToProbableDeadListVersioningDisabled);
+  FRIEND_TEST(S3PutObjectActionTest, MarkOldOidForDelValidation);
 };
 
 #endif
