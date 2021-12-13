8b36d98d75b7a2d028556d28c8e1976bf5216821
Nilesh Govande <nilesh.govande@seagate.com>
2021-12-13 07:07:57 -0700

    Motr commit Push
    Signed-off-by: Nilesh Govande <nilesh.govande@seagate.com>

--- third_party/build_motr.sh
+++ third_party/build_motr.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -xe
 # Script to build motr.
 # github repo: https://github.com/Seagate/cortx-motr
-# motr commit: 56d1cc382a67f6384f2a72483a5801c14ad6d99e
+# motr commit: e0cc9a72
 
 cd motr
 
