1e84fe01522a93a2a60a54da692e530c7a0b9ff9
Jeet Jain <jeet.jain@seagate.com>
2021-12-19 22:04:26 -0700

    fixed build error

--- server/s3_addb_map_auto.c
+++ server/s3_addb_map_auto.c
@@ -25,7 +25,7 @@
 
 #include "s3_addb_map.h"
 
-const uint64_t g_s3_to_addb_idx_func_name_map_size = 235;
+const uint64_t g_s3_to_addb_idx_func_name_map_size = 239;
 
 const char* g_s3_to_addb_idx_func_name_map[] = {
     "Action::check_authentication",
