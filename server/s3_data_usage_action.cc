63e437ca225e483fb2b1254d4b60e89ef190b92e
Alexander Voronov <alexander.voronov@seagate.com>
2021-12-22 08:45:13 -0700

    EOS-26626: update S3DataUsageAction to return actual counter
    Signed-off-by: Alexander Voronov <alexander.voronov@seagate.com>

--- server/s3_data_usage_action.cc
+++ server/s3_data_usage_action.cc
@@ -18,14 +18,40 @@
  *
  */
 
-#include "s3_error_codes.h"
-#include "s3_log.h"
+#include <iostream>
+#include <json/json.h>
+
 #include "s3_api_handler.h"
 #include "s3_data_usage_action.h"
-#include <iostream>
+#include "s3_error_codes.h"
+#include "s3_log.h"
+#include "s3_option.h"
 
-S3DataUsageAction::S3DataUsageAction(std::shared_ptr<S3RequestObject> req)
+extern struct s3_motr_idx_layout data_usage_accounts_index_layout;
+#define JSON_OBJECTS_COUNT "objects_count"
+#define JSON_BYTES_COUNT "bytes_count"
+
+S3DataUsageAction::S3DataUsageAction(
+    std::shared_ptr<S3RequestObject> req,
+    std::shared_ptr<S3MotrKVSReaderFactory> kvs_reader_factory,
+    std::shared_ptr<MotrAPI> motr_api)
     : S3Action(req, true, nullptr, true, true), request(req) {
+  if (motr_api) {
+    motr_kvs_api = std::move(motr_api);
+  } else {
+    motr_kvs_api = std::make_shared<ConcreteMotrAPI>();
+  }
+  if (kvs_reader_factory) {
+    motr_kvs_reader_factory = std::move(kvs_reader_factory);
+  } else {
+    motr_kvs_reader_factory = std::make_shared<S3MotrKVSReaderFactory>();
+  }
+  motr_kvs_reader =
+      motr_kvs_reader_factory->create_motr_kvs_reader(request, motr_kvs_api);
+  max_records_per_request =
+      S3Option::get_instance()->get_motr_idx_fetch_count();
+  last_key = "";
+
   setup_steps();
 }
 
@@ -33,18 +59,128 @@ S3DataUsageAction::~S3DataUsageAction() {
   s3_log(S3_LOG_DEBUG, "", "%s\n", __func__);
 }
 
-void S3DataUsageAction::prepare_the_response() {
-  json_response =
-      "[ \"12345\": { \"bytes_count\": 123456789 }, \"54321\": { "
-      "\"bytes_count\": 987654321 } ]";
+void S3DataUsageAction::get_data_usage_counters() {
+  // json_response =
+  //     "[ \"12345\": { \"bytes_count\": 123456789 }, \"54321\": { "
+  //     "\"bytes_count\": 987654321 } ]";
+
+  motr_kvs_reader->next_keyval(
+      data_usage_accounts_index_layout, last_key, max_records_per_request,
+      std::bind(&S3DataUsageAction::get_next_keyval_success, this),
+      std::bind(&S3DataUsageAction::get_next_keyval_failure, this));
+}
+
+std::string extract_account_id_from_motr_key(const std::string& motr_key) {
+  const char account_id_delimeter = '/';
+  auto pos = motr_key.find(account_id_delimeter);
+  return (pos != std::string::npos ? motr_key.substr(0, pos) : "");
+}
+
+int from_json(std::string json, int64_t* p_objects_count,
+              int64_t* p_bytes_count) {
+  Json::Value newroot;
+  Json::Reader reader;
+  bool parsingSuccessful = reader.parse(json.c_str(), newroot);
+  if (!parsingSuccessful) {
+    return -1;
+  }
+
+  if (!newroot.isMember(JSON_OBJECTS_COUNT) ||
+      !newroot.isMember(JSON_BYTES_COUNT)) {
+    return -1;
+  }
+
+  *p_objects_count = newroot[JSON_OBJECTS_COUNT].asInt64();
+  *p_bytes_count = newroot[JSON_BYTES_COUNT].asInt64();
+  return 0;
+}
+
+void S3DataUsageAction::get_next_keyval_success() {
+  s3_log(S3_LOG_DEBUG, stripped_request_id, "%s Entry\n", __func__);
+  auto& kvps = motr_kvs_reader->get_key_values();
+  size_t length = kvps.size();
+  for (auto& kv : kvps) {
+    std::string key = kv.first;
+    std::string json = kv.second.second;
+    s3_log(S3_LOG_DEBUG, request_id, "Read key = %s\n", key.c_str());
+    s3_log(S3_LOG_DEBUG, request_id, "Read Value = %s\n", json.c_str());
+    std::string account_id = extract_account_id_from_motr_key(key);
+    if (account_id.empty()) {
+      s3_log(
+          S3_LOG_ERROR, stripped_request_id,
+          "Failed to extract the account ID from key %s. Skipping the record",
+          key.c_str());
+      continue;
+    }
+    int64_t objects_counter;
+    int64_t bytes_counter;
+    if (from_json(json, &objects_counter, &bytes_counter) != 0) {
+      s3_log(S3_LOG_ERROR, stripped_request_id,
+             "Failed to extract the counters from json %s. Skipping the record",
+             json.c_str());
+      continue;
+    }
+
+    if (counters.find(account_id) != counters.end()) {
+      counters[account_id] = bytes_counter;
+    } else {
+      counters[account_id] += bytes_counter;
+    }
+  }
+
+  if (length < max_records_per_request) {
+    // Read all, move forward.
+    next();
+  } else {
+    // Read the next chunk of counter records.
+    last_key = kvps.rbegin()->first;
+    motr_kvs_reader->next_keyval(
+        data_usage_accounts_index_layout, last_key, max_records_per_request,
+        std::bind(&S3DataUsageAction::get_next_keyval_success, this),
+        std::bind(&S3DataUsageAction::get_next_keyval_failure, this));
+  }
+  s3_log(S3_LOG_DEBUG, stripped_request_id, "%s Exit\n", __func__);
+}
+
+void S3DataUsageAction::get_next_keyval_failure() {
+  s3_log(S3_LOG_DEBUG, stripped_request_id, "%s Entry\n", __func__);
+  auto reader_state = motr_kvs_reader->get_state();
+  if (reader_state == S3MotrKVSReaderOpState::missing) {
+    s3_log(S3_LOG_DEBUG, request_id, "No(more) records in data usage index");
+    next();
+    return;
+  } else if (reader_state == S3MotrKVSReaderOpState::failed_to_launch) {
+    s3_log(S3_LOG_ERROR, request_id,
+           "Failed to launch data usage index listing");
+    set_s3_error("ServiceUnavailable");
+  } else {
+    s3_log(S3_LOG_DEBUG, request_id,
+           "Internal error during data usage index listing");
+    set_s3_error("InternalError");
+  }
+  send_response_to_s3_client();
+  s3_log(S3_LOG_DEBUG, "", "%s Exit", __func__);
 }
 
 void S3DataUsageAction::setup_steps() {
-  prepare_the_response();
-  send_response_to_s3_client();
+  s3_log(S3_LOG_DEBUG, request_id, "Setting up the Data Usage action");
+  ACTION_TASK_ADD(S3DataUsageAction::get_data_usage_counters, this);
+  ACTION_TASK_ADD(S3DataUsageAction::send_response_to_s3_client, this);
 }
 
 void S3DataUsageAction::send_response_to_s3_client() {
   s3_log(S3_LOG_DEBUG, "", "%s Entry \n", __func__);
-  request->send_response(S3HttpSuccess200, json_response);
+  Json::Value root(Json::arrayValue);
+  for (auto& cnt : counters) {
+    Json::Value account;
+    Json::Value counters;
+    counters[JSON_BYTES_COUNT] = Json::Value((Json::Value::Int64)(cnt.second));
+    account[cnt.first] = counters;
+    root.append(account);
+  }
+
+  Json::FastWriter fastWriter;
+  std::string json = fastWriter.write(root);
+  request->send_response(S3HttpSuccess200, json);
+  s3_log(S3_LOG_DEBUG, "", "%s Exit \n", __func__);
 }
